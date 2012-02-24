72ba73804c5b0735cad1f4641c19a18d93b02d97
Dick Fickling <dick@fickling.us>
2012-02-24 15:44:54 -0500

    Add spacer to toolbar

---
+++ MenuActions/src/org/sleuthkit/autopsy/menuactions/SpacerPanel.java
@@ -0,0 +1,47 @@
+/*
+ * Autopsy Forensic Browser
+ * 
+ * Copyright 2011 Basis Technology Corp.
+ * Contact: carrier <at> sleuthkit <dot> org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sleuthkit.autopsy.menuactions;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import org.openide.util.actions.Presenter;
+
+/**
+ *
+ * @author dfickling
+ */
+public class SpacerPanel extends javax.swing.JPanel{
+    SpacerPanel() {
+        this.setPreferredSize(new Dimension(2000, 20));
+    }
+}
+
+class SpacerAction extends AbstractAction implements Presenter.Toolbar {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    @Override
+    public Component getToolbarPresenter() {
+        return new SpacerPanel();
+    }
+}
