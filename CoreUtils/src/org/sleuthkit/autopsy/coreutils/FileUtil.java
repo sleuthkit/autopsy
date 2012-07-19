63529764066263d5d815a356c5769b108ee7b540
adam-m <amalinowski@basistech.com>
2012-07-19 15:55:02 -0400

    Move delete dir method to FileUtil

---
+++ CoreUtils/src/org/sleuthkit/autopsy/coreutils/FileUtil.java
@@ -0,0 +1,48 @@
+/*
+ * Autopsy Forensic Browser
+ *
+ * Copyright 2012 Basis Technology Corp.
+ * Contact: carrier <at> sleuthkit <dot> org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sleuthkit.autopsy.coreutils;
+
+import java.io.File;
+
+/**
+ * File and dir utilities
+ */
+public class FileUtil {
+
+    /**
+     * Recursively delete a dir
+     *
+     * @param dirPath dir path of a dir to delete
+     * @return true if the dir deleted, false otherwise
+     */
+    public static boolean deleteDir(File dirPath) {
+        if (dirPath.exists()) {
+            File[] files = dirPath.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].isDirectory()) {
+                    deleteDir(files[i]);
+                } else {
+                    files[i].delete();
+                }
+            }
+        }
+        return (dirPath.delete());
+
+    }
+}
