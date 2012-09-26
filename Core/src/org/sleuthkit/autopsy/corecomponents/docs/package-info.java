ef698fb1e5dc499afba5d8743dc4509746d88ea5
Devin148 <dhurd@basistech.com>
2012-09-26 15:35:57 -0400

    Restructure user help docs.

--- Core/src/org/sleuthkit/autopsy/corecomponents/docs/package-info.java
+++ Core/src/org/sleuthkit/autopsy/corecomponents/docs/package-info.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-@HelpSetRegistration(helpSet = "ingest-hs.xml", position = 4000)
-package org.sleuthkit.autopsy.ingest.docs;
+@HelpSetRegistration(helpSet = "corecomponents-hs.xml", position = 4500)
+package org.sleuthkit.autopsy.corecomponents.docs;
 
 import org.netbeans.api.javahelp.HelpSetRegistration;
