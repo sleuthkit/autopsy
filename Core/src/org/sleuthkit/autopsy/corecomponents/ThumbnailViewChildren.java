345d0fd5e80fbe75c57aae698b3b78a796f4dcfe
adam-m <amalinowski@basistech.com>
2012-11-08 15:28:20 -0500

    Thumbnail viewer tweaks, fix resizing, add file name

--- Core/src/org/sleuthkit/autopsy/corecomponents/ThumbnailViewChildren.java
+++ Core/src/org/sleuthkit/autopsy/corecomponents/ThumbnailViewChildren.java
@@ -51,7 +51,7 @@ import org.sleuthkit.datamodel.File;
 class ThumbnailViewChildren extends Children.Keys<Integer> {
 
     private static final IsSupportedContentVisitor isSupportedVisitor = new IsSupportedContentVisitor();
-    static final int IMAGES_PER_PAGE = 1000;
+    static final int IMAGES_PER_PAGE = 200;
     private Node parent;
     private final HashMap<Integer, List<Node>> pages = new HashMap<Integer, List<Node>>();
     private int totalImages = 0;
