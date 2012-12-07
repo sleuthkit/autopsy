68af109a1f6ed501826ab95a31b672bc969c5428
adam-m <amalinowski@basistech.com>
2012-12-07 12:47:33 -0500

    add more info to report name

--- Core/src/org/sleuthkit/autopsy/report/ReportBodyFile.java
+++ Core/src/org/sleuthkit/autopsy/report/ReportBodyFile.java
@@ -168,7 +168,7 @@ public class ReportBodyFile implements ReportModule {
 
     @Override
     public String getName() {
-        String name = "Body File";
+        String name = "Body File (Timeline Report)";
         return name;
     }
 
