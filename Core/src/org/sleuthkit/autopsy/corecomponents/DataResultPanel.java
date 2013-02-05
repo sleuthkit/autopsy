6223619340c3fc1b9a0f52d5ff97df28d0817a17
adam-m <amalinowski@basistech.com>
2013-02-05 11:16:24 -0500

    remove method to set content viewer (using constructor for this)

--- Core/src/org/sleuthkit/autopsy/corecomponents/DataResultPanel.java
+++ Core/src/org/sleuthkit/autopsy/corecomponents/DataResultPanel.java
@@ -105,9 +105,6 @@ public class DataResultPanel extends javax.swing.JPanel implements DataResult, C
         this.customContentViewer = customContentViewer; 
     }
     
-    public void setContentViewer(DataContent contentViewer) {
-        this.customContentViewer = contentViewer;
-    }
 
     /**
      * Factory method to create, customize and open a new custom data result panel.
