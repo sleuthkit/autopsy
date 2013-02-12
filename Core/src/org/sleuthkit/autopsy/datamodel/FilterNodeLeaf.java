1e0b8508b6f0d920cb35e736a63f80583db3f077
adam-m <amalinowski@basistech.com>
2013-02-12 16:23:26 -0500

    - add a generic filter node class that can be reused by different viewers/clients - use leaf filter node in timeline

---
+++ Core/src/org/sleuthkit/autopsy/datamodel/FilterNodeLeaf.java
@@ -0,0 +1,34 @@
+/*
+ * Autopsy Forensic Browser
+ * 
+ * Copyright 2011-2013 Basis Technology Corp.
+ * Contact: carrier <at> sleuthkit <dot> org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sleuthkit.autopsy.datamodel;
+
+import org.openide.nodes.FilterNode;
+import org.openide.nodes.Node;
+
+/**
+ * AbstractFile filter node that filters out the children
+ */
+public class FilterNodeLeaf extends FilterNode {
+
+    public FilterNodeLeaf(Node node) {
+        super(node, Children.LEAF);
+    }
+
+    
+}
