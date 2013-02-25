ea05517665216bd60b7b012a7328eca3681e5311
Tim McIver <mciver@basistech.com>
2013-02-25 17:57:07 -0500

    Created FrameCapture interface and made DataContentViewerMedia implement it. Added ServiceProvider annotation so that lookup can be used to gain access to this implementation. Added support class VideoFrame. Added apache commons io to corelibs project.xml.

---
+++ Core/src/org/sleuthkit/autopsy/corecomponents/FrameCapture.java
@@ -0,0 +1,20 @@
+package org.sleuthkit.autopsy.corecomponents;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Interface used to capture frames from a video file.
+ */
+public interface FrameCapture {
+    
+    /**
+     * @param file the video file to use
+     * @param numFrames the number of frames to capture. Note that the actual
+     * number of frames returned may be less than this number. Specifically, this
+     * may happen if the video is very short.
+     * @return a list of VideoFrames representing the captured frames
+     */
+    List<VideoFrame> captureFrames(File file, int numFrames);
+
+}
---
+++ Core/src/org/sleuthkit/autopsy/corecomponents/VideoFrame.java
@@ -0,0 +1,25 @@
+package org.sleuthkit.autopsy.corecomponents;
+
+import java.awt.Image;
+
+/**
+ *
+ */
+public class VideoFrame {
+
+    private Image frame;
+    private long timeMillis;
+    
+    public VideoFrame(Image frame, long timeMillis) {
+        this.frame = frame;
+        this.timeMillis = timeMillis;
+    }
+
+    public Image getFrame() {
+        return frame;
+    }
+
+    public long getTimeMillis() {
+        return timeMillis;
+    }
+}
