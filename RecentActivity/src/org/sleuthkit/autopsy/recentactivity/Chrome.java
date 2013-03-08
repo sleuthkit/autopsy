e19f6b20df2fa0a8092d296ae98435b16e59a292
Tim McIver <mciver@basistech.com>
2013-03-08 12:57:17 -0500

    Updated method getHistory() in RecentActivity/Chrome.java so that the allocation status of the found history files is checked and only the allocated files are used. Log a warning if more than one allocated history file is found.

--- RecentActivity/src/org/sleuthkit/autopsy/recentactivity/Chrome.java
+++ RecentActivity/src/org/sleuthkit/autopsy/recentactivity/Chrome.java
@@ -51,6 +51,7 @@ import org.sleuthkit.datamodel.BlackboardAttribute;
 import org.sleuthkit.datamodel.BlackboardAttribute.ATTRIBUTE_TYPE;
 import org.sleuthkit.datamodel.Image;
 import org.sleuthkit.datamodel.TskCoreException;
+import org.sleuthkit.datamodel.TskData;
 
 /**
  * Chrome recent activity extraction
@@ -107,54 +108,69 @@ public class Chrome extends Extract implements IngestModuleImage {
         } catch (TskCoreException ex) {
             logger.log(Level.SEVERE, "Error when trying to get Chrome history files.", ex);
         }
+        
+        // get only the allocated ones, for now
+        List<FsContent> allocatedHistoryFiles = new ArrayList<>();
+        for (FsContent historyFile : historyFiles) {
+            if (historyFile.isMetaFlagSet(TskData.TSK_FS_META_FLAG_ENUM.ALLOC)) {
+                allocatedHistoryFiles.add(historyFile);
+            }
+        }
+        
+        // we should have only one allocated history file. Log a warning if we
+        // have more, but process them all
+        if (allocatedHistoryFiles.size() > 1) {
+            logger.log(Level.WARNING, "Found more than one allocated Chrome history file. Processing them all.");
+        } else if (allocatedHistoryFiles.size() == 0) {
+            logger.log(Level.WARNING, "Could not find an allocated Chrome history file.");
+            return;
+        }
 
         int j = 0;
-        if (historyFiles != null && !historyFiles.isEmpty()) {
-            while (j < historyFiles.size()) {
-                String temps = currentCase.getTempDirectory() + File.separator + historyFiles.get(j).getName().toString() + j + ".db";
-                int errors = 0;
-                final FsContent historyFile = historyFiles.get(j++);
-                if (historyFile.getSize() == 0) {
-                    continue;
-                }
-                try {
-                    ContentUtils.writeToFile(historyFile, new File(temps));
-                } catch (IOException ex) {
-                    logger.log(Level.SEVERE, "Error writing temp sqlite db for Chrome web history artifacts.{0}", ex);
-                    this.addErrorMessage(this.getName() + ": Error while trying to analyze file:" + historyFile.getName());
-                }
-                File dbFile = new File(temps);
-                if (controller.isCancelled()) {
-                    dbFile.delete();
-                    break;
-                }
-                List<HashMap<String, Object>> tempList = null;
-                tempList = this.dbConnect(temps, chquery);
-                logger.log(Level.INFO, moduleName + "- Now getting history from " + temps + " with " + tempList.size() + "artifacts identified.");
-                for (HashMap<String, Object> result : tempList) {
-
-                    Collection<BlackboardAttribute> bbattributes = new ArrayList<BlackboardAttribute>();
-                    bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_URL.getTypeID(), "Recent Activity", ((result.get("url").toString() != null) ? result.get("url").toString() : "")));
-                    bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_URL_DECODED.getTypeID(), "Recent Activity", ((result.get("url").toString() != null) ? EscapeUtil.decodeURL(result.get("url").toString()) : "")));
-                    //TODO Revisit usage of deprecated constructor per TSK-583
-                    //bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_LAST_ACCESSED.getTypeID(), "Recent Activity", "Last Visited", ((Long.valueOf(result.get("last_visit_time").toString())) / 10000000)));
-                    bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_DATETIME_ACCESSED.getTypeID(), "Recent Activity", ((Long.valueOf(result.get("last_visit_time").toString())) / 10000000)));
-                    bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_REFERRER.getTypeID(), "Recent Activity", ((result.get("from_visit").toString() != null) ? result.get("from_visit").toString() : "")));
-                    bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_NAME.getTypeID(), "Recent Activity", ((result.get("title").toString() != null) ? result.get("title").toString() : "")));
-                    bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_PROG_NAME.getTypeID(), "Recent Activity", "Chrome"));
-                    bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_DOMAIN.getTypeID(), "Recent Activity", (Util.extractDomain((result.get("url").toString() != null) ? result.get("url").toString() : ""))));
-                    this.addArtifact(ARTIFACT_TYPE.TSK_WEB_HISTORY, historyFile, bbattributes);
-
-                }
-                if (errors > 0) {
-                    this.addErrorMessage(this.getName() + ": Error parsing " + errors + " Chrome web history artifacts.");
-                }
-       
+        while (j < historyFiles.size()) {
+            String temps = currentCase.getTempDirectory() + File.separator + historyFiles.get(j).getName().toString() + j + ".db";
+            int errors = 0;
+            final FsContent historyFile = historyFiles.get(j++);
+            if (historyFile.getSize() == 0) {
+                continue;
+            }
+            try {
+                ContentUtils.writeToFile(historyFile, new File(temps));
+            } catch (IOException ex) {
+                logger.log(Level.SEVERE, "Error writing temp sqlite db for Chrome web history artifacts.{0}", ex);
+                this.addErrorMessage(this.getName() + ": Error while trying to analyze file:" + historyFile.getName());
+            }
+            File dbFile = new File(temps);
+            if (controller.isCancelled()) {
                 dbFile.delete();
+                break;
+            }
+            List<HashMap<String, Object>> tempList = null;
+            tempList = this.dbConnect(temps, chquery);
+            logger.log(Level.INFO, moduleName + "- Now getting history from " + temps + " with " + tempList.size() + "artifacts identified.");
+            for (HashMap<String, Object> result : tempList) {
+
+                Collection<BlackboardAttribute> bbattributes = new ArrayList<BlackboardAttribute>();
+                bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_URL.getTypeID(), "Recent Activity", ((result.get("url").toString() != null) ? result.get("url").toString() : "")));
+                bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_URL_DECODED.getTypeID(), "Recent Activity", ((result.get("url").toString() != null) ? EscapeUtil.decodeURL(result.get("url").toString()) : "")));
+                //TODO Revisit usage of deprecated constructor per TSK-583
+                //bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_LAST_ACCESSED.getTypeID(), "Recent Activity", "Last Visited", ((Long.valueOf(result.get("last_visit_time").toString())) / 10000000)));
+                bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_DATETIME_ACCESSED.getTypeID(), "Recent Activity", ((Long.valueOf(result.get("last_visit_time").toString())) / 10000000)));
+                bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_REFERRER.getTypeID(), "Recent Activity", ((result.get("from_visit").toString() != null) ? result.get("from_visit").toString() : "")));
+                bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_NAME.getTypeID(), "Recent Activity", ((result.get("title").toString() != null) ? result.get("title").toString() : "")));
+                bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_PROG_NAME.getTypeID(), "Recent Activity", "Chrome"));
+                bbattributes.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_DOMAIN.getTypeID(), "Recent Activity", (Util.extractDomain((result.get("url").toString() != null) ? result.get("url").toString() : ""))));
+                this.addArtifact(ARTIFACT_TYPE.TSK_WEB_HISTORY, historyFile, bbattributes);
+
+            }
+            if (errors > 0) {
+                this.addErrorMessage(this.getName() + ": Error parsing " + errors + " Chrome web history artifacts.");
             }
 
-            services.fireModuleDataEvent(new ModuleDataEvent("Recent Activity", BlackboardArtifact.ARTIFACT_TYPE.TSK_WEB_HISTORY));
+            dbFile.delete();
         }
+
+        services.fireModuleDataEvent(new ModuleDataEvent("Recent Activity", BlackboardArtifact.ARTIFACT_TYPE.TSK_WEB_HISTORY));
     }
 
     private void getBookmark(Image image, IngestImageWorkerController controller) {
