3bc95f86f3fa6812cd383d9a153fa22e6d76f963
Sean-M <Smoss117@gmail.com>
2013-04-05 16:11:45 -0400

    Separated out testing logic into separate modules to make it easier to use.

---
+++ test/script/Emailer.py
@@ -0,0 +1,83 @@
+import smtplib
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.MIMEBase import MIMEBase
+from email import Encoders
+import urllib2
+import xml
+from time import localtime, strftime
+from xml.dom.minidom import parse, parseString
+import subprocess
+import sys
+import os
+
+def send_email(parsed, errorem, attachl):
+	if(not args.list):
+		sys.exit()
+	element = parsed.getElementsByTagName("email")
+	if(len(element)<=0):
+		return
+	element = element[0]
+	toval = element.getAttribute("value").encode().decode("utf_8")
+	if(toval==None):
+		return
+	element = parsed.getElementsByTagName("mail_server")[0]
+	serverval = element.getAttribute("value").encode().decode("utf_8")
+	# Create the container (outer) email message.
+	msg = MIMEMultipart()
+	msg['Subject'] = 'Email Test'
+	# me == the sender's email address
+	# family = the list of all recipients' email addresses
+	msg['From'] = 'AutopsyContinuousTest'
+	msg['To'] = toval
+	msg.preamble = 'This is a test'
+	container = MIMEText(errorem, 'plain')
+	msg.attach(container)
+	Build_email(msg, attachl)
+	s = smtplib.SMTP(serverval)
+	s.sendmail(msg['From'], msg['To'], msg.as_string())
+	s.quit()
+	
+def Build_email(msg, attachl):
+	for file in attachl:
+		part = MIMEBase('application', "octet-stream")
+		atach = open(file, "rb")
+		attch = atach.read()
+		noml = file.split("\\")
+		nom = noml[len(noml)-1]
+		part.set_payload(attch)
+		Encoders.encode_base64(part)
+		part.add_header('Content-Disposition', 'attachment; filename="' + nom + '"')
+		msg.attach(part)
+		
+# Returns a Windows style path starting with the cwd and
+# ending with the list of directories given
+def make_local_path(*dirs):
+	path = wgetcwd()
+	for dir in dirs:
+		path += ("\\" + dir)
+	return path_fix(path)
+
+# Returns a Windows style path based only off the given directories
+def make_path(*dirs):
+	path = dirs[0]
+	for dir in dirs[1:]:
+		path += ("\\" + dir)
+	return path_fix(path)
+	
+# Fix a standard os.path by making it Windows format
+def path_fix(path):
+	return path.replace("/", "\\")
+	
+# Gets the true current working directory instead of Cygwin's
+def wgetcwd():
+	proc = subprocess.Popen(("cygpath", "-m", os.getcwd()), stdout=subprocess.PIPE)
+	out,err = proc.communicate()
+	tst = out.rstrip()
+	if os.getcwd == tst:
+		return os.getcwd
+	else:
+		proc = subprocess.Popen(("cygpath", "-m", os.getcwd()), stdout=subprocess.PIPE)
+		out,err = proc.communicate()
+		return out.rstrip()
---
+++ test/script/srcupdater.py
@@ -0,0 +1,186 @@
+import codecs
+import datetime
+import logging
+import os
+import re
+import shutil
+import socket
+import sqlite3
+import subprocess
+import sys
+from sys import platform as _platform
+import time
+import traceback
+import xml
+from xml.dom.minidom import parse, parseString
+import Emailer
+
+def compile():
+	global redo
+	global tryredo
+	global failedbool
+	global errorem
+	global attachl
+	global passed
+	global parsed
+	passed = True
+	tryredo = False
+	redo = True
+	while(redo):
+		passed = True
+		if(passed):
+			gitPull("sleuthkit")
+		if(passed):
+			vsBuild()
+		if(passed):
+			gitPull("autopsy")
+		if(passed):
+			antBuild("datamodel", False)
+		if(passed):
+			antBuild("autopsy", True)
+		if(passed):
+			redo = False
+		else:
+			print("Compile Failed")
+			time.sleep(3600)
+	attachl = []
+	errorem = "The test standard didn't match the gold standard.\n"
+	failedbool = False
+	if(tryredo):
+		errorem += "Rebuilt properly.\n"
+		send_email(parsed, errorem, attachl)
+		attachl = []
+		errorem = "The test standard didn't match the gold standard.\n"	
+		passed = True
+		
+#Pulls from git
+def gitPull(TskOrAutopsy):
+	global SYS
+	global errorem
+	global attachl
+	ccwd = ""
+	gppth = Emailer.make_local_path("..", "GitPullOutput" + TskOrAutopsy + ".txt")
+	attachl.append(gppth)
+	gpout = open(gppth, 'a')
+	toPull = "http://www.github.com/sleuthkit/" + TskOrAutopsy
+	call = ["git", "pull", toPull]
+	if TskOrAutopsy == "sleuthkit":
+		ccwd = os.path.join("..", "..", "..", "sleuthkit")
+	else:
+		ccwd = os.path.join("..", "..")
+	subprocess.call(call, stdout=gpout, cwd=ccwd)
+	gpout.close()
+	
+
+#Builds TSK as a win32 applicatiion
+def vsBuild():
+	global redo
+	global tryredo
+	global passed
+	global parsed
+	#Please ensure that the current working directory is $autopsy/testing/script
+	vs = []
+	vs.append("/cygdrive/c/windows/microsoft.NET/framework/v4.0.30319/MSBuild.exe")
+	vs.append(os.path.join("..", "..", "..","sleuthkit", "win32", "Tsk-win.sln"))
+	vs.append("/p:configuration=release")
+	vs.append("/p:platform=win32")
+	vs.append("/t:clean")
+	vs.append("/t:rebuild")
+	print(vs)
+	VSpth = Emailer.make_local_path("..", "VSOutput.txt")
+	VSout = open(VSpth, 'a')
+	subprocess.call(vs, stdout=VSout)
+	VSout.close()
+	chk = os.path.join("..", "..", "..","sleuthkit", "win32", "Release", "libtsk_jni.dll")
+	try:
+		open(chk)
+	except IOError as e:
+		global errorem
+		global attachl
+		if(not tryredo):
+			errorem += "LIBTSK C++ failed to build.\n"
+			attachl.append(VSpth)
+			send_email(parsed, errorem, attachl)
+		tryredo = True
+		passed = False
+		redo = True
+		
+	
+ 
+#Builds Autopsy or the Datamodel
+def antBuild(which, Build):
+	global redo
+	global passed
+	global tryredo
+	global parsed
+	directory = os.path.join("..", "..")
+	ant = []
+	if which == "datamodel":
+		directory = os.path.join("..", "..", "..", "sleuthkit", "bindings", "java")
+	ant.append("ant")
+	ant.append("-f")
+	ant.append(directory)
+	ant.append("clean")
+	if(Build):
+		ant.append("build")
+	else:
+		ant.append("dist")
+	antpth = Emailer.make_local_path("..", "ant" + which + "Output.txt")
+	antout = open(antpth, 'a')
+	succd = subprocess.call(ant, stdout=antout)
+	antout.close()
+	global errorem
+	global attachl
+	if which == "datamodel":
+		chk = os.path.join("..", "..", "..","sleuthkit",  "bindings", "java", "dist", "TSK_DataModel.jar")
+		try:
+			open(chk)
+		except IOError as e:
+			if(not tryredo):
+				errorem += "DataModel Java build failed.\n"
+				attachl.append(antpth)
+				send_email()
+			passed = False
+			tryredo = True
+	elif (succd != 0 and (not tryredo)):
+		errorem += "Autopsy build failed.\n"
+		attachl.append(antpth)
+		send_email(parsed, errorem, attachl)
+		tryredo = True
+	elif (succd != 0):
+		passed = False
+
+
+def main():
+	global parsed
+	global errorem
+	global attachl
+	print("hello")
+	errorem = ""
+	attachl = []
+	config_file = ""
+	print("hi")
+	arg = sys.argv.pop(0)
+	arg = sys.argv.pop(0)
+	config_file = arg
+	print(config_file)
+	parsed = parse(config_file)
+	compile()
+	
+class OS:
+  LINUX, MAC, WIN, CYGWIN = range(4)	  
+if __name__ == "__main__":
+	global SYS
+	if _platform == "linux" or _platform == "linux2":
+		SYS = OS.LINUX
+	elif _platform == "darwin":
+		SYS = OS.MAC
+	elif _platform == "win32":
+		SYS = OS.WIN
+	elif _platform == "cygwin":
+		SYS = OS.CYGWIN
+		
+	if SYS is OS.WIN or SYS is OS.CYGWIN:
+		main()
+	else:
+		print("We only support Windows and Cygwin at this time.")
