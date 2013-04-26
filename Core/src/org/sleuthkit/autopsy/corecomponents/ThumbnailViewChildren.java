1c1d3c52ae2b93dbbfef08f7256afd15ff3f67a6
adam-m <amalinowski@basistech.com>
2013-04-25 23:43:32 -0400

    add support for carved files in thumbnail view

--- Core/src/org/sleuthkit/autopsy/corecomponents/ThumbnailViewChildren.java
+++ Core/src/org/sleuthkit/autopsy/corecomponents/ThumbnailViewChildren.java
@@ -19,10 +19,8 @@
 package org.sleuthkit.autopsy.corecomponents;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.logging.Level;
 import javax.imageio.ImageIO;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
@@ -34,6 +32,7 @@ import org.sleuthkit.datamodel.Content;
 import org.sleuthkit.datamodel.ContentVisitor;
 import org.sleuthkit.datamodel.DerivedFile;
 import org.sleuthkit.datamodel.File;
+import org.sleuthkit.datamodel.LayoutFile;
 
 /**
  * Complementary class to ThumbnailViewNode. Children node factory. Wraps around
@@ -172,6 +171,12 @@ class ThumbnailViewChildren extends Children.Keys<Integer> {
         public Boolean visit(DerivedFile f) {
             return isSupported(f);
         }
+        
+        @Override
+        public Boolean visit(LayoutFile f) {
+            return isSupported(f);
+        }
+
 
         @Override
         public Boolean visit(File f) {
