a67f7fde537d7f402de97e9232e6152f0d2b1924
jmillman <jmillman@basistech.com>
2013-09-13 17:12:50 -0400

    vik- 174 : added min string length to language detection

--- KeywordSearch/src/org/sleuthkit/autopsy/keywordsearch/TikaLanguageIdentifier.java
+++ KeywordSearch/src/org/sleuthkit/autopsy/keywordsearch/TikaLanguageIdentifier.java
@@ -12,59 +12,65 @@ import org.sleuthkit.datamodel.BlackboardAttribute;
 import org.sleuthkit.datamodel.TskCoreException;
 
 /**
- *TextLanguageIdentifier implementation based on a wrapped Tike LanguageIdentifier
- * 
- * 
+ * TextLanguageIdentifier implementation based on a wrapped Tike
+ * LanguageIdentifier
+ *
+ *
  * @author jmillman
  */
 public class TikaLanguageIdentifier implements TextLanguageIdentifier {
 
     private static final Logger logger = Logger.getLogger(TikaLanguageIdentifier.class.getName());
+    private static final int MIN_STRING_LENGTH = 1000;
 
     @Override
     public void addLanguageToBlackBoard(String extracted, AbstractFile sourceFile) {
+        if (extracted.length() > MIN_STRING_LENGTH) {
+            org.apache.tika.language.LanguageIdentifier li = new org.apache.tika.language.LanguageIdentifier(extracted);
 
-        org.apache.tika.language.LanguageIdentifier li = new org.apache.tika.language.LanguageIdentifier(extracted);
-        
-        logger.log(Level.INFO, sourceFile.getName() + " detected language: " + li.getLanguage()
-                + "with " + ((li.isReasonablyCertain()) ? "HIGH" : "LOW") + "confidence");
-        
-        BlackboardArtifact genInfo;
-        try {
-            genInfo = sourceFile.newArtifact(BlackboardArtifact.ARTIFACT_TYPE.TSK_GEN_INFO);
+            logger.log(Level.INFO, sourceFile.getName() + " detected language: " + li.getLanguage()
+                    + " with " + ((li.isReasonablyCertain()) ? "HIGH" : "LOW") + " confidence");
 
-            BlackboardAttribute textLang = new BlackboardAttribute(
-                    BlackboardAttribute.ATTRIBUTE_TYPE.TSK_TEXT_LANGUAGE.getTypeID(),
-                    KeywordSearchIngestModule.MODULE_NAME, li.getLanguage());
+            BlackboardArtifact genInfo;
+            try {
+                genInfo = sourceFile.newArtifact(BlackboardArtifact.ARTIFACT_TYPE.TSK_GEN_INFO);
+
+                BlackboardAttribute textLang = new BlackboardAttribute(
+                        BlackboardAttribute.ATTRIBUTE_TYPE.TSK_TEXT_LANGUAGE.getTypeID(),
+                        KeywordSearchIngestModule.MODULE_NAME, li.getLanguage());
 
 
-            genInfo.addAttribute(textLang);
+                genInfo.addAttribute(textLang);
 
-        } catch (TskCoreException ex) {
-            logger.log(Level.WARNING, "failed to add TSK_TEXT_LANGUAGE attribute to TSK_GEN_INFO artifact for file: " + sourceFile.getName(), ex);
+            } catch (TskCoreException ex) {
+                logger.log(Level.WARNING, "failed to add TSK_TEXT_LANGUAGE attribute to TSK_GEN_INFO artifact for file: " + sourceFile.getName(), ex);
+            }
+
+
+
+
+        }else{
+            logger.info("extracted text too short, skipping language detection on " + sourceFile.getName());
         }
+    } /*  //attempt to verify that artifact with attribute was created
+     ArrayList<BlackboardArtifact> arts;
+        
+        
+     try {
+     arts = Case.getCurrentCase().getSleuthkitCase().getBlackboardArtifacts(BlackboardArtifact.ARTIFACT_TYPE.TSK_GEN_INFO, sourceFile.getId());
+        
+     for (BlackboardArtifact art : arts) {
+        
+     List<BlackboardAttribute> attrs = art.getAttributes();
+     for (BlackboardAttribute attr : attrs) {
+     if (attr.getAttributeTypeID() == BlackboardAttribute.ATTRIBUTE_TYPE.TSK_TEXT_LANGUAGE.getTypeID()) {
+     logger.log(Level.INFO, "succesfully added " + attr.getValueString() + " to gen info for:" + sourceFile.getName());
+     break;
+     }
+     }
+     }
+     } catch (TskCoreException ex) {
+     Exceptions.printStackTrace(ex);
+     }*/
 
-
-
-              /*  //attempt to verify that artifact with attribute was created
-                ArrayList<BlackboardArtifact> arts;
-        
-        
-                try {
-                    arts = Case.getCurrentCase().getSleuthkitCase().getBlackboardArtifacts(BlackboardArtifact.ARTIFACT_TYPE.TSK_GEN_INFO, sourceFile.getId());
-        
-                    for (BlackboardArtifact art : arts) {
-        
-                        List<BlackboardAttribute> attrs = art.getAttributes();
-                        for (BlackboardAttribute attr : attrs) {
-                            if (attr.getAttributeTypeID() == BlackboardAttribute.ATTRIBUTE_TYPE.TSK_TEXT_LANGUAGE.getTypeID()) {
-                                logger.log(Level.INFO, "succesfully added " + attr.getValueString() + " to gen info for:" + sourceFile.getName());
-                                break;
-                            }
-                        }
-                    }
-                } catch (TskCoreException ex) {
-                    Exceptions.printStackTrace(ex);
-                }*/
-    }
 }
