a3a027eea0bc065918e70f6a3a19d9336c3f251c
raman-bt <raman@basis.com>
2013-10-16 08:14:17 -0400

    New file, DSPProgressMonitor.java - that got missed in the the previous commit.

---
+++ Core/src/org/sleuthkit/autopsy/casemodule/DSPProgressMonitor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sleuthkit.autopsy.casemodule;
+
+/*
+ * An GUI agnostic DSPProgressMonitor interface for DataSorceProcesssors to 
+ * indicate progress.
+ * It models after a JProgressbar though could use any underlying implementation
+ */
+public interface DSPProgressMonitor {
+ 
+    void setIndeterminate(boolean indeterminate);
+    
+    void setProgress(int progress);
+    
+    void setText(String text);   
+}
