c04f4e64b95d4b676f964b2cae70690c1a484553
Jeff Wallace <jwallace@basistech.com>
2013-10-31 10:45:15 -0400

    Fixed the case where matches label was incorrect for 0 results.

--- Core/src/org/sleuthkit/autopsy/corecomponents/DataResultPanel.java
+++ Core/src/org/sleuthkit/autopsy/corecomponents/DataResultPanel.java
@@ -625,12 +625,7 @@ public class DataResultPanel extends javax.swing.JPanel implements DataResult, C
             if (load && containsReal(delta)) {
                 load = false;
                 setupTabs(nme.getNode());
-                SwingUtilities.invokeLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        numberMatchLabel.setText(Integer.toString(rootNode.getChildren().getNodesCount()));
-                    }
-                });
+                updateMatches();
             }
         }
         
@@ -642,9 +637,25 @@ public class DataResultPanel extends javax.swing.JPanel implements DataResult, C
             }
             return false;
         }
+        
+        /**
+         * Updates the Number of Matches label on the DataResultPanel.
+         * 
+         */
+        private void updateMatches() {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    if (rootNode != null && rootNode.getChildren() != null) {
+                        setNumMatches(rootNode.getChildren().getNodesCount());
+                    }
+                }
+            });
+        }
 
         @Override
         public void childrenRemoved(NodeMemberEvent nme) {
+            updateMatches();
         }
 
         @Override
