6dab171f21f12c2ab1c0bb30d8e25e320bc4978f
Jeff Wallace <jwallace@basistech.com>
2013-12-04 13:33:10 -0500

    Removed stopwatch and debug printlns.

--- Core/src/org/sleuthkit/autopsy/report/ReportGenerator.java
+++ Core/src/org/sleuthkit/autopsy/report/ReportGenerator.java
@@ -420,12 +420,7 @@ public class ReportGenerator {
                     continue;
                 }
 
-                StopWatch stopwatch = new StopWatch();
-                stopwatch.start();
                 List<ArtifactData> unsortedArtifacts = getFilteredArtifacts(type, tagNamesFilter);
-                stopwatch.stop();
-                System.out.println("Number of Artifacts:\t" + unsortedArtifacts.size());
-                System.out.println("getFilteredArtifacts:\t" + stopwatch.getElapsedTime());
                 
                 if (unsortedArtifacts.isEmpty()) {
                     // Don't report on this artifact type if there are no results
@@ -434,11 +429,7 @@ public class ReportGenerator {
                 // The most efficient way to sort all the Artifacts is to add them to a List, and then
                 // sort that List based off a Comparator. Adding to a TreeMap/Set/List sorts the list
                 // each time an element is added, which adds unnecessary overhead if we only need it sorted once.
-                stopwatch.reset();
-                stopwatch.start();
                 Collections.sort(unsortedArtifacts);
-                stopwatch.stop();
-                System.out.println("Collections.sort:\t" + stopwatch.getElapsedTime());
 
                 // Get the column headers appropriate for the artifact type.
                 /* @@@ BC: Seems like a better design here would be to have a method that 
@@ -472,8 +463,6 @@ public class ReportGenerator {
                     }
                 }
                 
-                stopwatch.reset();
-                stopwatch.start();
                 boolean msgSent = false;    
                 for(ArtifactData artifactData : unsortedArtifacts) {
                     HashSet<String> tags = artifactData.getTags();
@@ -510,8 +499,6 @@ public class ReportGenerator {
                         }                        
                     }
                 }
-                stopwatch.stop();
-                System.out.println("Adding Data:\t" + stopwatch.getElapsedTime());
                 // Finish up this data type
                 for (TableReportModule module : tableModules) {
                     tableProgress.get(module).increment();
