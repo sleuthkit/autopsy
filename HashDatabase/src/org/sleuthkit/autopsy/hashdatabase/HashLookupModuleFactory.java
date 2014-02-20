e317129722e822333ca6b73c09131119034427fa
Richard Cordovano <rcordovano@basistech.com>
2014-02-20 12:30:16 -0500

    Complete method stubs for HashLookupModuleFactory

--- HashDatabase/src/org/sleuthkit/autopsy/hashdatabase/HashLookupModuleFactory.java
+++ HashDatabase/src/org/sleuthkit/autopsy/hashdatabase/HashLookupModuleFactory.java
@@ -19,31 +19,99 @@
 
 package org.sleuthkit.autopsy.hashdatabase;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+import org.openide.util.NbBundle;
 import org.openide.util.lookup.ServiceProvider;
 import org.sleuthkit.autopsy.coreutils.Version;
 import org.sleuthkit.autopsy.ingest.AbstractIngestModuleFactory;
+import org.sleuthkit.autopsy.ingest.FileIngestModule;
 import org.sleuthkit.autopsy.ingest.IngestModuleFactory;
 
 /**
  * An factory that creates file ingest modules that do hash database lookups.
  */
 @ServiceProvider(service=IngestModuleFactory.class)
-public class HashLookupModuleFactory extends AbstractIngestModuleFactory {
-    private final static String MODULE_NAME = "Hash Lookup";
-    private final static String MODULE_DESCRIPTION = "Identifies known and notables files using supplied hash databases, such as a standard NSRL database.";
-    
+public class HashLookupModuleFactory extends AbstractIngestModuleFactory { 
     @Override
     public String getModuleDisplayName() {
-        return MODULE_NAME;
+        return NbBundle.getMessage(HashDbIngestModule.class, "HashDbIngestModule.moduleName");
     }
     
     @Override
     public String getModuleDescription() {
-        return MODULE_DESCRIPTION;        
+        return NbBundle.getMessage(HashDbIngestModule.class, "HashDbIngestModule.moduleDescription");        
     }
     
     @Override
     public String getModuleVersionNumber() {
         return Version.getVersion();        
+    }
+    
+    @Override
+    public Serializable getDefaultIngestOptions() {
+        return new IngestOptions();        
+    }
+    
+    @Override
+    public boolean providesIngestOptionsPanels() {
+        return true;
+    }
+    
+    @Override
+    public JPanel getIngestOptionsPanel(Serializable ingestOptions) {
+        HashDbSimpleConfigPanel ingestOptionsPanel = new HashDbSimpleConfigPanel();  
+        ingestOptionsPanel.load();
+        return ingestOptionsPanel;                
+    }
+    
+    @Override
+    public Serializable getIngestOptionsFromPanel(JPanel ingestOptionsPanel) throws InvalidOptionsException {
+        if (!(ingestOptionsPanel instanceof HashDbSimpleConfigPanel)) {
+            throw new InvalidOptionsException(""); // RJCTODO
+        }
+
+        HashDbSimpleConfigPanel panel = (HashDbSimpleConfigPanel)ingestOptionsPanel;
+        panel.store();
+                
+        return new IngestOptions(); // RJCTODO
+    }
+    
+    @Override
+    public boolean providesGlobalOptionsPanels() {
+        return true;    
+    }
+    
+    @Override
+    public JPanel getGlobalOptionsPanel() {
+        HashDbConfigPanel globalOptionsPanel = new HashDbConfigPanel();        
+        globalOptionsPanel.load();
+        return globalOptionsPanel;
+    }    
+    
+    @Override
+    public void saveGlobalOptionsFromPanel(JPanel globalOptionsPanel) throws InvalidOptionsException {
+        if (!(globalOptionsPanel instanceof HashDbConfigPanel)) {
+            throw new InvalidOptionsException(""); // RJCTODO
+        }
+        
+        HashDbConfigPanel panel = (HashDbConfigPanel)globalOptionsPanel;
+        panel.store();
+    }
+    
+    @Override
+    public boolean isFileIngestModuleFactory() {
+        return true;            
+    }
+    
+    @Override
+    public FileIngestModule createFileIngestModule(Serializable ingestOptions) throws InvalidOptionsException {
+        return null; // RJCTODO
+    }
+    
+    private static class IngestOptions implements Serializable {
+        boolean alwaysCalcHashes = true;
+        ArrayList<String> hashSetNames = new ArrayList<>();
     }
 }
