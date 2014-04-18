f6524b1c4a1167e38338e879f316847998a40b54
Samuel H. Kenyon <skenyon@basistech.com>
2014-04-18 15:13:17 -0400

    updated KeywordSearchIngestModule to keep track of ingest status per job, not all jobs combined together

--- KeywordSearch/src/org/sleuthkit/autopsy/keywordsearch/KeywordSearchIngestModule.java
+++ KeywordSearch/src/org/sleuthkit/autopsy/keywordsearch/KeywordSearchIngestModule.java
@@ -103,11 +103,22 @@ public final class KeywordSearchIngestModule extends IngestModuleAdapter impleme
         SKIPPED_ERROR_TEXTEXTRACT, ///< File was skipped because of text extraction issues
         SKIPPED_ERROR_IO    ///< File was skipped because of IO issues reading it
     };
-    private static final Map<Long, IngestStatus> ingestStatus = new HashMap<>(); //guarded by itself
+    private static final Map<Long, Map<Long, IngestStatus>> ingestStatus = new HashMap<>(); //guarded by itself
 
-    static void putIngestStatus(long id, IngestStatus status) {
-        synchronized(ingestStatus) {
-            ingestStatus.put(id, status);
+    private static void initIngestStatus(long ingestJobId) {
+        synchronized(ingestStatus) {                   
+            if (ingestStatus.get(ingestJobId) == null) {
+               Map<Long, IngestStatus> ingestStatusForJob = new HashMap<>();
+               ingestStatus.put(ingestJobId, ingestStatusForJob);
+            }
+        }
+    }    
+    
+    private static void putIngestStatus(long ingestJobId, long fileId, IngestStatus status) {
+        synchronized(ingestStatus) {            
+            Map<Long, IngestStatus> ingestStatusForJob = ingestStatus.get(ingestJobId);           
+            ingestStatusForJob.put(fileId, status);
+            ingestStatus.put(ingestJobId, ingestStatusForJob);
         }
     }    
     
@@ -201,7 +212,7 @@ public final class KeywordSearchIngestModule extends IngestModuleAdapter impleme
         if (initialized == false) //error initializing indexing/Solr
         {
             logger.log(Level.WARNING, "Skipping processing, module not initialized, file: {0}", abstractFile.getName());
-            putIngestStatus(abstractFile.getId(), IngestStatus.SKIPPED_ERROR_INDEXING);
+            putIngestStatus(jobId, abstractFile.getId(), IngestStatus.SKIPPED_ERROR_INDEXING);
             return ProcessResult.OK;
         }
         try {
@@ -314,16 +325,17 @@ public final class KeywordSearchIngestModule extends IngestModuleAdapter impleme
         int error_io = 0;
 
         synchronized(ingestStatus) {
-            for (IngestStatus s : ingestStatus.values()) {
+            Map<Long, IngestStatus> ingestStatusForJob = ingestStatus.get(jobId);
+            for (IngestStatus s : ingestStatusForJob.values()) {
                 switch (s) {
                     case TEXT_INGESTED:
-                        ++text_ingested;
+                        text_ingested++;
                         break;
                     case METADATA_INGESTED:
-                        ++metadata_ingested;
+                        metadata_ingested++;
                         break;
                     case STRINGS_INGESTED:
-                        ++strings_ingested;
+                        strings_ingested++;
                         break;
                     case SKIPPED_ERROR_TEXTEXTRACT:
                         error_text++;
@@ -411,16 +423,16 @@ public final class KeywordSearchIngestModule extends IngestModuleAdapter impleme
         private boolean extractStringsAndIndex(AbstractFile aFile) {
             try {
                 if (stringExtractor.index(aFile)) {
-                    putIngestStatus(aFile.getId(), IngestStatus.STRINGS_INGESTED);
+                    putIngestStatus(jobId, aFile.getId(), IngestStatus.STRINGS_INGESTED);
                     return true;
                 } else {
                     logger.log(Level.WARNING, "Failed to extract strings and ingest, file ''{0}'' (id: {1}).", new Object[]{aFile.getName(), aFile.getId()});
-                    putIngestStatus(aFile.getId(), IngestStatus.SKIPPED_ERROR_TEXTEXTRACT);
+                    putIngestStatus(jobId, aFile.getId(), IngestStatus.SKIPPED_ERROR_TEXTEXTRACT);
                     return false;
                 }
             } catch (IngesterException ex) {
                 logger.log(Level.WARNING, "Failed to extract strings and ingest, file '" + aFile.getName() + "' (id: " + aFile.getId() + ").", ex);
-                putIngestStatus(aFile.getId(), IngestStatus.SKIPPED_ERROR_INDEXING);
+                putIngestStatus(jobId, aFile.getId(), IngestStatus.SKIPPED_ERROR_INDEXING);
                 return false;
             }
         }
@@ -466,9 +478,9 @@ public final class KeywordSearchIngestModule extends IngestModuleAdapter impleme
             if ((indexContent == false || aFile.isDir() || size == 0)) {
                 try {
                     ingester.ingest(aFile, false); //meta-data only
-                    putIngestStatus(aFile.getId(), IngestStatus.METADATA_INGESTED);
+                    putIngestStatus(jobId, aFile.getId(), IngestStatus.METADATA_INGESTED);
                 } catch (IngesterException ex) {
-                    putIngestStatus(aFile.getId(), IngestStatus.SKIPPED_ERROR_INDEXING);
+                    putIngestStatus(jobId, aFile.getId(), IngestStatus.SKIPPED_ERROR_INDEXING);
                     logger.log(Level.WARNING, "Unable to index meta-data for file: " + aFile.getId(), ex);
                 }
                 return;
@@ -502,9 +514,9 @@ public final class KeywordSearchIngestModule extends IngestModuleAdapter impleme
             if (AbstractFileExtract.ARCHIVE_MIME_TYPES.contains(detectedFormat)) {
                 try {
                     ingester.ingest(aFile, false); //meta-data only
-                    putIngestStatus(aFile.getId(), IngestStatus.METADATA_INGESTED);
+                    putIngestStatus(jobId, aFile.getId(), IngestStatus.METADATA_INGESTED);
                 } catch (IngesterException ex) {
-                    putIngestStatus(aFile.getId(), IngestStatus.SKIPPED_ERROR_INDEXING);
+                    putIngestStatus(jobId, aFile.getId(), IngestStatus.SKIPPED_ERROR_INDEXING);
                     logger.log(Level.WARNING, "Unable to index meta-data for file: " + aFile.getId(), ex);
                 }
                 return;
@@ -517,20 +529,20 @@ public final class KeywordSearchIngestModule extends IngestModuleAdapter impleme
                     //logger.log(Level.INFO, "indexing: " + aFile.getName());
                     if (!extractTextAndIndex(aFile, detectedFormat)) {
                         logger.log(Level.WARNING, "Failed to extract text and ingest, file ''{0}'' (id: {1}).", new Object[]{aFile.getName(), aFile.getId()});
-                        putIngestStatus(aFile.getId(), IngestStatus.SKIPPED_ERROR_TEXTEXTRACT);
+                        putIngestStatus(jobId, aFile.getId(), IngestStatus.SKIPPED_ERROR_TEXTEXTRACT);
                     } else {
-                        putIngestStatus(aFile.getId(), IngestStatus.TEXT_INGESTED);
+                        putIngestStatus(jobId, aFile.getId(), IngestStatus.TEXT_INGESTED);
                         wasTextAdded = true;
                     }
 
                 } catch (IngesterException e) {
                     logger.log(Level.INFO, "Could not extract text with Tika, " + aFile.getId() + ", "
                             + aFile.getName(), e);
-                    putIngestStatus(aFile.getId(), IngestStatus.SKIPPED_ERROR_INDEXING);
+                    putIngestStatus(jobId, aFile.getId(), IngestStatus.SKIPPED_ERROR_INDEXING);
                 } catch (Exception e) {
                     logger.log(Level.WARNING, "Error extracting text with Tika, " + aFile.getId() + ", "
                             + aFile.getName(), e);
-                    putIngestStatus(aFile.getId(), IngestStatus.SKIPPED_ERROR_TEXTEXTRACT);
+                    putIngestStatus(jobId, aFile.getId(), IngestStatus.SKIPPED_ERROR_TEXTEXTRACT);
                 }
             }
 
