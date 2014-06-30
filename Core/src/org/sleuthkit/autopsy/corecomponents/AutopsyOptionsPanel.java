23faee9c1a0cab0a3ea5fc09faf149bde04b99b0
Richard Cordovano <rcordovano@basistech.com>
2014-06-30 16:58:47 -0400

    Corrected max file ingest threads option calculation

--- Core/src/org/sleuthkit/autopsy/corecomponents/AutopsyOptionsPanel.java
+++ Core/src/org/sleuthkit/autopsy/corecomponents/AutopsyOptionsPanel.java
@@ -30,7 +30,8 @@ final class AutopsyOptionsPanel extends javax.swing.JPanel {
     AutopsyOptionsPanel(AutopsyOptionsPanelController controller) {
         initComponents();
         numberOfFileIngestThreadsComboBox.setModel(new DefaultComboBoxModel<>(new Integer[]{1, 2, 4, 6, 8, 12, 16}));
-        restartRequiredLabel.setText(NbBundle.getMessage(AutopsyOptionsPanel.class, "AutopsyOptionsPanel.restartRequiredLabel.text", Runtime.getRuntime().availableProcessors() - 2));
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        restartRequiredLabel.setText(NbBundle.getMessage(AutopsyOptionsPanel.class, "AutopsyOptionsPanel.restartRequiredLabel.text", availableProcessors > 2 ? availableProcessors - 2 : 1));
         // TODO listen to changes in form fields and call controller.changed()
     }
 
