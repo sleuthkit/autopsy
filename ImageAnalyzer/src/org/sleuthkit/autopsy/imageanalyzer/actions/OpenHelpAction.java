b47cc16d88f083e8e5090f02f0b502b9c4d352b1
jmillman <jmillman@basistech.com>
2014-08-15 12:20:21 -0400

    correct url for help

--- ImageAnalyzer/src/org/sleuthkit/autopsy/imageanalyzer/actions/OpenHelpAction.java
+++ ImageAnalyzer/src/org/sleuthkit/autopsy/imageanalyzer/actions/OpenHelpAction.java
@@ -43,7 +43,7 @@ public final class OpenHelpAction implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         try {
-            Desktop.getDesktop().browse(URI.create("http://www.google.com"));
+            Desktop.getDesktop().browse(URI.create("http://sleuthkit.org/autopsy/docs/user-docs/"));
         } catch (IOException ex) {
             Exceptions.printStackTrace(ex);
         }
