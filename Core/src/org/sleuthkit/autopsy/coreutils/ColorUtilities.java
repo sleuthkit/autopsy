3d2631ffef01c8003a3ed21176000dedb2072aa2
jmillman <jmillman@basistech.com>
2014-08-28 15:10:45 -0400

    prevent instantiation of ColorUtilities

--- Core/src/org/sleuthkit/autopsy/coreutils/ColorUtilities.java
+++ Core/src/org/sleuthkit/autopsy/coreutils/ColorUtilities.java
@@ -25,12 +25,14 @@ import javafx.scene.paint.Color;
  */
 public class ColorUtilities {
 
-   
+    private ColorUtilities() {
+    }
+
     public static String getRGBCode(Color color) {
         return String.format("#%02X%02X%02X%02X",
-                             (int) (color.getRed() * 255),
-                             (int) (color.getGreen() * 255),
-                             (int) (color.getBlue() * 255),
-                             (int) (color.getOpacity() * 255));
+                (int) (color.getRed() * 255),
+                (int) (color.getGreen() * 255),
+                (int) (color.getBlue() * 255),
+                (int) (color.getOpacity() * 255));
     }
 }
