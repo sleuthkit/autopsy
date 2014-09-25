7217907150d854ed10734d7b2259efeee4da05ef
rcordovano <rcordovano@dell-w7-richard.basistech.net>
2014-09-25 12:55:35 -0400

    Add look up IngestModuleFactoryAdapter instances to IngestModuleFactoryLoader

--- Core/src/org/sleuthkit/autopsy/ingest/IngestModuleFactoryLoader.java
+++ Core/src/org/sleuthkit/autopsy/ingest/IngestModuleFactoryLoader.java
@@ -46,9 +46,12 @@ import org.sleuthkit.autopsy.python.JythonModuleLoader;
 final class IngestModuleFactoryLoader {
 
     private static final Logger logger = Logger.getLogger(IngestModuleFactoryLoader.class.getName());
+    private static final String SAMPLE_MODULE_FACTORY_CLASS_NAME = SampleIngestModuleFactory.class.getCanonicalName();
+    private static final String SAMPLE_EXECUTABLE_MODULE_FACTORY_CLASS_NAME = SampleExecutableIngestModuleFactory.class.getCanonicalName();
     private static final ArrayList<String> coreModuleOrdering = new ArrayList<String>() {
         {
-            // The ordering of Core module factories is hard-coded. 
+            // The ordering of the core ingest module factories implemented
+            // using Java is hard-coded. 
             add("org.sleuthkit.autopsy.recentactivity.RecentActivityExtracterModuleFactory"); //NON-NLS
             add(HashLookupModuleFactory.class.getCanonicalName());
             add(FileTypeIdModuleFactory.class.getCanonicalName());
@@ -64,37 +67,36 @@ final class IngestModuleFactoryLoader {
     };
 
     /**
-     * Get the currently available set of ingest module factories. The factories
-     * are not cached between calls since NetBeans modules with classes labeled
-     * as IngestModuleFactory service providers and/or Python scripts defining
-     * classes derived from IngestModuleFactory may be added or removed between
-     * invocations.
+     * Gets the currently available set of ingest module factories. The
+     * factories are not cached between calls since NetBeans modules with
+     * classes annotated as IngestModuleFactory service providers and/or Python
+     * scripts defining classes derived from IngestModuleFactory may be added or
+     * removed between invocations.
      *
      * @return A list of objects that implement the IngestModuleFactory
      * interface.
      */
     static List<IngestModuleFactory> getIngestModuleFactories() {
-        // Discover the ingest module factories implemented using Java, making a
-        // hash map of display names to discovered factories and a hash set of
-        // display names.
+        // A hash set of display names and a hash map of class names to 
+        // discovered factories are used to de-duplicate and order the 
+        // factories.
         HashSet<String> moduleDisplayNames = new HashSet<>();
         HashMap<String, IngestModuleFactory> javaFactoriesByClass = new HashMap<>();
+
+        // Discover the ingest module factories implemented using Java with a
+        // service provider annotation for the IngestModuleFactory interface.
         for (IngestModuleFactory factory : Lookup.getDefault().lookupAll(IngestModuleFactory.class)) {
-            if (!moduleDisplayNames.contains(factory.getModuleDisplayName())) {
-                moduleDisplayNames.add(factory.getModuleDisplayName());
-                javaFactoriesByClass.put(factory.getClass().getCanonicalName(), factory);
-                logger.log(Level.INFO, "Found ingest module factory: name = {0}, version = {1}", new Object[]{factory.getModuleDisplayName(), factory.getModuleVersionNumber()}); //NON-NLS
-            } else {
-                logger.log(Level.SEVERE, "Found duplicate ingest module display name (name = {0})", factory.getModuleDisplayName()); //NON-NLS
-                DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(
-                        NbBundle.getMessage(IngestModuleFactoryLoader.class, "IngestModuleFactoryLoader.errorMessages.duplicateDisplayName", factory.getModuleDisplayName()),
-                        NotifyDescriptor.ERROR_MESSAGE));
-            }
+            IngestModuleFactoryLoader.addFactory(factory, moduleDisplayNames, javaFactoriesByClass);
         }
 
-        // Kick out the sample ingest module factories implemented in Java.
-        javaFactoriesByClass.remove(SampleIngestModuleFactory.class.getCanonicalName());
-        javaFactoriesByClass.remove(SampleExecutableIngestModuleFactory.class.getCanonicalName());
+        // Discover the ingest module factories implemented using Java with a
+        // service provider annotation for the IngestModuleFactoryAdapter 
+        // abstract base class.
+        for (IngestModuleFactory factory : Lookup.getDefault().lookupAll(IngestModuleFactoryAdapter.class)) {
+            if (!javaFactoriesByClass.containsValue(factory)) {
+                IngestModuleFactoryLoader.addFactory(factory, moduleDisplayNames, javaFactoriesByClass);
+            }
+        }
 
         // Add the core ingest module factories in the desired order, removing
         // the core factories from the map so that the map will only contain 
@@ -130,4 +132,25 @@ final class IngestModuleFactoryLoader {
 
         return factories;
     }
+
+    private static void addFactory(IngestModuleFactory factory, HashSet<String> moduleDisplayNames, HashMap<String, IngestModuleFactory> javaFactoriesByClass) {
+        // Ignore the sample ingest module factories implemented in Java.        
+        String className = factory.getClass().getCanonicalName();
+        if (className.equals(IngestModuleFactoryLoader.SAMPLE_MODULE_FACTORY_CLASS_NAME)
+                || className.equals(IngestModuleFactoryLoader.SAMPLE_EXECUTABLE_MODULE_FACTORY_CLASS_NAME)) {
+            return;
+        }
+
+        if (!moduleDisplayNames.contains(factory.getModuleDisplayName())) {
+            moduleDisplayNames.add(factory.getModuleDisplayName());
+            javaFactoriesByClass.put(factory.getClass().getCanonicalName(), factory);
+            logger.log(Level.INFO, "Found ingest module factory: name = {0}, version = {1}", new Object[]{factory.getModuleDisplayName(), factory.getModuleVersionNumber()}); //NON-NLS
+        } else {
+            logger.log(Level.SEVERE, "Found duplicate ingest module display name (name = {0})", factory.getModuleDisplayName()); //NON-NLS
+            DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(
+                    NbBundle.getMessage(IngestModuleFactoryLoader.class, "IngestModuleFactoryLoader.errorMessages.duplicateDisplayName", factory.getModuleDisplayName()),
+                    NotifyDescriptor.ERROR_MESSAGE));
+        }
+    }
+
 }
