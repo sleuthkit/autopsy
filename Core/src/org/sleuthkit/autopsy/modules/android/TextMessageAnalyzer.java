a6ea3e529c76fff54b5535207ef3d1c27625ab42
Brian Carrier <carrier@sleuthkit.org>
2014-10-31 17:03:25 -0400

    made use of new getDisplayString in attribute, fixed report bug if attributes were not defined, made cell phone attributes more consistent

--- Core/src/org/sleuthkit/autopsy/modules/android/TextMessageAnalyzer.java
+++ Core/src/org/sleuthkit/autopsy/modules/android/TextMessageAnalyzer.java
@@ -89,19 +89,21 @@ class TextMessageAnalyzer {
             while (resultSet.next()) {
                 address = resultSet.getString("address");
                 Long date = Long.valueOf(resultSet.getString("date")) / 1000;
-                if (resultSet.getString("type").equals("1")) {
-                    direction = "Incoming";
-                } else {
-                    direction = "Outgoing";
-                }
+                
                 read = resultSet.getInt("read");
                 subject = resultSet.getString("subject");
                 body = resultSet.getString("body");
 
                 BlackboardArtifact bba = f.newArtifact(BlackboardArtifact.ARTIFACT_TYPE.TSK_MESSAGE); //create Message artifact and then add attributes from result set.
-                bba.addAttribute(new BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_PHONE_NUMBER.getTypeID(), moduleName, address));
+                if (resultSet.getString("type").equals("1")) {
+                    bba.addAttribute(new BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_DIRECTION.getTypeID(), moduleName, "Incoming"));
+                    bba.addAttribute(new BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_PHONE_NUMBER_FROM.getTypeID(), moduleName, address));
+                } else {
+                    bba.addAttribute(new BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_DIRECTION.getTypeID(), moduleName, "Outgoing"));
+                    bba.addAttribute(new BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_PHONE_NUMBER_TO.getTypeID(), moduleName, address));
+                }
                 bba.addAttribute(new BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_DATETIME.getTypeID(), moduleName, date));
-                bba.addAttribute(new BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_DIRECTION.getTypeID(), moduleName, direction));
+                
                 bba.addAttribute(new BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_READ_STATUS.getTypeID(), moduleName, read));
                 bba.addAttribute(new BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_SUBJECT.getTypeID(), moduleName, subject));
                 bba.addAttribute(new BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_TEXT.getTypeID(), moduleName, body));
