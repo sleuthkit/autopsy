502e8f4612765dd02ecae1c4dc6cb07f0ac69f73
Brian Carrier <carrier@sleuthkit.org>
2015-02-02 00:01:38 -0500

    minor cleanup to code that opens .aut file from double click

--- Core/src/org/sleuthkit/autopsy/casemodule/OpenFromArguments.java
+++ Core/src/org/sleuthkit/autopsy/casemodule/OpenFromArguments.java
@@ -1,41 +1,57 @@
 /*
-// * To change this template, choose Tools | Templates
-// * and open the template in the editor.
-// */
+ * Autopsy Forensic Browser
+ * 
+ * Copyright 2014 Basis Technology Corp.
+ * Contact: carrier <at> sleuthkit <dot> org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.sleuthkit.autopsy.casemodule;
 
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import org.netbeans.spi.sendopts.*;
+import org.netbeans.api.sendopts.CommandException;
+import org.netbeans.spi.sendopts.Env;
+import org.netbeans.spi.sendopts.Option;
+import org.netbeans.spi.sendopts.OptionProcessor;
 import org.openide.util.lookup.ServiceProvider;
 
-
+/**
+ * Allows Autopsy to get path to .aut file passed in via associating the file type in Windows. 
+ */
 @ServiceProvider(service = OptionProcessor.class)
-public class OpenFromArguments extends OptionProcessor
-{
+public class OpenFromArguments extends OptionProcessor {
 
-    private String path ="";
-    private Option option1=Option.defaultArguments();
- 
-  
+    /* Stores the .aut file if it was passed in as argument */
+    private String autPath = "";
+    private final Option option1 = Option.defaultArguments();
+
     @Override
-    protected Set getOptions()
-    {
-        Set set = new HashSet();
-        set.add(option1);
-        return set;
+    protected Set<Option> getOptions() {
+        final Set<Option> options = new HashSet<>();
+        options.add(option1);
+        return options;
     }
+
     @Override
-       public void process(Env env, Map<Option,String[]> values) {
-     if (values.containsKey(option1))
-     {
-         path=values.get(option1)[0];
-     }
-   }
-    public String getArg(){
-        return path;
+    protected void process(Env env, Map<Option, String[]> maps) throws CommandException {
+        if (maps.containsKey(option1)) {
+            autPath = maps.get(option1)[0];
+        }
+    }
+
+    public String getDefaultArg() {
+        return autPath;
     }
 }
-
-
