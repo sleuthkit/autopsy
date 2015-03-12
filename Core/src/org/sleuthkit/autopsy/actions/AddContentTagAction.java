30c36346045e9306659c061098869eb50c131e32
sidheshenator <sidhesh@basistech.com>
2015-03-12 17:32:07 -0400

    JOptionPanel.showMessageDialog used for consistency

--- Core/src/org/sleuthkit/autopsy/actions/AddContentTagAction.java
+++ Core/src/org/sleuthkit/autopsy/actions/AddContentTagAction.java
@@ -118,9 +118,13 @@ public class AddContentTagAction extends AddTagAction {
                 List<ContentTag> contentTagList = tagsManager.getContentTagsByContent(file);
                 for (ContentTag contentTag : contentTagList) {
                     if (contentTag.getName().getDisplayName().equals(tagName.getDisplayName())) {
-                        MessageNotifyUtil.Message.error(NbBundle.getMessage(this.getClass(),
-                                "AddContentTagAction.tagExists",
-                                file.getName(), tagName.getDisplayName()));
+                        JOptionPane.showMessageDialog(null,
+                                NbBundle.getMessage(this.getClass(),
+                                        "AddContentTagAction.tagExists",
+                                        file.getName(), tagName.getDisplayName()),
+                                NbBundle.getMessage(this.getClass(),
+                                        "AddContentTagAction.cannotApplyTagErr"),
+                                JOptionPane.WARNING_MESSAGE);
                         return;
                     }
                 }
