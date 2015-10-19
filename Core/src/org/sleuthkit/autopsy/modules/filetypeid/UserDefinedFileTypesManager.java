cdd2c76a058de16b9a8c774f9ff0cf340253b397
momo <mhmdfy@gmail.com>
2015-10-19 10:22:25 -0400

    comments

--- Core/src/org/sleuthkit/autopsy/modules/filetypeid/UserDefinedFileTypesManager.java
+++ Core/src/org/sleuthkit/autopsy/modules/filetypeid/UserDefinedFileTypesManager.java
@@ -235,6 +235,13 @@ final class UserDefinedFileTypesManager {
         addFileTypeToMap(fileTypes, fileType);
     }
 
+    /**
+     * Adds given FileType to given map:
+     * if the mimetype exists, add fileType to that mimetype's list.
+     * otherwise, create a new mimetype with the new fileType.
+     * @param map The map to be modified.
+     * @param fileType The added FileType
+     */
     void addFileTypeToMap(Map<String, List<FileType>> map, FileType fileType) {
         String mimeType = fileType.getMimeType();
         if (map.containsKey(mimeType)) {
@@ -246,6 +253,11 @@ final class UserDefinedFileTypesManager {
         }
     }
 
+    /**
+     * Removes given fileType from given map if it exists.
+     * @param map
+     * @param fileType 
+     */
     void removeFileTypeFromMap(Map<String, List<FileType>> map, FileType fileType) {
         String mimeType = fileType.getMimeType();
         if (map.containsKey(mimeType)) {
