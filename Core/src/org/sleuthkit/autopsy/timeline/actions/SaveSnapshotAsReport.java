de96de9c491b6181f0fdbf9a4037773a20a3bc52
millmanorama <millmanorama@gmail.com>
2015-11-10 15:07:09 -0500

    WIP

--- Core/src/org/sleuthkit/autopsy/timeline/actions/SaveSnapshotAsReport.java
+++ Core/src/org/sleuthkit/autopsy/timeline/actions/SaveSnapshotAsReport.java
@@ -1,7 +1,7 @@
 /*
  * Autopsy Forensic Browser
  *
- * Copyright 2014 Basis Technology Corp.
+ * Copyright 2014-15 Basis Technology Corp.
  * Contact: carrier <at> sleuthkit <dot> org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,21 +25,26 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.Writer;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.logging.Level;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
-import javafx.stage.DirectoryChooser;
 import javafx.util.Pair;
 import javax.imageio.ImageIO;
 import org.controlsfx.control.action.Action;
+import org.openide.util.Exceptions;
 import org.openide.util.NbBundle;
 import org.sleuthkit.autopsy.casemodule.Case;
 import org.sleuthkit.autopsy.coreutils.Logger;
@@ -57,7 +62,7 @@ public class SaveSnapshotAsReport extends Action {
 
     private static final Logger LOGGER = Logger.getLogger(SaveSnapshotAsReport.class.getName());
 
-    @NbBundle.Messages({"SaveSnapshot.action.name.text=Snapshot",
+    @NbBundle.Messages({"SaveSnapshot.action.name.text=Snapshot Report",
         "SaveSnapshot.action.longText=Save a screen capture of the visualization as a report.",
         "SaveSnapshot.fileChoose.title.text=Save snapshot to",})
     public SaveSnapshotAsReport(TimeLineController controller, Node node) {
@@ -69,17 +74,14 @@ public class SaveSnapshotAsReport extends Action {
             @Override
             public void accept(ActionEvent t) {
                 //choose location/name
-                DirectoryChooser fileChooser = new DirectoryChooser();
-                fileChooser.setTitle(Bundle.SaveSnapshot_fileChoose_title_text());
-                fileChooser.setInitialDirectory(new File(Case.getCurrentCase().getReportDirectory()));
-                File reportDirectory = fileChooser.showDialog(null);
-                if (reportDirectory == null) {
-                    return;
+                ZonedDateTime now = ZonedDateTime.now();
+                String reportName = "timeline-report-" + now.toString();
+                Path reportPath = Paths.get(Case.getCurrentCase().getReportDirectory(), reportName).toAbsolutePath();
+                try {
+                    Files.createDirectories(reportPath);
+                } catch (IOException ex) {
+                    Exceptions.printStackTrace(ex);
                 }
-                reportDirectory.mkdir();
-                String reportName = reportDirectory.getName();
-                String reportPath = reportDirectory.getPath();
-
                 //gather metadata
                 List<Pair<String, String>> reportMetaData = new ArrayList<>();
 
@@ -95,7 +97,7 @@ public class SaveSnapshotAsReport extends Action {
                 try {
                     WritableImage snapshot = node.snapshot(null, null);
                     ImageIO.write(SwingFXUtils.fromFXImage(snapshot, null), "png",
-                            new File(reportPath, reportName + REPORT_IMAGE_EXTENSION)); // NON-NLS
+                            reportPath.resolve(reportName + REPORT_IMAGE_EXTENSION).toFile()); // NON-NLS
                 } catch (IOException ex) {
                     LOGGER.log(Level.WARNING, "failed to write snapshot to disk", ex); // NON-NLS
                     return;
@@ -104,15 +106,15 @@ public class SaveSnapshotAsReport extends Action {
                 //build html string
                 StringBuilder wrapper = new StringBuilder();
                 wrapper.append("<html>\n<head>\n\t<title>").append("timeline snapshot").append("</title>\n\t<link rel=\"stylesheet\" type=\"text/css\" href=\"index.css\" />\n</head>\n<body>\n"); // NON-NLS
-                wrapper.append("<div id=\"content\">\n<h1>").append(reportDirectory.getName()).append("</h1>\n"); // NON-NLS
-                wrapper.append("<img src = \"").append(reportDirectory.getName()).append(REPORT_IMAGE_EXTENSION + "\" alt = \"snaphot\">"); // NON-NLS
+                wrapper.append("<div id=\"content\">\n<h1>").append(reportName).append("</h1>\n"); // NON-NLS
+                wrapper.append("<img src = \"").append(reportName).append(REPORT_IMAGE_EXTENSION + "\" alt = \"snaphot\">"); // NON-NLS
                 wrapper.append("<table>\n"); // NON-NLS
                 for (Pair<String, String> pair : reportMetaData) {
                     wrapper.append("<tr><td>").append(pair.getKey()).append(": </td><td>").append(pair.getValue()).append("</td></tr>\n"); // NON-NLS
                 }
                 wrapper.append("</table>\n"); // NON-NLS
                 wrapper.append("</div>\n</body>\n</html>"); // NON-NLS
-                File reportHTMLFIle = new File(reportDirectory, reportName + HTML_EXT);
+                File reportHTMLFIle = reportPath.resolve(reportName + HTML_EXT).toFile();
 
                 //write html wrapper
                 try (Writer htmlWriter = new FileWriter(reportHTMLFIle)) {
@@ -127,7 +129,7 @@ public class SaveSnapshotAsReport extends Action {
 
                 //copy css
                 try (InputStream resource = this.getClass().getResourceAsStream("/org/sleuthkit/autopsy/timeline/index.css")) { // NON-NLS
-                    Files.copy(resource, Paths.get(reportPath, "index.css")); // NON-NLS
+                    Files.copy(resource, reportPath.resolve("index.css")); // NON-NLS
                 } catch (IOException ex) {
                     LOGGER.log(Level.WARNING, "failed to copy css file", ex); // NON-NLS
                 }
@@ -138,6 +140,24 @@ public class SaveSnapshotAsReport extends Action {
                 } catch (TskCoreException ex) {
                     LOGGER.log(Level.WARNING, "failed add html wrapper as a report", ex); // NON-NLS
                 }
+
+                ButtonType show = new ButtonType("Show in Autopsy");
+                ButtonType open = new ButtonType("Open");
+
+                Optional<ButtonType> showAndWait = new Alert(Alert.AlertType.INFORMATION, "Snapshot saved at " + reportPath, open, show, ButtonType.OK).showAndWait();
+
+                showAndWait.ifPresent((ButtonType t1) -> {
+                    if (t1 == show) {
+                        //do something
+                        System.out.println("show");
+                    } else if (t1 == open) {
+                        //do something
+                        System.out.println("open");
+                    } else {
+                        //do something
+                        System.out.println("nothing");
+                    }
+                });
             }
         });
     }
