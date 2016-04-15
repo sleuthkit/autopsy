0aee9411793cec4df8ef81f74570d9d1dec48781
Mathias Vetsch <m.vetsch@gmail.com>
2016-04-15 15:34:02 +0200

    combine ClamAV files to combined files

--- Core/src/org/sleuthkit/autopsy/modules/hashdatabase/ClamavHashSetPreparer.java
+++ Core/src/org/sleuthkit/autopsy/modules/hashdatabase/ClamavHashSetPreparer.java
@@ -1,9 +1,13 @@
 package org.sleuthkit.autopsy.modules.hashdatabase;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.LinkedList;
 import java.util.List;
 import javax.swing.JProgressBar;
@@ -15,8 +19,11 @@ import org.python.apache.commons.compress.compressors.gzip.GzipCompressorInputSt
 @ServiceProvider(service = HashSetPreparer.class)
 public class ClamavHashSetPreparer extends HashSetPreparer {
 
+    private static final String OUTPUT_FILE_NAME = "ClamAV-combined.md5";
+
     private static final String[] SOURCES = {"http://database.clamav.net/main.cvd", "http://database.clamav.net/daily.cvd", "http://database.clamav.net/bytecode.cvd"};
     private List<String> downloadedFiles = new LinkedList<>();
+    private List<String> extractedFiles = new LinkedList<>();
     private String outputDirectory;
     private JProgressBar progressbar;
 
@@ -57,19 +64,26 @@ public class ClamavHashSetPreparer extends HashSetPreparer {
 
     @Override
     public void extract() throws HashSetUpdateException {
+        extractMdbFilesFromDownloadedArchives();
+        combineMdbFiles();
+    }
+
+    private void extractMdbFilesFromDownloadedArchives() throws HashSetUpdateException {
         for (String fileName : downloadedFiles) {
             try {
-                TarArchiveInputStream a = new TarArchiveInputStream(new GzipCompressorInputStream(new FileInputStream(fileName)));
-                TarArchiveEntry x;
-                while ((x = a.getNextTarEntry()) != null) {
-                    if (x.getName().substring(x.getName().lastIndexOf('.')+1).equals("mdb")) {
-                        OutputStream o = new FileOutputStream(new File(getDirectory() + x.getName()));
-                        copyStreams(a, o);
+                TarArchiveInputStream tarArchiveStream = new TarArchiveInputStream(new GzipCompressorInputStream(new FileInputStream(fileName)));
+                TarArchiveEntry tarArchiveEntry;
+                while ((tarArchiveEntry = tarArchiveStream.getNextTarEntry()) != null) {
+                    if (tarArchiveEntry.getName().substring(tarArchiveEntry.getName().lastIndexOf('.') + 1).equals("mdb")) {
+                        String extractedFileName = getDirectory() + tarArchiveEntry.getName();
+                        OutputStream extractedFile = new FileOutputStream(new File(extractedFileName));
+                        copyStreams(tarArchiveStream, extractedFile);
+                        extractedFiles.add(extractedFileName);
                     }
 
                 }
             } catch (Exception ex) {
-                throw new HashSetUpdateException("YEY");
+                throw new HashSetUpdateException("Error while extracting ClamAV HashSets");
             }
         }
     }
@@ -82,5 +96,41 @@ public class ClamavHashSetPreparer extends HashSetPreparer {
     @Override
     public boolean newVersionAvailable() {
         return true;
+    }
+
+    private void combineMdbFiles() throws HashSetUpdateException {
+        progressbar.setMaximum(extractedFiles.size());
+        int counter = 0;
+
+        try {
+            PrintWriter targetFile = new PrintWriter(new File(getDirectory() + OUTPUT_FILE_NAME));
+
+            for (String fileName : extractedFiles) {
+                writeValidMd5SumsToTargetFile(fileName, targetFile);
+                progressbar.setValue(counter++);
+            }
+
+        } catch (IOException ex) {
+            throw new HashSetUpdateException("Error while combining ClamAV parts");
+        }
+        extractedFile = getDirectory() + OUTPUT_FILE_NAME;
+    }
+
+    private void writeValidMd5SumsToTargetFile(String fileName, PrintWriter targetFile) throws IOException {
+        int md5lenth = 32;
+        BufferedReader inputStream;
+        inputStream = new BufferedReader(new InputStreamReader(new FileInputStream(new File(fileName))));
+        String line;
+        while ((line = inputStream.readLine()) != null) {
+            if (line.matches("[0-9]*:[a-f0-9]{32}:(.*)")) {
+                int md5start = line.indexOf(':') + 1;
+                targetFile.println(line.substring(md5start, md5start + md5lenth));
+            }
+        }
+    }
+
+    @Override
+    public HashDbManager.HashDb.KnownFilesType getHashSetType() {
+        return HashDbManager.HashDb.KnownFilesType.KNOWN_BAD;
     }
 }
