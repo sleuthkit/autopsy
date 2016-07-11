bbfa4689e8e9e72443e216fa914a1c4b9dd967bd
Richard Cordovano <rcordovano@basistech.com>
2016-07-11 13:23:56 -0400

    Add timeout to regression test to allow for setup of ingest job settings panel

--- Testing/test/qa-functional/src/org/sleuthkit/autopsy/testing/RegressionTest.java
+++ Testing/test/qa-functional/src/org/sleuthkit/autopsy/testing/RegressionTest.java
@@ -1,15 +1,15 @@
 /*
  * Autopsy Forensic Browser
- * 
+ *
  * Copyright 2011-2014 Basis Technology Corp.
  * Contact: carrier <at> sleuthkit <dot> org
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -215,6 +215,12 @@ public class RegressionTest extends TestCase {
     }
 
     public void testConfigureIngest1() {
+        /*
+         * This timeout is to allow the setup for the ingest job settings panel
+         * to complete.
+         */
+        new Timeout("pausing", 10000).sleep();
+
         logger.info("Looking for hash lookup module in ingest job settings panel");
         WizardOperator wo = new WizardOperator("Add Data");
         JTableOperator jto = new JTableOperator(wo, 0);
