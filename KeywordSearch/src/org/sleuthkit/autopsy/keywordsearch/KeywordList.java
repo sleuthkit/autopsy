56786863bb7053650b1158bf8198d36681c2db7e
Richard Cordovano <rcordovano@basistech.com>
2016-10-22 15:19:27 -0400

    Remove incorrect but unused equals and hashCode methods from KeywordList

--- KeywordSearch/src/org/sleuthkit/autopsy/keywordsearch/KeywordList.java
+++ KeywordSearch/src/org/sleuthkit/autopsy/keywordsearch/KeywordList.java
@@ -209,25 +209,4 @@ public class KeywordList {
         return isEditable;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final KeywordList other = (KeywordList) obj;
-        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        return hash;
-    }
-
 }
