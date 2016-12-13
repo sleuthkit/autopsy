2711788582e5a04763e353813b419717a0716597
U-BASIS\zhaohui <zhaohui@win-cray-4678.basistech.net>
2016-12-13 17:42:02 -0500

    2123: correction

--- KeywordSearch/src/org/sleuthkit/autopsy/keywordsearch/LuceneQuery.java
+++ KeywordSearch/src/org/sleuthkit/autopsy/keywordsearch/LuceneQuery.java
@@ -227,8 +227,6 @@ class LuceneQuery implements KeywordSearchQuery {
                 //no case open, must be just closed
                 return matches;
             }
-            
-            
             for (SolrDocument resultDoc : resultList) {
                 KeywordHit contentHit;
                 try {
@@ -265,7 +263,7 @@ class LuceneQuery implements KeywordSearchQuery {
         q.setRows(MAX_RESULTS);
 
         q.setFields(Server.Schema.ID.toString());
-        //q.addSort(Server.Schema.ID.toString(), SolrQuery.ORDER.asc);
+        q.addSort(Server.Schema.ID.toString(), SolrQuery.ORDER.asc);
         for (KeywordQueryFilter filter : filters) {
             q.addFilterQuery(filter.toString());
         }
