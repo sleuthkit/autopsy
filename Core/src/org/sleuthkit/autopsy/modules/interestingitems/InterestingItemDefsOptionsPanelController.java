f052539e21e56c509585830a8f546d16716963e0
William Schaefer <wschaefer@basistech.net>
2017-01-11 13:32:00 -0500

    2197-renumbered Option panel position to account for new panel

--- Core/src/org/sleuthkit/autopsy/modules/interestingitems/InterestingItemDefsOptionsPanelController.java
+++ Core/src/org/sleuthkit/autopsy/modules/interestingitems/InterestingItemDefsOptionsPanelController.java
@@ -32,7 +32,7 @@ import org.openide.util.Lookup;
         iconBase = "org/sleuthkit/autopsy/images/interesting_item_32x32.png",
         keywords = "#OptionsCategory_Keywords_InterestingItemDefinitions",
         keywordsCategory = "InterestingItemDefinitions",
-        position = 7
+        position = 9
 )
 public final class InterestingItemDefsOptionsPanelController extends OptionsPanelController {
 
