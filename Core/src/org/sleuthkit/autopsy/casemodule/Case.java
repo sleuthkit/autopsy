1baf51682818668867d6f53ab7cbaa13010a8872
Richard Cordovano <rcordovano@basistech.com>
2017-01-11 12:22:33 -0500

    Temporarily prevent SolrSearchService from executing openCaseResources

--- Core/src/org/sleuthkit/autopsy/casemodule/Case.java
+++ Core/src/org/sleuthkit/autopsy/casemodule/Case.java
@@ -1320,18 +1320,20 @@ public class Case implements SleuthkitCase.ErrorObserver {
                     MessageNotifyUtil.Notify.error(NbBundle.getMessage(Case.class, "Case.CollaborationSetup.FailNotify.Title"), NbBundle.getMessage(Case.class, "Case.CollaborationSetup.FailNotify.ErrMsg"));
                 }
             }
-            
+
             AutopsyService.CaseContext context = new AutopsyService.CaseContext(Case.currentCase, new LoggingProgressIndicator());
             String serviceName = "";
             for (AutopsyService service : Lookup.getDefault().lookupAll(AutopsyService.class)) {
                 try {
                     serviceName = service.getServiceName();
-                    service.openCaseResources(context);
+                    if (!serviceName.equals("Solr Keyword Search Service")) {
+                        service.openCaseResources(context);
+                    }
                 } catch (AutopsyService.AutopsyServiceException ex) {
                     Case.logger.log(Level.SEVERE, String.format("%s service failed to open case resources", serviceName), ex);
                 }
-            }           
-            
+            }
+
             eventPublisher.publishLocally(new AutopsyEvent(Events.CURRENT_CASE.toString(), null, currentCase));
 
         } else {
