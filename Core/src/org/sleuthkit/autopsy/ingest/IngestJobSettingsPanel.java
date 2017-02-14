2e28f72d85f859c52d710c36c2f640c39e99045f
William Schaefer <wschaefer@basistech.net>
2017-02-14 17:07:37 -0500

    2198 removed netbeans warning regarding method which could be overridden

--- Core/src/org/sleuthkit/autopsy/ingest/IngestJobSettingsPanel.java
+++ Core/src/org/sleuthkit/autopsy/ingest/IngestJobSettingsPanel.java
@@ -60,6 +60,8 @@ import org.sleuthkit.autopsy.modules.interestingitems.FilesSetsManager;
  */
 public final class IngestJobSettingsPanel extends javax.swing.JPanel {
 
+    @Messages("IngestJobSettingsPanel.name.text=Configure Ingest Modules")
+
     private static final long serialVersionUID = 1L;
     private static ImageIcon warningIcon = new ImageIcon(IngestJobSettingsPanel.class.getResource("/org/sleuthkit/autopsy/images/warning_triangle.png"));
     private static ImageIcon infoIcon = new ImageIcon(IngestJobSettingsPanel.class.getResource("/org/sleuthkit/autopsy/images/information-frame.png"));
@@ -84,7 +86,6 @@ public final class IngestJobSettingsPanel extends javax.swing.JPanel {
         initComponents();
         customizeComponents();
         fileIngestFilterComboBox.setSelectedItem(settings.getFileIngestFilter().getName());
-        this.setName("Configure Ingest Modules");
     }
 
     /**
@@ -184,6 +185,7 @@ public final class IngestJobSettingsPanel extends javax.swing.JPanel {
          * and jobs for which to display the history.
          */
         pastJobsButton.setEnabled(!dataSources.isEmpty() && !ingestJobs.isEmpty());
+        this.setName(Bundle.IngestJobSettingsPanel_name_text());
     }
 
     void setPastJobsButtonVisible(boolean isVisible) {
