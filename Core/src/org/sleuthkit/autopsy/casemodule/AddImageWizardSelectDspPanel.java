9ef1a9a06aaed91e796ff8a418129f3953f56912
William Schaefer <wschaefer@basistech.net>
2017-03-21 16:06:31 -0400

    2451-Title of Data Source choosing panel updated and spacer added

--- Core/src/org/sleuthkit/autopsy/casemodule/AddImageWizardSelectDspPanel.java
+++ Core/src/org/sleuthkit/autopsy/casemodule/AddImageWizardSelectDspPanel.java
@@ -38,12 +38,12 @@ import org.sleuthkit.autopsy.ingest.runIngestModuleWizard.ShortcutWizardDescript
  */
 final class AddImageWizardSelectDspPanel extends ShortcutWizardDescriptorPanel implements PropertyChangeListener {
 
-    @NbBundle.Messages("SelectDataSourceProcessorPanel.name.text=Select Type of Data")
+    @NbBundle.Messages("SelectDataSourceProcessorPanel.name.text=Select Type of Data Source To Add")
     private AddImageWizardSelectDspVisual component;
     private static final String LAST_DSP_PROPERTIES_FILE = "LastDspUsed"; //NON-NLS
     private static final String LAST_DSP_USED_KEY = "Last_Dsp_Used"; //NON-NLS
     private static final Logger logger = Logger.getLogger(AddImageWizardSelectDspVisual.class.getName());
- 
+
     @Override
     public Component getComponent() {
         if (component == null) {
