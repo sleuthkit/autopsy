fe9513f2f98466d0436005faa946af4000bd9ef6
Richard Cordovano <rcordovano@basistech.com>
2017-04-12 13:51:51 -0400

    Move case name lock back into AIM

--- Core/src/org/sleuthkit/autopsy/casemodule/Case.java
+++ Core/src/org/sleuthkit/autopsy/casemodule/Case.java
@@ -778,8 +778,8 @@ public class Case {
      *
      * @param caseName The case name.
      *
-     * @return The case name transformed into a corresponding PostgreSQL
-     *         case database name.
+     * @return The case name transformed into a corresponding PostgreSQL case
+     *         database name.
      */
     private static String caseNameToCaseDbName(String caseName) throws IllegalCaseNameException {
         /*
@@ -958,31 +958,6 @@ public class Case {
             SwingUtilities.invokeLater(() -> {
                 RecentCases.getInstance().removeRecentCase(metadata.getCaseDisplayName(), metadata.getFilePath().toString());
             });
-        }
-    }
-
-    /**
-     * Acquires an exclusive case name lock.
-     *
-     * @param caseName The case name (not the case display name, which can be
-     *                 changed by a user).
-     *
-     * @return The lock.
-     *
-     * @throws CaseActionException with a user-friendly message if the lock
-     *                             cannot be acquired.
-     */
-    @Messages({"Case.creationException.couldNotAcquireNameLock=Failed to get lock on case name"})
-    private static CoordinationService.Lock acquireExclusiveCaseNameLock(String caseName) throws CaseActionException {
-        try {
-            Lock lock = CoordinationService.getInstance().tryGetExclusiveLock(CategoryNode.CASES, caseName, NAME_LOCK_TIMOUT_HOURS, TimeUnit.HOURS);
-            if (null == lock) {
-                throw new CaseActionException(Bundle.Case_creationException_couldNotAcquireNameLock());
-            }
-            return lock;
-
-        } catch (InterruptedException | CoordinationServiceException ex) {
-            throw new CaseActionException(Bundle.Case_creationException_couldNotAcquireNameLock(), ex);
         }
     }
 
@@ -1679,37 +1654,30 @@ public class Case {
                 open(caseDir, caseName, caseDisplayName, caseNumber, examiner, caseType, progressIndicator);
             } else {
                 /*
-                 * First, acquire an exclusive case name lock to prevent two
-                 * nodes from creating the same case at the same time.
+                 * Acquire a shared case directory lock that will be held as
+                 * long as this node has this case open. This will prevent
+                 * deletion of the case by another node.
                  */
                 progressIndicator.start(Bundle.Case_progressMessage_openingCaseResources());
-                try (CoordinationService.Lock nameLock = Case.acquireExclusiveCaseNameLock(caseName)) {
-                    assert (null != nameLock);
-                    /*
-                     * Next, acquire a shared case directory lock that will be
-                     * held as long as this node has this case open. This will
-                     * prevent deletion of the case by another node.
-                     */
-                    acquireSharedCaseDirLock(caseDir);
-                    /*
-                     * Finally, acquire an exclusive case resources lock to
-                     * ensure only one node at a time can
-                     * create/open/upgrade/close the case resources.
-                     */
-                    try (CoordinationService.Lock resourcesLock = acquireExclusiveCaseResourcesLock(caseName)) {
-                        assert (null != resourcesLock);
-                        try {
-                            open(caseDir, caseName, caseDisplayName, caseNumber, examiner, caseType, progressIndicator);
-                        } catch (CaseActionException ex) {
-                            /*
-                             * Release the case directory lock immediately if
-                             * there was a problem opening the case.
-                             */
-                            if (CaseType.MULTI_USER_CASE == caseType) {
-                                releaseSharedCaseDirLock(caseName);
-                            }
-                            throw ex;
+                acquireSharedCaseDirLock(caseDir);
+                /*
+                 * Acquire an exclusive case resources lock to ensure only one
+                 * node at a time can create/open/upgrade/close the case
+                 * resources.
+                 */
+                try (CoordinationService.Lock resourcesLock = acquireExclusiveCaseResourcesLock(caseName)) {
+                    assert (null != resourcesLock);
+                    try {
+                        open(caseDir, caseName, caseDisplayName, caseNumber, examiner, caseType, progressIndicator);
+                    } catch (CaseActionException ex) {
+                        /*
+                         * Release the case directory lock immediately if there
+                         * was a problem opening the case.
+                         */
+                        if (CaseType.MULTI_USER_CASE == caseType) {
+                            releaseSharedCaseDirLock(caseName);
                         }
+                        throw ex;
                     }
                 }
             }
