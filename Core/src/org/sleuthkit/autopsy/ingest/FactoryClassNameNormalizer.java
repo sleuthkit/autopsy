96ab9d1dfa2d4e0770d5d7c1a8782e3ee34dc616
U-BASIS\dgrove <dgrove@WIN-DGROV-4999.basistech.net>
2017-06-20 13:44:32 -0400

    Refined RegEx to only purge numbers at the end of a string.

--- Core/src/org/sleuthkit/autopsy/ingest/FactoryClassNameNormalizer.java
+++ Core/src/org/sleuthkit/autopsy/ingest/FactoryClassNameNormalizer.java
@@ -1,7 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Autopsy Forensic Browser
+ *
+ * Copyright 2016-2017 Basis Technology Corp.
+ * Contact: carrier <at> sleuthkit <dot> org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.sleuthkit.autopsy.ingest;
 
@@ -21,14 +34,14 @@ class FactoryClassNameNormalizer {
             // of their file name. This block of code gets rid of that variable
             // instance number and helps maitains constant module name over
             // multiple runs.
-            String moduleClassName = canonicalClassName.replaceAll("[$\\d]", ""); //NON-NLS NON-NLS
+            String moduleClassName = canonicalClassName.replaceAll("\\d*$", ""); //NON-NLS NON-NLS
             return moduleClassName;
         }
         return canonicalClassName;
     }
 
     /**
-     * Determines if the moduleSettingsFilePath is that of a serialized jython
+     * Determines if the moduleSettingsFilePath is that of a serialized Jython
      * instance. Serialized Jython instances (settings saved on the disk)
      * contain "org.python.proxies." in their fileName based on the current
      * implementation.
