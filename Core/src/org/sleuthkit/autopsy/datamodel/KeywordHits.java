8166bd22723600515c69a4e51144b7943d9a762a
millmanorama <millmanorama@gmail.com>
2017-06-22 11:47:24 +0200

    put the correct node name in the lookup

--- Core/src/org/sleuthkit/autopsy/datamodel/KeywordHits.java
+++ Core/src/org/sleuthkit/autopsy/datamodel/KeywordHits.java
@@ -774,7 +774,7 @@ public class KeywordHits implements AutopsyVisitableItem {
         private final String instance;
 
         public RegExpInstanceNode(String setName, String keyword, String instance) {
-            super(Children.create(new HitsFactory(setName, keyword, instance), true), Lookups.singleton(keyword));
+            super(Children.create(new HitsFactory(setName, keyword, instance), true), Lookups.singleton(instance));
             super.setName(instance);  //the instance represents the name of the keyword hit at this point as the keyword is the regex
             this.setName = setName;
             this.keyword = keyword;
