b5186013f13363e55b277f659038bc6e943366d1
William Schaefer <wschaefer@basistech.net>
2017-06-23 13:01:49 -0400

    2667 All regripper plugins with while loops dependent on substr have way to end

--- thirdparty/rr-full/plugins/shellbags_tln.pl
+++ thirdparty/rr-full/plugins/shellbags_tln.pl
@@ -356,6 +356,7 @@ sub parseVariableEntry {
 	  	while($tag) {
 	  		my $sz = unpack("V",substr($stuff,$cnt,4));
 	  		my $id = unpack("V",substr($stuff,$cnt + 4,4));
+                        return %item unless (defined $sz); 
 #--------------------------------------------------------------
 # sub-segment types
 # 0x0a - file name
@@ -386,6 +387,7 @@ sub parseVariableEntry {
 #	  		my $sz = unpack("V",substr($stuff,$cnt,4));
 #	  		my $id = unpack("V",substr($stuff,$cnt + 4,4));
 #	  		
+#                        return %item unless (defined $sz); 
 #	  		if ($sz == 0x00) {
 #	  			$tag = 0;
 #	  			next;
@@ -652,6 +654,7 @@ sub parseFolderEntry {
 	my $str = "";
 	while($tag) {
 		my $s = substr($data,$ofs_shortname + $cnt,1);
+                return %item unless (defined $s); 
 		if ($s =~ m/\x00/ && ((($cnt + 1) % 2) == 0)) {
 			$tag = 0;
 		}
@@ -667,7 +670,9 @@ sub parseFolderEntry {
 	$tag = 1;
 	$cnt = 0;
 	while ($tag) {
-		if (unpack("v",substr($data,$ofs + $cnt,2)) == 0xbeef) {
+                my $s = substr($data,$ofs + $cnt,2);
+                return %item unless (defined $s); 
+		if (unpack("v",$s) == 0xbeef) {
 			$tag = 0;
 		}
 		else {
