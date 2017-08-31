36eb41780283dccd9760904ab9f0a1182a203224
esaunders <esaunders@basistech.com>
2017-08-31 11:31:39 -0400

    Remove more unused import.

--- Core/src/org/sleuthkit/autopsy/datamodel/ExtractedContent.java
+++ Core/src/org/sleuthkit/autopsy/datamodel/ExtractedContent.java
@@ -26,7 +26,6 @@ import java.util.Comparator;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 import java.util.logging.Level;
 import org.openide.nodes.ChildFactory;
 import org.openide.nodes.Children;
