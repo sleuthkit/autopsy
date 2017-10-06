9712f0329d8941e7dad82507e96854c0570f75ce
U-BASIS\dgrove <dgrove@WIN-DGROV-4999.basistech.net>
2017-10-06 13:25:42 -0400

    Replaced folder scan with coordination service call.

--- Experimental/src/org/sleuthkit/autopsy/experimental/autoingest/AutoIngestCaseManager.java
+++ Experimental/src/org/sleuthkit/autopsy/experimental/autoingest/AutoIngestCaseManager.java
@@ -22,8 +22,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.openide.util.NbBundle;
+import org.openide.util.NbBundle.Messages;
 import org.sleuthkit.autopsy.casemodule.Case;
 import org.sleuthkit.autopsy.casemodule.CaseActionException;
+import org.sleuthkit.autopsy.coordinationservice.CoordinationService;
+import org.sleuthkit.autopsy.coordinationservice.CoordinationService.CoordinationServiceException;
+import org.sleuthkit.autopsy.coreutils.MessageNotifyUtil;
 import org.sleuthkit.autopsy.experimental.configuration.AutoIngestUserPreferences;
 
 /**
@@ -31,14 +38,17 @@ import org.sleuthkit.autopsy.experimental.configuration.AutoIngestUserPreference
  */
 final class AutoIngestCaseManager {
 
+    private static final Logger LOGGER = Logger.getLogger(AutoIngestCaseManager.class.getName());
     private static AutoIngestCaseManager instance;
+    
+    private CoordinationService coordinationService;
 
     /**
      * Gets the auto ingest case manager.
      *
      * @return The auto ingest case manager singleton.
      */
-    synchronized static AutoIngestCaseManager getInstance() {
+    synchronized static AutoIngestCaseManager getInstance() throws AutoIngestCaseManager.AutoIngestCaseManagerException {
         if (null == instance) {
             instance = new AutoIngestCaseManager();
         }
@@ -49,20 +59,80 @@ final class AutoIngestCaseManager {
      * Constructs an object that handles locating and opening cases created by
      * auto ingest.
      */
-    private AutoIngestCaseManager() {
+    private AutoIngestCaseManager() throws AutoIngestCaseManagerException {
+        try {
+            init();
+        } catch (AutoIngestCaseManager.AutoIngestCaseManagerException ex) {
+            Logger.getLogger(AutoIngestCaseManager.class.getName()).log(Level.SEVERE, null, ex);
+            throw ex;
+        }
+    }
+    
+    /**
+     * Initialize the coordination service.
+     * 
+     * @throws org.sleuthkit.autopsy.experimental.autoingest.AutoIngestCaseManager.AutoIngestCaseManagerException 
+     */
+    private void init() throws AutoIngestCaseManager.AutoIngestCaseManagerException {
+        try {
+            coordinationService = CoordinationService.getInstance();
+        } catch (CoordinationServiceException ex) {
+            throw new AutoIngestCaseManager.AutoIngestCaseManagerException(ex.getMessage(), ex);
+        }
     }
 
-    /*
+    /**
      * Gets a list of the cases in the top level case folder used by auto
      * ingest.
      */
+    @Messages({
+        "AutoIngestCaseManager.CasePathsError=An error occurred while trying to retrieve the list of case paths."
+    })
     List<AutoIngestCase> getCases() {
         List<AutoIngestCase> cases = new ArrayList<>();
-        List<Path> caseFolders = PathUtils.findCaseFolders(Paths.get(AutoIngestUserPreferences.getAutoModeResultsFolder()));
-        for (Path caseFolderPath : caseFolders) {
-            cases.add(new AutoIngestCase(caseFolderPath));
+        List<Path> casePathList;
+        try {
+            casePathList = getCasePaths();
+            for (Path casePath : casePathList) {
+                cases.add(new AutoIngestCase(casePath));
+            }
+        } catch (AutoIngestCaseManagerException ex) {
+            String errorMessage = NbBundle.getMessage(AutoIngestCaseManager.class, "AutoIngestCaseManager.CasePathsError");
+            LOGGER.log(Level.SEVERE, errorMessage, ex);
+            MessageNotifyUtil.Message.error(errorMessage);
         }
         return cases;
+    }
+    
+    /**
+     * Retrieve all of the case nodes and filter for only those that represent
+     * case paths.
+     * 
+     * @return List of case paths.
+     */
+    private List<Path> getCasePaths() throws AutoIngestCaseManagerException {
+        try {
+            List<String> nodeList = coordinationService.getNodeList(CoordinationService.CategoryNode.CASES);
+            List<Path> casePathList = new ArrayList<Path>(0);
+            for (String node : nodeList) {
+                if(node.indexOf('\\') >= 0 || node.indexOf('/') >= 0) {
+                    /*
+                     * This is not a case name lock (name specifies a path).
+                     */
+                    String nodeUpperCase = node.toUpperCase();
+                    if(!nodeUpperCase.endsWith("_RESOURCES") && !nodeUpperCase.endsWith("AUTO_INGEST_LOG.TXT")) {
+                        /*
+                         * This is not a case resource lock. Collect the path.
+                         */
+                        casePathList.add(Paths.get(node));
+                    }
+                }
+            }
+            return casePathList;
+            
+        } catch (CoordinationServiceException ex) {
+            throw new AutoIngestCaseManager.AutoIngestCaseManagerException("Failed to get node list from coordination service.", ex);
+        }
     }
 
     /**
@@ -77,5 +147,36 @@ final class AutoIngestCaseManager {
          * Open the case.
          */
         Case.openAsCurrentCase(caseMetadataFilePath.toString());
+    }
+    
+    /**
+     * Exception type thrown when there is an error completing an auto ingest
+     * monitor operation.
+     */
+    static final class AutoIngestCaseManagerException extends Exception {
+
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * Constructs an instance of the exception type thrown when there is an
+         * error completing an auto ingest case manager operation.
+         *
+         * @param message The exception message.
+         */
+        private AutoIngestCaseManagerException(String message) {
+            super(message);
+        }
+
+        /**
+         * Constructs an instance of the exception type thrown when there is an
+         * error completing an auto ingest case manager operation.
+         *
+         * @param message The exception message.
+         * @param cause   A Throwable cause for the error.
+         */
+        private AutoIngestCaseManagerException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
     }
 }
