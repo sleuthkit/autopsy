8921a3a1d35d87578bfd4038daecd3c4033b3ddc
William Schaefer <wschaefer@basistech.net>
2017-10-13 16:08:18 -0400

    Removed un-used example factory and references to it for if it was used

--- Core/src/org/sleuthkit/autopsy/ingest/IngestModuleFactoryLoader.java
+++ Core/src/org/sleuthkit/autopsy/ingest/IngestModuleFactoryLoader.java
@@ -29,8 +29,6 @@ import org.openide.NotifyDescriptor;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 import org.sleuthkit.autopsy.coreutils.Logger;
-import org.sleuthkit.autopsy.examples.SampleExecutableIngestModuleFactory;
-import org.sleuthkit.autopsy.examples.SampleIngestModuleFactory;
 import org.sleuthkit.autopsy.modules.e01verify.E01VerifierModuleFactory;
 import org.sleuthkit.autopsy.modules.exif.ExifParserModuleFactory;
 import org.sleuthkit.autopsy.modules.fileextmismatch.FileExtMismatchDetectorModuleFactory;
@@ -47,8 +45,6 @@ import org.sleuthkit.autopsy.python.JythonModuleLoader;
 final class IngestModuleFactoryLoader {
 
     private static final Logger logger = Logger.getLogger(IngestModuleFactoryLoader.class.getName());
-    private static final String SAMPLE_MODULE_FACTORY_CLASS_NAME = SampleIngestModuleFactory.class.getCanonicalName();
-    private static final String SAMPLE_EXECUTABLE_MODULE_FACTORY_CLASS_NAME = SampleExecutableIngestModuleFactory.class.getCanonicalName();
     private static final ArrayList<String> coreModuleOrdering = new ArrayList<String>() {
         {
             // The ordering of the core ingest module factories implemented
@@ -141,11 +137,6 @@ final class IngestModuleFactoryLoader {
     private static void addFactory(IngestModuleFactory factory, HashSet<String> moduleDisplayNames, HashMap<String, IngestModuleFactory> javaFactoriesByClass) {
         // Ignore the sample ingest module factories implemented in Java.        
         String className = factory.getClass().getCanonicalName();
-        if (className.equals(IngestModuleFactoryLoader.SAMPLE_MODULE_FACTORY_CLASS_NAME)
-                || className.equals(IngestModuleFactoryLoader.SAMPLE_EXECUTABLE_MODULE_FACTORY_CLASS_NAME)) {
-            return;
-        }
-
         if (!moduleDisplayNames.contains(factory.getModuleDisplayName())) {
             moduleDisplayNames.add(factory.getModuleDisplayName());
             javaFactoriesByClass.put(factory.getClass().getCanonicalName(), factory);
