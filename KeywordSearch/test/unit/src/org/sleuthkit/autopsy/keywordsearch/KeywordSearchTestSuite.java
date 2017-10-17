10d239dd25f062faaeac857e5f0cbe7d9cb009e2
U-BASIS\zhaohui <zhaohui@Win-Zhaoh-4990.basistech.net>
2017-10-17 14:47:32 -0400

    3142: Experiment with adding a JUnit TestSuite to the automated unit tests

---
+++ KeywordSearch/test/unit/src/org/sleuthkit/autopsy/keywordsearch/KeywordSearchTestSuite.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sleuthkit.autopsy.keywordsearch;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+/**
+ *
+ * @author 
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CreditCardValidatorTest.class})
+public class KeywordSearchTestSuite {
+    public KeywordSearchTestSuite() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+    @Ignore
+    public static void main(String[] args) {
+        System.out.println("main method");
+    }
+}
