763c0fa03c610f92b99c288ed1e47a3d37eb42e6
U-BASIS\zhaohui <zhaohui@win-zhaoh-3257.basistech.net>
2017-10-27 13:59:48 -0400

    2969: update MD5 error messages

--- Core/src/org/sleuthkit/autopsy/filesearch/HashSearchFilter.java
+++ Core/src/org/sleuthkit/autopsy/filesearch/HashSearchFilter.java
@@ -63,8 +63,8 @@ class HashSearchFilter extends AbstractFileSearchFilter<HashSearchPanel> {
     @Override
     @Messages({
         "HashSearchFilter.errorMessage.emptyHash=Hash data is empty.",
-        "# {0} - hash data length", "HashSearchFilter.errorMessage.wrongLength=Input lenght({0}), doesn't match the MD5 length(32).",
-        "HashSearchFilter.errorMessage.wrongCharacter=Input data is an invalid MD5 hex data."
+        "# {0} - hash data length", "HashSearchFilter.errorMessage.wrongLength=Input lenght({0}), doesn''t match the MD5 length(32).",
+        "HashSearchFilter.errorMessage.wrongCharacter=MD5 contains invalid hex characters."
     })
     public boolean isValid() {
         String inputHashData = this.getComponent().getSearchTextField().getText();
