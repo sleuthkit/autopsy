9ae63a7d8d722c0ee871c02db2c8e5d83a533cc8
U-BASIS\dgrove <dgrove@WIN-DGROV-4999.basistech.net>
2017-11-03 17:01:55 -0400

    Removed unnecessary method.

--- Core/src/org/sleuthkit/autopsy/casemodule/CaseMetadata.java
+++ Core/src/org/sleuthkit/autopsy/casemodule/CaseMetadata.java
@@ -175,17 +175,6 @@ public final class CaseMetadata {
     public String getCaseDirectory() {
         return metadataFilePath.getParent().toString();
     }
-    
-    /**
-     * Gets the full case directory path.
-     * 
-     * @return The case directory path.
-     */
-    public String getCaseDirectoryPath() {
-        String fileName = metadataFilePath.getFileName().toString();
-        String caseDirectoryPath = metadataFilePath.toString();
-        return caseDirectoryPath.substring(0, caseDirectoryPath.lastIndexOf(fileName));
-    }
 
     /**
      * Gets the case type.
