59539302a9dd92b912f8dc43fe4d9c078ecde984
Eli Kohlenberg <ekohlenb@rand.org>
2017-11-11 15:32:43 -0500

    reenabling multi-user case for non-windows platforms

--- Core/src/org/sleuthkit/autopsy/core/UserPreferences.java
+++ Core/src/org/sleuthkit/autopsy/core/UserPreferences.java
@@ -238,7 +238,7 @@ public final class UserPreferences {
     }
 
     public static boolean getIsMultiUserModeEnabled() {
-        if (!IS_WINDOWS_OS) {
+        if (/*!IS_WINDOWS_OS*/ false) {
             return false;
         }
         return preferences.getBoolean(IS_MULTI_USER_MODE_ENABLED, false);
