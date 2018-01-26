4a4e163571d569679681db5f83aea31314ade978
U-BASIS\zhaohui <zhaohui@win-zhaoh-3257.basistech.net>
2018-01-26 11:58:29 -0500

    3469_3470: cleanup

--- Core/src/org/sleuthkit/autopsy/report/TableReportGenerator.java
+++ Core/src/org/sleuthkit/autopsy/report/TableReportGenerator.java
@@ -1,7 +1,7 @@
 /*
  * Autopsy Forensic Browser
  *
- * Copyright 2013-16 Basis Technology Corp.
+ * Copyright 2013-2018 Basis Technology Corp.
  * Contact: carrier <at> sleuthkit <dot> org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -40,7 +40,6 @@ import java.util.logging.Level;
 import org.openide.util.NbBundle;
 import org.sleuthkit.autopsy.casemodule.Case;
 import org.sleuthkit.autopsy.casemodule.services.TagsManager;
-import org.sleuthkit.autopsy.coreutils.EscapeUtil;
 import org.sleuthkit.autopsy.coreutils.ImageUtils;
 import org.sleuthkit.autopsy.coreutils.Logger;
 import org.sleuthkit.autopsy.datamodel.ContentUtils;
