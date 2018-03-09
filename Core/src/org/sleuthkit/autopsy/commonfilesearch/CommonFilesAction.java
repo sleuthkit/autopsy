fd5e7d548673882cdecd5f27bc850914fd940621
Brian Sweeney <bsweeney@ciphertechsolutions.com>
2018-03-09 14:08:38 -0700

    stubbed out lots of ui components for common files search

---
+++ Core/src/org/sleuthkit/autopsy/commonfilesearch/CommonFilesAction.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sleuthkit.autopsy.commonfilesearch;
+
+import org.openide.util.HelpCtx;
+import org.openide.util.actions.CallableSystemAction;
+import org.sleuthkit.autopsy.directorytree.FileSearchProvider;
+
+/**
+ *
+ * @author bsweeney
+ */
+public class CommonFilesAction extends CallableSystemAction implements FileSearchProvider {
+
+    @Override
+    public void performAction() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public HelpCtx getHelpCtx() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void showDialog() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
