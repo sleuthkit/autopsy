13574a54be2279cbc944f6703cf8e44715376b6f
U-BASIS\zhaohui <zhaohui@Win-Zhaoh-4990.basistech.net>
2018-03-15 16:33:27 -0400

    3178: Add data source to file filters unit test class

--- Core/test/qa-functional/src/org/sleuthkit/autopsy/ingest/IngestFileFiltersTest.java
+++ Core/test/qa-functional/src/org/sleuthkit/autopsy/ingest/IngestFileFiltersTest.java
@@ -22,6 +22,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import static junit.framework.Assert.assertFalse;
 import junit.framework.TestCase;
 import org.netbeans.junit.NbModuleSuite;
 import org.sleuthkit.autopsy.casemodule.Case;
@@ -29,12 +31,26 @@ import org.sleuthkit.autopsy.casemodule.CaseActionException;
 import org.sleuthkit.autopsy.casemodule.CaseDetails;
 import junit.framework.Test;
 import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.openide.util.Exceptions;
+import org.python.icu.impl.Assert;
+import org.sleuthkit.autopsy.casemodule.ImageDSProcessor;
+import org.sleuthkit.autopsy.casemodule.NoCurrentCaseException;
+import org.sleuthkit.autopsy.casemodule.services.FileManager;
+import org.sleuthkit.autopsy.datasourceprocessors.AutoIngestDataSourceProcessor;
+import org.sleuthkit.autopsy.testutils.*;
+import org.sleuthkit.autopsy.testutils.DataSourceProcessorRunner.ProcessorCallback;
+import org.sleuthkit.datamodel.AbstractFile;
+import org.sleuthkit.datamodel.Content;
+import org.sleuthkit.datamodel.TskCoreException;
 
 public class IngestFileFiltersTest extends TestCase {
 
     private static final Path caseDirectoryPath = Paths.get(System.getProperty("java.io.tmpdir"), "IngestFileFiltersTest");
     private static final File CASE_DIR = new File(caseDirectoryPath.toString());
+    private static final Path imagePath = Paths.get("test/filter_test1.img");
+    private final ImageDSProcessor dataSourceProcessor = new ImageDSProcessor();
     
     public static Test suite() {
         NbModuleSuite.Configuration conf = NbModuleSuite.createConfiguration(IngestFileFiltersTest.class).
@@ -44,16 +60,46 @@ public class IngestFileFiltersTest extends TestCase {
     }
 
     @Override
+    @BeforeClass
     public void setUp() {
+        // Delete the test directory, if it exists
+        if (caseDirectoryPath.toFile().exists()) {
+            try {
+                FileUtils.deleteDirectory(caseDirectoryPath.toFile());
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+                Assert.fail(ex);
+            }
+        }
+        assertFalse("Unable to delete existing test directory", caseDirectoryPath.toFile().exists());
+ 
+        // Create the test directory
+        caseDirectoryPath.toFile().mkdirs();
+        assertTrue("Unable to create test directory", caseDirectoryPath.toFile().exists());
+
         try {
             Case.createAsCurrentCase(Case.CaseType.SINGLE_USER_CASE, caseDirectoryPath.toString(), new CaseDetails("IngestFiltersTest"));
         } catch (CaseActionException ex) {
             Exceptions.printStackTrace(ex);
+            Assert.fail(ex);
         }        
         assertTrue(CASE_DIR.exists());
+        ProcessorCallback callBack = null;
+        try {
+            callBack = DataSourceProcessorRunner.runDataSourceProcessor(dataSourceProcessor, imagePath);
+        } catch (AutoIngestDataSourceProcessor.AutoIngestDataSourceProcessorException | InterruptedException ex) {
+            Exceptions.printStackTrace(ex);
+            Assert.fail(ex);
+            
+        }
+        List<Content> dataSourceContent = callBack.getDataSourceContent();
+        assertEquals(1, dataSourceContent.size());
+        List<String> errorMessages = callBack.getErrorMessages();
+        assertEquals(0, errorMessages.size());
     }
 
     @Override
+    @AfterClass
     public void tearDown() {
         try {
             Case.closeCurrentCase();
@@ -64,8 +110,28 @@ public class IngestFileFiltersTest extends TestCase {
         }
         assertFalse(CASE_DIR.exists());
     }
+    
+    public void testFileNotFound() {
+        try {
+            FileManager fm = Case.getOpenCase().getServices().getFileManager();
+            List<AbstractFile> results = fm.findFiles("noFound");
+            assertEquals(0, results.size());
+            
+        } catch (TskCoreException | NoCurrentCaseException ex) {
+            Exceptions.printStackTrace(ex);
+            Assert.fail(ex);
+        }
+    }
 
-    public void testFilter() {
-        System.out.println("testFilter");
+    public void testFileFound() {
+        try {
+            FileManager fm = Case.getOpenCase().getServices().getFileManager();
+            List<AbstractFile> results = fm.findFiles("file.jpg", "dir1");
+            assertEquals(1, results.size());
+            assertEquals("file.jpg", results.get(0).getName());
+        } catch (TskCoreException | NoCurrentCaseException ex) {
+            Exceptions.printStackTrace(ex);
+            Assert.fail(ex);
+        }
     }
 }
