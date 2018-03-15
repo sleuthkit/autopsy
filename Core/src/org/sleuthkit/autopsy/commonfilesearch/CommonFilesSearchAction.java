3930b725960b107643f8ddac1ead8aac5c14cf7b
Brian Sweeney <bsweeney@ciphertechsolutions.com>
2018-03-15 15:04:14 -0600

    better error hanlding

--- Core/src/org/sleuthkit/autopsy/commonfilesearch/CommonFilesSearchAction.java
+++ Core/src/org/sleuthkit/autopsy/commonfilesearch/CommonFilesSearchAction.java
@@ -21,27 +21,46 @@ package org.sleuthkit.autopsy.commonfilesearch;
 import java.awt.event.ActionEvent;
 import java.beans.PropertyChangeEvent;
 import java.util.EnumSet;
+import java.util.logging.Level;
+import org.openide.util.Exceptions;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.CallableSystemAction;
 import org.sleuthkit.autopsy.casemodule.Case;
+import org.sleuthkit.autopsy.casemodule.NoCurrentCaseException;
+import org.sleuthkit.autopsy.coreutils.Logger;
+import org.sleuthkit.datamodel.SleuthkitCase;
+import org.sleuthkit.datamodel.TskCoreException;
 
 /**
  * Encapsulates a menu action which triggers the common files search dialog.
  */
 final class CommonFilesSearchAction extends CallableSystemAction {
 
+    private static final Logger LOGGER = Logger.getLogger(CommonFilesPanel.class.getName());
+    
     private static CommonFilesSearchAction instance = null;
     private static final long serialVersionUID = 1L;
 
     CommonFilesSearchAction() {
         super();
-        this.setEnabled(Case.isCaseOpen());
+        this.setEnabled(this.caseHasMultipleSources());
         Case.addEventTypeSubscriber(EnumSet.of(Case.Events.CURRENT_CASE), (PropertyChangeEvent evt) -> {
             if (evt.getPropertyName().equals(Case.Events.CURRENT_CASE.toString())) {
-                setEnabled(evt.getNewValue() != null);
+                this.setEnabled(evt.getNewValue() != null && this.caseHasMultipleSources());
             }
         });
+    }
+    
+    private boolean caseHasMultipleSources(){
+        try {
+            Case currentCase = Case.getOpenCase();
+            SleuthkitCase tskDb = currentCase.getSleuthkitCase();
+            return tskDb.getDataSources().size() >= 2;
+        } catch (NoCurrentCaseException | TskCoreException ex) {
+            LOGGER.log(Level.SEVERE, "Unable to count datasources.", ex);
+        }
+        return false;
     }
 
     public static synchronized CommonFilesSearchAction getDefault() {
