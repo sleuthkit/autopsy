f2aa63890dbf3d8a09151ff0c85ffea208c7c023
Brian Sweeney <bsweeney@ciphertechsolutions.com>
2018-04-11 10:33:04 -0600

    cleanup

--- Core/src/org/sleuthkit/autopsy/datamodel/CommonFileChildNode.java
+++ Core/src/org/sleuthkit/autopsy/datamodel/CommonFileChildNode.java
@@ -20,34 +20,40 @@ package org.sleuthkit.autopsy.datamodel;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import org.apache.commons.lang3.StringUtils;
 import org.openide.nodes.Sheet;
 import org.openide.util.NbBundle;
+import org.sleuthkit.autopsy.coreutils.Logger;
 import org.sleuthkit.datamodel.AbstractFile;
+import org.sleuthkit.datamodel.TskCoreException;
 
 /**
  * Encapsulates data being pushed to Common Files component in top right pane.
  */
 public class CommonFileChildNode extends FileNode {
-        
-    private final String dataSource;
+
+    private static final Logger LOGGER = Logger.getLogger(CommonFileChildNode.class.getName());
     
+    private final String dataSource;
+
     public CommonFileChildNode(AbstractFile fsContent, String dataSource) {
         super(fsContent);
         this.content = fsContent;
         this.dataSource = dataSource;
     }
-    
-    @Override 
-    public <T> T accept(DisplayableItemNodeVisitor<T> visitor){
+
+    @Override
+    public <T> T accept(DisplayableItemNodeVisitor<T> visitor) {
         return visitor.visit(this);
     }
-    
+
     @Override
-    public AbstractFile getContent(){
+    public AbstractFile getContent() {
         return this.content;
     }
-    
-    String getDataSource(){
+
+    String getDataSource() {
         return this.dataSource;
     }
 
@@ -68,37 +74,46 @@ public class CommonFileChildNode extends FileNode {
             final String propString = propType.toString();
             sheetSet.put(new NodeProperty<>(propString, propString, NO_DESCR, map.get(propString)));
         }
+        
+        this.addTagProperty(sheetSet);
 
         return sheet;
     }
-    
+
     /**
      * Fill map with AbstractFile properties
      *
-     * @param map     map with preserved ordering, where property names/values
-     *                are put
+     * @param map map with preserved ordering, where property names/values are
+     * put
      * @param node The item to get properties for.
      */
     static private void fillPropertyMap(Map<String, Object> map, CommonFileChildNode node) {
+
         map.put(CommonFilePropertyType.File.toString(), node.getName());
+        map.put(CommonFilePropertyType.ParentPath.toString(), node.getContent().getParentPath());
+        map.put(CommonFilePropertyType.HashsetHits.toString(), getHashSetHitsForFile(node.getContent()));
         map.put(CommonFilePropertyType.DataSource.toString(), node.getDataSource());
     }
-    
+
     @NbBundle.Messages({
         "CommonFilePropertyType.fileColLbl=File",
+        "CommonFilePropertyType.pathColLbl=Parent Path",
+        "CommonFilePropertyType.hashsetHitsColLbl=Hash Set Hits",
         "CommonFilePropertyType.dataSourceColLbl=Data Source"
-        })
+    })
     public enum CommonFilePropertyType {
-        
+
         File(Bundle.CommonFilePropertyType_fileColLbl()),
+        ParentPath(Bundle.CommonFilePropertyType_pathColLbl()),
+        HashsetHits(Bundle.CommonFilePropertyType_hashsetHitsColLbl()),
         DataSource(Bundle.CommonFilePropertyType_dataSourceColLbl());
-        
+
         final private String displayString;
-        
-        private CommonFilePropertyType(String displayString){
+
+        private CommonFilePropertyType(String displayString) {
             this.displayString = displayString;
         }
-        
+
         @Override
         public String toString() {
             return displayString;
