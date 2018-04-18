5cbec60395575f1e131218fe40afc014b3a46f78
Andrew Ziehl <aziehl@ciphertechsolutions.com>
2018-04-17 20:12:22 -0700

    Fix for not filtering by mime type. TODO, include mime-type as column?

--- Core/src/org/sleuthkit/autopsy/commonfilesearch/CommonFilesMetaDataBuilder.java
+++ Core/src/org/sleuthkit/autopsy/commonfilesearch/CommonFilesMetaDataBuilder.java
@@ -197,8 +197,9 @@ abstract class CommonFilesMetaDataBuilder {
                mimeTypeFilter.append("\"").append(mimeType).append("\",");
            }
            mimeTypeString = mimeTypeFilter.toString().substring(0, mimeTypeFilter.length() - 1);
+           mimeTypeString = String.format(filterByMimeTypesWhereClause, new Object[]{mimeTypeString});
         }
-        return String.format(filterByMimeTypesWhereClause, new Object[]{mimeTypeString});
+        return mimeTypeString;
     }
 
  
