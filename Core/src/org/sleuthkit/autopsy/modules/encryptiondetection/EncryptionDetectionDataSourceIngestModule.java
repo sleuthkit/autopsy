cdae2abe976b3d058f71fef46aae0b565401485b
U-BASIS\dgrove <dgrove@WIN-DGROV-4999.basistech.net>
2018-05-04 11:39:49 -0400

    New 'BitlockerDetection' class to handle that functionality.

--- Core/src/org/sleuthkit/autopsy/modules/encryptiondetection/EncryptionDetectionDataSourceIngestModule.java
+++ Core/src/org/sleuthkit/autopsy/modules/encryptiondetection/EncryptionDetectionDataSourceIngestModule.java
@@ -18,9 +18,7 @@
  */
 package org.sleuthkit.autopsy.modules.encryptiondetection;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
@@ -49,17 +47,6 @@ import org.sleuthkit.datamodel.VolumeSystem;
  * Data source module to detect encryption.
  */
 final class EncryptionDetectionDataSourceIngestModule implements DataSourceIngestModule {
-
-    private static final int BITLOCKER_BIOS_PARAMETER_BLOCK_SIZE = 0x54;
-    private static final byte[] BITLOCKER_SIGNATURE_BYTES = {'-', 'F', 'V', 'E', '-', 'F', 'S', '-'};
-    private static final int BITLOCKER_ADDRESS_SIGNATURE = 0x3;
-    private static final int BITLOCKER_ADDRESS_SECTORS_PER_CLUSTER = 0xD;
-    private static final int BITLOCKER_ADDRESS_RESERVED_CLUSTERS = 0xE;
-    private static final int BITLOCKER_ADDRESS_FAT_COUNT = 0x10;
-    private static final int BITLOCKER_ADDRESS_ROOT_ENTRIES = 0x11;
-    private static final int BITLOCKER_ADDRESS_SECTORS = 0x13;
-    private static final int BITLOCKER_ADDRESS_SECTORS_PER_FAT = 0x16;
-    private static final int BITLOCKER_ADDRESS_LARGE_SECTORS = 0x20;
 
     private final IngestServices services = IngestServices.getInstance();
     private final Logger logger = services.getLogger(EncryptionDetectionModuleFactory.getModuleName());
@@ -99,7 +86,7 @@ final class EncryptionDetectionDataSourceIngestModule implements DataSourceInges
                 List<VolumeSystem> volumeSystems = ((Image) dataSource).getVolumeSystems();
                 for (VolumeSystem volumeSystem : volumeSystems) {
                     for (Volume volume : volumeSystem.getVolumes()) {
-                        if (isBitlockerVolume(volume)) {
+                        if (BitlockerDetection.isBitlockerVolume(volume)) {
                             return flagVolume(volume, BlackboardArtifact.ARTIFACT_TYPE.TSK_ENCRYPTION_DETECTED,
                                     Bundle.EncryptionDetectionDataSourceIngestModule_artifactComment_bitlocker());
                         }
@@ -206,92 +193,5 @@ final class EncryptionDetectionDataSourceIngestModule implements DataSourceInges
             }
         }
         return false;
-    }
-
-    /**
-     * This method checks if the Volume input has been encrypted with Bitlocker.
-     *
-     * @param volume Volume to be checked.
-     *
-     * @return True if the Volume has been encrypted with Bitlocker.
-     *
-     * @throws ReadContentInputStreamException If there is a failure reading
-     *                                         from the InputStream.
-     * @throws IOException                     If there is a failure closing or
-     *                                         reading from the InputStream.
-     */
-    private boolean isBitlockerVolume(Volume volume) throws ReadContentInputStream.ReadContentInputStreamException, IOException {
-        /*
-         * Logic in this method is based on
-         * https://blogs.msdn.microsoft.com/si_team/2006/10/26/detecting-bitlocker/
-         */
-
-        boolean bitlockerVolume = false;
-
-        InputStream in = null;
-        BufferedInputStream bin = null;
-
-        try {
-            in = new ReadContentInputStream(volume);
-            bin = new BufferedInputStream(in);
-
-            byte[] bpbArray = new byte[BITLOCKER_BIOS_PARAMETER_BLOCK_SIZE];
-            bin.read(bpbArray, 0, BITLOCKER_BIOS_PARAMETER_BLOCK_SIZE);
-
-            boolean signatureMatches = true;
-            for (int i = 0; i < BITLOCKER_SIGNATURE_BYTES.length; i++) {
-                if (bpbArray[BITLOCKER_ADDRESS_SIGNATURE + i] != BITLOCKER_SIGNATURE_BYTES[i]) {
-                    signatureMatches = false;
-                    break;
-                }
-            }
-
-            if (signatureMatches) {
-                switch ((int) bpbArray[BITLOCKER_ADDRESS_SECTORS_PER_CLUSTER]) {
-                    case 0x01:
-                    case 0x02:
-                    case 0x04:
-                    case 0x08:
-                    case 0x10:
-                    case 0x20:
-                    case 0x40:
-                    case 0x80:
-                        short reservedClusters
-                                = (short) ((bpbArray[BITLOCKER_ADDRESS_RESERVED_CLUSTERS] << 8)
-                                | (bpbArray[BITLOCKER_ADDRESS_RESERVED_CLUSTERS + 1] & 0xFF));
-                        byte fatCount
-                                = bpbArray[BITLOCKER_ADDRESS_FAT_COUNT];
-                        short rootEntries
-                                = (short) ((bpbArray[BITLOCKER_ADDRESS_ROOT_ENTRIES] << 8)
-                                | (bpbArray[BITLOCKER_ADDRESS_ROOT_ENTRIES + 1] & 0xFF));
-                        short sectors
-                                = (short) ((bpbArray[BITLOCKER_ADDRESS_SECTORS] << 8)
-                                | (bpbArray[BITLOCKER_ADDRESS_SECTORS + 1] & 0xFF));
-                        short sectorsPerFat
-                                = (short) ((bpbArray[BITLOCKER_ADDRESS_SECTORS_PER_FAT] << 8)
-                                | (bpbArray[BITLOCKER_ADDRESS_SECTORS_PER_FAT + 1] & 0xFF));
-                        int largeSectors
-                                = ((bpbArray[BITLOCKER_ADDRESS_LARGE_SECTORS] << 24)
-                                | ((bpbArray[BITLOCKER_ADDRESS_LARGE_SECTORS + 1] & 0xFF) << 16)
-                                | ((bpbArray[BITLOCKER_ADDRESS_LARGE_SECTORS + 2] & 0xFF) << 8)
-                                | (bpbArray[BITLOCKER_ADDRESS_LARGE_SECTORS + 3] & 0xFF));
-
-                        if (reservedClusters == 0 && fatCount == 0 && rootEntries == 0
-                                && sectors == 0 && sectorsPerFat == 0 && largeSectors == 0) {
-                            bitlockerVolume = true;
-                        }
-                }
-            }
-
-            return bitlockerVolume;
-
-        } finally {
-            if (in != null) {
-                in.close();
-            }
-            if (bin != null) {
-                bin.close();
-            }
-        }
     }
 }
