d7e87d7831ad4da591461eefd00508e1c88ad724
Brian Sweeney <bsweeney@ciphertechsolutions.com>
2018-05-14 09:06:32 -0600

    try-with-resources

--- Core/src/org/sleuthkit/autopsy/commonfilesearch/DataSourceLoader.java
+++ Core/src/org/sleuthkit/autopsy/commonfilesearch/DataSourceLoader.java
@@ -44,8 +44,10 @@ public class DataSourceLoader {
 
     private void loadLogicalSources(SleuthkitCase tskDb, Map<Long, String> dataSouceMap) throws TskCoreException, SQLException {
         //try block releases resources - exceptions are handled in done()
-        try (SleuthkitCase.CaseDbQuery query = tskDb.executeQuery(SELECT_DATA_SOURCES_LOGICAL)) {
-            ResultSet resultSet = query.getResultSet();
+        try (
+                SleuthkitCase.CaseDbQuery query = tskDb.executeQuery(SELECT_DATA_SOURCES_LOGICAL);
+                ResultSet resultSet = query.getResultSet()
+                ) {
             while (resultSet.next()) {
                 Long objectId = resultSet.getLong(1);
                 String dataSourceName = resultSet.getString(2);
