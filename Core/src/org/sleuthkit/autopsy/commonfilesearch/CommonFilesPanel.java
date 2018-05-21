6dd5324ebf28d2703ee9fc817a02f17db2729be3
Brian Sweeney <bsweeney@ciphertechsolutions.com>
2018-05-21 13:17:09 -0600

    missing import

--- Core/src/org/sleuthkit/autopsy/commonfilesearch/CommonFilesPanel.java
+++ Core/src/org/sleuthkit/autopsy/commonfilesearch/CommonFilesPanel.java
@@ -24,6 +24,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
