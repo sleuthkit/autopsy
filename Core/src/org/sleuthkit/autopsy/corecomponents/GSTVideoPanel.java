c875aa1f05b6461be5793422a8b299383b873259
Richard Cordovano <rcordovano@basistech.com>
2018-06-25 17:51:26 -0400

    Add dummy GSTVideoPanel class for VT backwards compatibility

--- Core/src/org/sleuthkit/autopsy/corecomponents/GSTVideoPanel.java
+++ Core/src/org/sleuthkit/autopsy/corecomponents/GSTVideoPanel.java
@@ -22,9 +22,8 @@ package org.sleuthkit.autopsy.corecomponents;
  * This class exists to support backwards compatibility of an erroneous call to
  * Logger.getLogger(GSTVideoPanel.class.getName()) in OpenCVFrameCapture.java in
  * an older version of the Video Triage Net Beans Module (NBM). It should be
- * removed when the Video Triage NBM changes its version dependency on the
- * Autopsy-Core NBM, which is currently set to major release version 10 and
- * specification version 10. The Video Triage code has already been updated.
+ * removed when we are ready to stop supporting older Video Triage NBMs. The
+ * current Video Triage code has already been updated.
  */
 @Deprecated
 public class GSTVideoPanel {
