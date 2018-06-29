a36c4a61e3ba72d014425e14a9120f86dc5d8558
Brian Sweeney <bsweeney@ciphertechsolutions.com>
2018-06-28 19:27:52 -0600

    trying to quiet codacy

--- Core/test/qa-functional/src/org/sleuthkit/autopsy/commonfilessearch/MatchesInAtLeastTwoSources.java
+++ Core/test/qa-functional/src/org/sleuthkit/autopsy/commonfilessearch/MatchesInAtLeastTwoSources.java
@@ -34,11 +34,7 @@ import org.sleuthkit.autopsy.casemodule.NoCurrentCaseException;
 import org.sleuthkit.autopsy.commonfilesearch.AllDataSourcesCommonFilesAlgorithm;
 import org.sleuthkit.autopsy.commonfilesearch.CommonFilesMetadata;
 import org.sleuthkit.autopsy.commonfilesearch.CommonFilesMetadataBuilder;
-import static org.sleuthkit.autopsy.commonfilessearch.IntraCaseUtils.DOC;
-import static org.sleuthkit.autopsy.commonfilessearch.IntraCaseUtils.EMPTY;
-import static org.sleuthkit.autopsy.commonfilessearch.IntraCaseUtils.IMG;
-import static org.sleuthkit.autopsy.commonfilessearch.IntraCaseUtils.SET1;
-import static org.sleuthkit.autopsy.commonfilessearch.IntraCaseUtils.SET4;
+import static org.sleuthkit.autopsy.commonfilessearch.IntraCaseUtils.*;
 import org.sleuthkit.autopsy.ingest.IngestJobSettings;
 import org.sleuthkit.autopsy.ingest.IngestModuleTemplate;
 import org.sleuthkit.autopsy.modules.filetypeid.FileTypeIdModuleFactory;
@@ -48,11 +44,12 @@ import org.sleuthkit.datamodel.AbstractFile;
 import org.sleuthkit.datamodel.TskCoreException;
 
 /**
- * Ensures that matches only are found for files which appear in at least two data sources.
- * 
- * The two datasources used here have no common files.  One of the data sources 
- * has two identical files within it.  This should not count as a match.
- * 
+ * Ensures that matches only are found for files which appear in at least two
+ * data sources.
+ *
+ * The two datasources used here have no common files. One of the data sources
+ * has two identical files within it. This should not count as a match.
+ *
  * None of the test files should be found in the results of this test.
  */
 public class MatchesInAtLeastTwoSources extends NbTestCase {
@@ -109,20 +106,20 @@ public class MatchesInAtLeastTwoSources extends NbTestCase {
 
             CommonFilesMetadataBuilder allSourcesBuilder = new AllDataSourcesCommonFilesAlgorithm(dataSources, false, false);
             CommonFilesMetadata metadata = allSourcesBuilder.findCommonFiles();
-            
+
             Map<Long, String> objectIdToDataSource = IntraCaseUtils.mapFileInstancesToDataSources(metadata);
 
             List<AbstractFile> files = IntraCaseUtils.getFiles(objectIdToDataSource.keySet());
-            
+
             assertTrue(IntraCaseUtils.verifyFileExistanceAndCount(files, dataSources, IMG, SET1, 0));
             assertTrue(IntraCaseUtils.verifyFileExistanceAndCount(files, dataSources, IMG, SET4, 0));
-            
+
             assertTrue(IntraCaseUtils.verifyFileExistanceAndCount(files, dataSources, DOC, SET1, 0));
             assertTrue(IntraCaseUtils.verifyFileExistanceAndCount(files, dataSources, DOC, SET4, 0));
-            
+
             assertTrue(IntraCaseUtils.verifyFileExistanceAndCount(files, dataSources, EMPTY, SET1, 0));
             assertTrue(IntraCaseUtils.verifyFileExistanceAndCount(files, dataSources, EMPTY, SET4, 0));
-            
+
         } catch (NoCurrentCaseException | TskCoreException | SQLException ex) {
             Exceptions.printStackTrace(ex);
             Assert.fail(ex);
