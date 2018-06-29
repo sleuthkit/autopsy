f878ca5cd287737b4dc4062c604823645243b98b
William Schaefer <wschaefer@basistech.net>
2018-06-29 10:30:24 -0400

    3978 add checks for confidence and size to Unicode extractor

--- KeywordSearch/src/org/sleuthkit/autopsy/keywordsearch/UnicodeTextExtractor.java
+++ KeywordSearch/src/org/sleuthkit/autopsy/keywordsearch/UnicodeTextExtractor.java
@@ -29,10 +29,18 @@ import org.sleuthkit.datamodel.Content;
 import org.sleuthkit.datamodel.ReadContentInputStream;
 
 /**
- * Extract text from unicode files 
+ * Extract text from unicode files
  */
 final class UnicodeTextExtractor extends ContentTextExtractor {
 
+    //Set an upper limit on the amount of data in a single file to index
+    static final private int MAX_DATA_SIZE_BYTES = 100000000;
+    //Set a Minimum confidence value to reject matches that may not have a valid text encoding
+    //Values of valid text encodings were generally 100, xml code sometimes had a value around 50, 
+    //and pictures and other files with a .txt extention were showing up with a value of 5 or less in limited testing.
+    //This limited information was used to select the current value as one that would filter out clearly non-text 
+    //files while hopefully working on all files with a valid text encoding
+    static final private int MIN_MATCH_CONFIDENCE = 20;
     static final private Logger logger = Logger.getLogger(UnicodeTextExtractor.class.getName());
 
     @Override
@@ -49,11 +57,19 @@ final class UnicodeTextExtractor extends ContentTextExtractor {
     public Reader getReader(Content source) throws TextExtractorException {
         CharsetDetector detector = new CharsetDetector();
         ReadContentInputStream stream = new ReadContentInputStream(source);
-        byte[] byteData = new byte[(int) source.getSize()];
+        int size = (int) source.getSize();
+        if (size > MAX_DATA_SIZE_BYTES) {
+            size = MAX_DATA_SIZE_BYTES;
+            logger.log(Level.WARNING, "Text file size exceeded 100 mb, ony the first 100 mb has been indexed");
+        }
+        byte[] byteData = new byte[size];
         try {
-            stream.read(byteData, 0, (int) source.getSize());
+            stream.read(byteData, 0, size);
             detector.setText(byteData);
             CharsetMatch match = detector.detect();
+            if (match.getConfidence() < MIN_MATCH_CONFIDENCE) {
+                throw new TextExtractorException("Text does not match any character set with a high enough confidence for UnicodeTextExtractor");
+            }
             try {
                 return new StringReader(match.getString());
 
