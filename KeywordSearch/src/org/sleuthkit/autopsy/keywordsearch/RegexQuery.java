6c2ba70fa9174ddb0458c05de1278a0200601157
Richard Cordovano <rcordovano@basistech.com>
2018-08-20 17:57:52 -0400

    Revert "Accounting for associated artifact."

--- KeywordSearch/src/org/sleuthkit/autopsy/keywordsearch/RegexQuery.java
+++ KeywordSearch/src/org/sleuthkit/autopsy/keywordsearch/RegexQuery.java
@@ -466,9 +466,6 @@ final class RegexQuery implements KeywordSearchQuery {
         if (StringUtils.isNotBlank(listName)) {
             attributesList.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_SET_NAME, MODULE_NAME, listName));
         }
-        hit.getArtifactID().ifPresent(artifactID
-                -> attributesList.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_ASSOCIATED_ARTIFACT, MODULE_NAME, artifactID))
-        );
 
         try {
             SleuthkitCase tskCase = Case.getCurrentCaseThrows().getSleuthkitCase();
@@ -498,6 +495,10 @@ final class RegexQuery implements KeywordSearchQuery {
         if (snippet != null) {
             attributesList.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_KEYWORD_PREVIEW, MODULE_NAME, snippet));
         }
+
+        hit.getArtifactID().ifPresent(artifactID
+                -> attributesList.add(new BlackboardAttribute(ATTRIBUTE_TYPE.TSK_ASSOCIATED_ARTIFACT, MODULE_NAME, artifactID))
+        );
 
         try {
             newArtifact.addAttributes(attributesList);
