ba84b4c74bead012662cdd62dd310239835f4e99
Ann Priestman <apriestman@basistech.com>
2018-10-25 12:27:54 -0400

    Prepend the archive name to any images extracted in the archive DSP. Don't use the device ID for the name in the Local Files DSP.

--- Core/src/org/sleuthkit/autopsy/casemodule/LocalFilesDSProcessor.java
+++ Core/src/org/sleuthkit/autopsy/casemodule/LocalFilesDSProcessor.java
@@ -369,7 +369,7 @@ public class LocalFilesDSProcessor implements DataSourceProcessor, AutoIngestDat
     @Override
     public void process(String deviceId, Path dataSourcePath, DataSourceProcessorProgressMonitor progressMonitor, DataSourceProcessorCallback callBack) {
         List<String> filePaths = Arrays.asList(new String[]{dataSourcePath.toString()});
-        run(deviceId, deviceId, filePaths, progressMonitor, callBack);
+        run(deviceId, "", filePaths, progressMonitor, callBack);
     }
 
     /**
