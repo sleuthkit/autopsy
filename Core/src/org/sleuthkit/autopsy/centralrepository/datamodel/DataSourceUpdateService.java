ec94b8ecde8e2c4996f424c4c1604a442a8e32b3
William Schaefer <wschaefer@basistech.net>
2018-11-16 16:34:59 -0500

    4361 remove empty line white space change

--- Core/src/org/sleuthkit/autopsy/centralrepository/datamodel/DataSourceUpdateService.java
+++ Core/src/org/sleuthkit/autopsy/centralrepository/datamodel/DataSourceUpdateService.java
@@ -46,7 +46,7 @@ public class DataSourceUpdateService implements AutopsyService {
                 EamDb centralRepository = EamDb.getInstance();
                 CorrelationCase correlationCase = centralRepository.getCase(context.getCase());
                 //if the case isn't in the central repository yet there won't be data sources in it to update
-                if (correlationCase != null) { 
+                if (correlationCase != null) {
                     for (CorrelationDataSource correlationDataSource : centralRepository.getDataSources()) {
                         //ResultSet.getLong has a value of 0 when the value is null
                         if (correlationDataSource.getCaseID() == correlationCase.getID() && correlationDataSource.getDataSourceObjectID() == 0) {
@@ -56,7 +56,6 @@ public class DataSourceUpdateService implements AutopsyService {
                                     break;
                                 }
                             }
-
                         }
                     }
                 }
