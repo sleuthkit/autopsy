8a3693f39d08a10253f5598ba949f3e973ab2d75
U-BASIS\dsmyda <dsmyda@win-dsmyd-4990.basistech.net>
2018-12-03 14:50:46 -0500

    Made the file name less fun

--- Core/src/org/sleuthkit/autopsy/casemodule/Case.java
+++ Core/src/org/sleuthkit/autopsy/casemodule/Case.java
@@ -1122,7 +1122,7 @@ public class Case {
                  */
                 
                 //Deny ability to add a data source if the special admin access file is present.
-                File denyAddDataSourcePermissions = new File(PlatformUtil.getUserConfigDirectory(), "child_lock");
+                File denyAddDataSourcePermissions = new File(PlatformUtil.getUserConfigDirectory(), "adminAccess");
                 if(!denyAddDataSourcePermissions.exists()) {
                     CallableSystemAction.get(AddImageAction.class).setEnabled(true);
                 }
