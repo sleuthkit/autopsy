e4926c0c1979f6f5e34cae6ba18d7ead218be743
millmanorama <millmanorama@gmail.com>
2019-01-31 11:32:32 +0100

    update copyright headers

--- Core/test/unit/src/org/sleuthkit/autopsy/timeline/ui/filtering/datamodel/CompoundFilterStateImplTest.java
+++ Core/test/unit/src/org/sleuthkit/autopsy/timeline/ui/filtering/datamodel/CompoundFilterStateImplTest.java
@@ -1,22 +1,32 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Central Repository
+ *
+ * Copyright 2019 Basis Technology Corp.
+ * Contact: carrier <at> sleuthkit <dot> org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.sleuthkit.autopsy.timeline.ui.filtering.datamodel;
 
 import java.util.Arrays;
-import org.hamcrest.CoreMatchers;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.sleuthkit.datamodel.timeline.TimelineFilter;
 import org.sleuthkit.datamodel.timeline.TimelineFilter.CompoundFilter;
 import org.sleuthkit.datamodel.timeline.TimelineFilter.FileTypeFilter;
 import org.sleuthkit.datamodel.timeline.TimelineFilter.IntersectionFilter;
-import sun.font.CoreMetrics;
 
 public class CompoundFilterStateImplTest extends FilterStateTestAbstract<CompoundFilter<TimelineFilter>, CompoundFilterState<TimelineFilter, CompoundFilter<TimelineFilter>>> {
 
