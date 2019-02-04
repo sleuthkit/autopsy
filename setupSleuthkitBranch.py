0467bc2f261c8fd78a253bc4c5b2901569006d23
William Schaefer <wschaefer@basistech.net>
2019-02-04 12:25:00 -0500

    4690 first attmept to modify setupSleuthkitBranch script to get remote branches

--- setupSleuthkitBranch.py
+++ setupSleuthkitBranch.py
@@ -12,9 +12,12 @@ import subprocess
 import xml.etree.ElementTree as ET
 
 TSK_HOME=os.getenv("TSK_HOME",False)
+ORIGIN_OWNER="origin"
+DEVELOP_BRANCH='develop'
+
 passed = 1
 
-def getSleuthkitBranchList():
+def getSleuthkitBranchList(branchOwner):
     # Returns the list of sleuthkit branches
     cmd = ['git','branch','-a']
     retdir = os.getcwd()
@@ -22,12 +25,12 @@ def getSleuthkitBranchList():
     output = subprocess.check_output(cmd)
     branches = []
     for branch in output.strip().split():
-        if branch.startswith('remotes/origin'):
+        if branch.startswith('remotes/'+branchOwner):
             branches.append(branch.split('/')[2])
     os.chdir(retdir)
     return branches
 
-def gitSleuthkitCheckout(branch):
+def gitSleuthkitCheckout(branch, branchOwner):
     '''
         Checksout sleuthkit branch
         Args:
@@ -36,7 +39,19 @@ def gitSleuthkitCheckout(branch):
     # passed is a global variable that gets set to non-zero integer
     # When an error occurs
     global passed
-    cmd = ['git','checkout',branch]
+    if (branchOwner==ORIGIN_OWNER):
+        print("Checkinging out origin branch")
+        cmd = ['git','checkout', branch]
+    else:
+        #add the remotes
+        checkout=['git','checkout','-b',branchOwner+'-'+branch]
+        print("checking out "+branchOwner+"-"+branch)
+        passed = subprocess.call(checkout, stdout=sys.stdout,cwd=TSK_HOME)
+        cmd = ['git','pull', "/".join(["https://github.com", branchOwner, "sleuthkit.git"]), branch]
+        if !passed:
+            #unable to create new branch return instead of pulling
+            return
+    print("Perfroming CMD: "+ *cmd)
     passed = subprocess.call(cmd,stdout=sys.stdout,cwd=TSK_HOME)
 
 def parseXML(xmlFile):
@@ -65,28 +80,34 @@ def main():
     APPVEYOR=os.getenv("APPVEYOR",False)
     if TRAVIS == "true":
         CURRENT_BRANCH=os.getenv("TRAVIS_BRANCH",False)
+        BRANCH_OWNER=os.getenv("TRAVIS_PULL_REQUEST_SLUG", False).split('/')[0]
     elif APPVEYOR:
         CURRENT_BRANCH=os.getenv("APPVEYOR_REPO_BRANCH",False)
+        BRANCH_OWNER=os.getenv("APPVEYOR_REPO_NAME", False).split('/')[0]
     else:
         cmd=['git','rev-parse','--abbrev-ref','HEAD']
         output = subprocess.check_output(cmd)
         CURRENT_BRANCH=output.strip()
-        
+        BRANCH_OWNER=ORIGIN_OWNER
+    print("Current Branch: " + CURRENT_BRANCH)
+    print("Branch owner: " + BRANCH_OWNER)
     # If we are in an Autopsy release branch, then use the
     # info in TSKVersion.xml to find the corresponding TSK 
     # release branch.  For other branches, we don't always
     # trust that TSKVersion has been updated.
     if CURRENT_BRANCH.startswith('release'):
+        print("branch starts with release")
         version = parseXML('TSKVersion.xml')
         RELEASE_BRANCH = "release-"+version
-        gitSleuthkitCheckout(RELEASE_BRANCH)
+        gitSleuthkitCheckout(RELEASE_BRANCH, BRANCH_OWNER)
     # Check if the same branch exists in TSK (develop->develop, custom1->custom1, etc.)
-    elif CURRENT_BRANCH in getSleuthkitBranchList(): 
-        gitSleuthkitCheckout(CURRENT_BRANCH)
+    elif CURRENT_BRANCH in getSleuthkitBranchList(BRANCH_OWNER): 
+        print("current branch found")
+        gitSleuthkitCheckout(CURRENT_BRANCH, BRANCH_OWNER)
 
-    # Otherwise, default to develop
+    # Otherwise, default to origin develop
     if passed != 0:
-        gitSleuthkitCheckout('develop')
+        gitSleuthkitCheckout(DEVELOP_BRANCH, ORIGIN_OWNER)
         
     if passed != 0:
         print('Error checking out a Sleuth Kit branch')
