0219607f8a0af19e20008081b9e11c856f2da34b
Richard Cordovano <rcordovano@basistech.com>
2019-02-23 14:04:12 -0500

    Revert "4589 gst video panel"

--- Core/src/org/sleuthkit/autopsy/contentviewers/MediaViewImagePanel.java
+++ Core/src/org/sleuthkit/autopsy/contentviewers/MediaViewImagePanel.java
@@ -245,7 +245,7 @@ class MediaViewImagePanel extends JPanel implements MediaFileViewer.MediaViewPan
      * @return supported mime types
      */
     @Override
-    public List<String> getSupportedMimeTypes() {
+    public List<String> getMimeTypes() {
         return Collections.unmodifiableList(Lists.newArrayList(supportedMimes));
     }
 
@@ -255,7 +255,7 @@ class MediaViewImagePanel extends JPanel implements MediaFileViewer.MediaViewPan
      * @return
      */
     @Override
-    public List<String> getSupportedExtensions() {
+    public List<String> getExtensionsList() {
         return getExtensions();
     }
 
