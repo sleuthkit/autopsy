1e1e01098082d63cf6aade95eeeb33994626e10c
Kelly Kelly <kelly@basistech.com>
2019-02-25 10:05:17 -0500

    Made changes based on review comments, including the addition of NON-NLS and changing hashtable to hashmap

--- RecentActivity/src/org/sleuthkit/autopsy/recentactivity/Extract.java
+++ RecentActivity/src/org/sleuthkit/autopsy/recentactivity/Extract.java
@@ -201,11 +201,19 @@ abstract class Extract {
         return moduleName;
     }
 
+    /**
+     * Returns the state of foundData
+     * @return 
+     */
     public boolean foundData() {
         return dataFound;
     }
     
-    protected void setFoundData(boolean b){
-        dataFound = b;
+    /**
+     * Sets the value of foundData
+     * @param foundData 
+     */
+    protected void setFoundData(boolean foundData){
+        dataFound = foundData;
     }
 }
