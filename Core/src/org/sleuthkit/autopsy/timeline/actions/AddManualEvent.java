52372f85ac7dd8cfb1d7c07b1daf8114d3882d54
millmanorama <millmanorama@gmail.com>
2019-02-28 16:41:29 +0100

    add datasource chooser

--- Core/src/org/sleuthkit/autopsy/timeline/actions/AddManualEvent.java
+++ Core/src/org/sleuthkit/autopsy/timeline/actions/AddManualEvent.java
@@ -20,7 +20,6 @@ package org.sleuthkit.autopsy.timeline.actions;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -30,15 +29,17 @@ import java.util.logging.Level;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.StringConverter;
+import javax.swing.SwingUtilities;
 import jfxtras.scene.control.LocalDateTimeTextField;
 import org.apache.commons.lang3.StringUtils;
-import static org.apache.commons.lang3.StringUtils.substringAfter;
-import static org.apache.commons.lang3.StringUtils.substringBefore;
 import org.controlsfx.control.action.Action;
 import org.controlsfx.control.textfield.TextFields;
 import org.controlsfx.tools.ValueExtractor;
@@ -46,6 +47,7 @@ import org.controlsfx.validation.ValidationSupport;
 import org.controlsfx.validation.Validator;
 import org.openide.util.NbBundle;
 import org.sleuthkit.autopsy.coreutils.Logger;
+import org.sleuthkit.autopsy.coreutils.MessageNotifyUtil;
 import org.sleuthkit.autopsy.coreutils.TimeZoneUtils;
 import org.sleuthkit.autopsy.timeline.FXMLConstructor;
 import org.sleuthkit.autopsy.timeline.PromptDialogManager;
@@ -56,6 +58,7 @@ import org.sleuthkit.datamodel.BlackboardAttribute;
 import static org.sleuthkit.datamodel.BlackboardAttribute.ATTRIBUTE_TYPE.TSK_DATETIME;
 import static org.sleuthkit.datamodel.BlackboardAttribute.ATTRIBUTE_TYPE.TSK_DESCRIPTION;
 import static org.sleuthkit.datamodel.BlackboardAttribute.ATTRIBUTE_TYPE.TSK_TL_EVENT_TYPE;
+import org.sleuthkit.datamodel.DataSource;
 import org.sleuthkit.datamodel.SleuthkitCase;
 import org.sleuthkit.datamodel.TskCoreException;
 import org.sleuthkit.datamodel.timeline.EventType;
@@ -64,13 +67,13 @@ import org.sleuthkit.datamodel.timeline.EventType;
  * Action that prompts the user for the event data and then adds it to the case
  * via an artifact.
  */
-public class CreateManualEvent extends Action {
+public class AddManualEvent extends Action {
 
-    private final static Logger logger = Logger.getLogger(CreateManualEvent.class.getName());
+    private final static Logger logger = Logger.getLogger(AddManualEvent.class.getName());
+    private static final String MANUAL_CREATION = "Manual Creation";
+    private static final Image ADD_EVENT_IMAGE = new Image("/org/sleuthkit/autopsy/timeline/images/add.png", 16, 16, true, true, true); // NON-NLS
 
     private final TimeLineController controller;
-
-    private static final String MANUAL_CREATION = "Manual Creation";
 
     static {
         ValueExtractor.addObservableValueExtractor(LocalDateTimeTextField.class::isInstance,
@@ -80,19 +83,19 @@ public class CreateManualEvent extends Action {
     @NbBundle.Messages({
         "CreateManualEvent.text=Add Event",
         "CreateManualEvent.longText=Manually add an event to the timeline."})
-    public CreateManualEvent(TimeLineController controller) {
+    public AddManualEvent(TimeLineController controller) {
         super(Bundle.CreateManualEvent_text());
         this.controller = controller;
-//        setGraphic(value);
+        setGraphic(new ImageView(ADD_EVENT_IMAGE));
         setLongText(Bundle.CreateManualEvent_longText());
-        setEventHandler(actionEvent -> new EventCreationDialog().showAndWait().ifPresent(this::addEvent));
+        setEventHandler(actionEvent -> new EventCreationDialog(controller).showAndWait().ifPresent(this::addEvent));
     }
 
     private void addEvent(ManualEventInfo eventInfo) throws IllegalArgumentException {
         SleuthkitCase sleuthkitCase = controller.getEventsModel().getSleuthkitCase();
 
         try {
-            BlackboardArtifact artifact = sleuthkitCase.newBlackboardArtifact(BlackboardArtifact.ARTIFACT_TYPE.TSK_TL_EVENT, 1);
+            BlackboardArtifact artifact = sleuthkitCase.newBlackboardArtifact(BlackboardArtifact.ARTIFACT_TYPE.TSK_TL_EVENT, eventInfo.datasource.getId());
             Collection<BlackboardAttribute> attributes = Arrays.asList(
                     new BlackboardAttribute(
                             TSK_TL_EVENT_TYPE, MANUAL_CREATION,
@@ -113,12 +116,16 @@ public class CreateManualEvent extends Action {
         }
     }
 
+    /**
+     * The dialog that allows the user to enter the event information.
+     */
     private static class EventCreationDialog extends Dialog<ManualEventInfo> {
 
-        private final EventCreationDialogPane eventCreationDialogPane = new EventCreationDialogPane();
+        private final EventCreationDialogPane eventCreationDialogPane;
 
-        EventCreationDialog() {
-            setHeaderText("Add Event");
+        EventCreationDialog(TimeLineController controller) {
+            this.eventCreationDialogPane = new EventCreationDialogPane(controller);
+            setTitle("Add Event");
             setDialogPane(eventCreationDialogPane);
             setOnShown(dialogEvent -> {
                 Platform.runLater(() -> {
@@ -134,8 +141,14 @@ public class CreateManualEvent extends Action {
             );
         }
 
+        /**
+         * The dialog pane that hosts the controls that allows the user to enter
+         * the event information.
+         */
         static private class EventCreationDialogPane extends DialogPane {
 
+            @FXML
+            private ChoiceBox<DataSource> dataSourceChooser;
             @FXML
             private TextField descriptionTextField;
             @FXML
@@ -145,12 +158,15 @@ public class CreateManualEvent extends Action {
 
             private final List<String> timeZoneList = TimeZoneUtils.createTimeZoneList();
             private final ValidationSupport validationSupport = new ValidationSupport();
+            private final TimeLineController controller;
 
-            EventCreationDialogPane() {
+            EventCreationDialogPane(TimeLineController controller) {
+                this.controller = controller;
                 FXMLConstructor.construct(this, "EventCreationDialog.fxml");
             }
 
             @FXML
+            @NbBundle.Messages("AddManualEvent.EventCreationDialogPane.initialize.dataSourcesError=Error getting datasources in case.")
             void initialize() {
                 assert descriptionTextField != null : "fx:id=\"descriptionTextField\" was not injected: check your FXML file 'EventCreationDialog.fxml'.";
 
@@ -160,12 +176,31 @@ public class CreateManualEvent extends Action {
 
                 timePicker.setLocalDateTime(LocalDateTime.now());
 
+                try {
+                    dataSourceChooser.getItems().setAll(controller.getAutopsyCase().getSleuthkitCase().getDataSources());
+
+                } catch (TskCoreException ex) {
+                    logger.log(Level.SEVERE, "Error getting datasources in case.", ex);
+                    SwingUtilities.invokeLater(() -> MessageNotifyUtil.Message.error(Bundle.AddManualEvent_EventCreationDialogPane_initialize_dataSourcesError()));
+                }
+                dataSourceChooser.getSelectionModel().select(0);
+                dataSourceChooser.setConverter(new StringConverter<DataSource>() {
+                    @Override
+                    public String toString(DataSource dataSource) {
+                        return dataSource.getName() + "(ID: " + dataSource.getId() + ")";
+                    }
+
+                    @Override
+                    public DataSource fromString(String string) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+                });
             }
 
             void installValidation() {
-                validationSupport.registerValidator(descriptionTextField, Validator.createEmptyValidator("Description is required"));
-                validationSupport.registerValidator(timePicker, Validator.createPredicateValidator(Objects::nonNull, "Invalid datetime"));
-                validationSupport.registerValidator(timeZoneChooser, Validator.createPredicateValidator((String zone) -> timeZoneList.contains(zone.trim()), "Invalid time zone"));
+                validationSupport.registerValidator(descriptionTextField, false, Validator.createEmptyValidator("Description is required"));
+                validationSupport.registerValidator(timePicker, false, Validator.createPredicateValidator(Objects::nonNull, "Invalid datetime"));
+                validationSupport.registerValidator(timeZoneChooser, false, Validator.createPredicateValidator((String zone) -> timeZoneList.contains(zone.trim()), "Invalid time zone"));
 
                 validationSupport.initInitialDecoration();
 
@@ -175,7 +210,7 @@ public class CreateManualEvent extends Action {
             ManualEventInfo getManualEventInfo() {
                 String zone = StringUtils.substringAfter(timeZoneChooser.getValue(), ")").trim();
                 long toEpochSecond = timePicker.getLocalDateTime().atZone(ZoneId.of(zone)).toEpochSecond();
-                return new ManualEventInfo(descriptionTextField.getText(), toEpochSecond);
+                return new ManualEventInfo(dataSourceChooser.getValue(), descriptionTextField.getText(), toEpochSecond);
             }
         }
     }
@@ -185,10 +220,12 @@ public class CreateManualEvent extends Action {
      */
     private static class ManualEventInfo {
 
+        DataSource datasource;
         String description;
         long time;
 
-        ManualEventInfo(String description, long time) {
+        ManualEventInfo(DataSource datasource, String description, long time) {
+            this.datasource = datasource;
             this.description = description;
             this.time = time;
         }
