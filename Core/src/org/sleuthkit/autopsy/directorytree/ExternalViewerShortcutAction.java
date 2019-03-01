31dc94d794dc97cc48ea433090d359bab22345a4
William Schaefer <wschaefer@basistech.net>
2019-03-01 17:17:06 -0500

    4755 Add Shortcut for opening files in external viewer Ctrl+E

---
+++ Core/src/org/sleuthkit/autopsy/directorytree/ExternalViewerShortcutAction.java
@@ -0,0 +1,57 @@
+/*
+ * Autopsy Forensic Browser
+ * 
+ * Copyright 2011-2018 Basis Technology Corp.
+ * Contact: carrier <at> sleuthkit <dot> org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sleuthkit.autopsy.directorytree;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.Collection;
+import java.util.HashSet;
+import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
+import org.openide.util.NbBundle.Messages;
+import org.openide.util.Utilities;
+import org.sleuthkit.datamodel.AbstractFile;
+
+/**
+ * Extracts a File object to a temporary file in the case directory, and then
+ * tries to open it in the user's system with the default or user specified
+ * associated application.
+ */
+@Messages({"ExternalViewerShortcutAction.title.text=Open in External Viewer"})
+public class ExternalViewerShortcutAction extends AbstractAction {
+    public static final KeyStroke EXTERNAL_VIEWER_SHORTCUT = KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.CTRL_MASK);
+    
+    public ExternalViewerShortcutAction() {
+        super(Bundle.ExternalViewerShortcutAction_title_text());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+          final Collection<AbstractFile> selectedFiles = new HashSet<>(Utilities.actionsGlobalContext().lookupAll(AbstractFile.class));
+          if (!selectedFiles.isEmpty()) {
+              for (AbstractFile file : selectedFiles){
+                  ExternalViewerAction action = new ExternalViewerAction(Bundle.ExternalViewerShortcutAction_title_text(), file, false);              
+                  if(action.isEnabled()){
+                      action.actionPerformed(e);
+                  }
+              }
+          }
+    }
+}
