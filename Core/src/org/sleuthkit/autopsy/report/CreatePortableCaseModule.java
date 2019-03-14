21bf07b5a92f186e33690e8c122c0983d93bc8f4
Ann Priestman <apriestman@basistech.com>
2019-03-14 09:30:39 -0400

    Added auto ingest setup and admin pages. Fixed doxygen warnings.

--- Core/src/org/sleuthkit/autopsy/report/CreatePortableCaseModule.java
+++ Core/src/org/sleuthkit/autopsy/report/CreatePortableCaseModule.java
@@ -484,7 +484,7 @@ public class CreatePortableCaseModule implements GeneralReportModule {
      * Get the artifact type ID in the portable case and create new artifact type if needed.
      * For built-in artifacts this will be the same as the original.
      * 
-     * @param oldArtifactTypeId The artifact type ID in the current case
+     * @param oldArtifact The artifact in the current case
      * 
      * @return The corresponding artifact type ID in the portable case
      */
@@ -507,7 +507,7 @@ public class CreatePortableCaseModule implements GeneralReportModule {
      * Get the attribute type ID in the portable case and create new attribute type if needed.
      * For built-in attributes this will be the same as the original.
      * 
-     * @param oldAttributeTypeId The attribute type ID in the current case
+     * @param oldAttribute The attribute in the current case
      * 
      * @return The corresponding attribute type in the portable case
      */
