c900746b48ccd5b1f205ae18607fab787082ff76
millmanorama <millmanorama@gmail.com>
2019-03-27 12:34:21 +0100

    Fix method javadoc

--- Core/src/org/sleuthkit/autopsy/timeline/FilteredEventsModel.java
+++ Core/src/org/sleuthkit/autopsy/timeline/FilteredEventsModel.java
@@ -1,7 +1,7 @@
 /*
  * Autopsy Forensic Browser
  *
- * Copyright 2011-2018 Basis Technology Corp.
+ * Copyright 2011-2019 Basis Technology Corp.
  * Contact: carrier <at> sleuthkit <dot> org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -342,7 +342,7 @@ public final class FilteredEventsModel {
         return getZoomState().getTypeZoomLevel();
     }
 
-    /** S
+    /** Get the default filter used at startup.
      *
      * @return the default filter used at startup
      */
