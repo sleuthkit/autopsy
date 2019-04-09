f1962bd082ecdf5c5d863a6c6c8ce839d12a4303
Kelly Kelly <kelly@basistech.com>
2019-04-09 10:34:55 -0400

    Inital POC of new Communications Relationship Panel

---
+++ Core/src/org/sleuthkit/autopsy/communications/RelationshipsViewer.java
@@ -0,0 +1,34 @@
+/*
+ * Autopsy Forensic Browser
+ *
+ * Copyright 2019 Basis Technology Corp.
+ * Contact: carrier <at> sleuthkit <dot> org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obt ain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sleuthkit.autopsy.communications;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * 
+ */
+public interface RelationshipsViewer {
+   
+    public String getDisplayName();
+    
+    public JPanel getPanel();
+    
+    public void setSelectionInfo(SelectionInfo info); 
+}
