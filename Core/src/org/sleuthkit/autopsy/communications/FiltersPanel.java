b1158de9cca712dd2092f2596e7e79eabba76ce7
Kelly Kelly <kelly@basistech.com>
2019-05-08 13:15:51 -0400

    Added code to initalize the start and end date filters to earliest and latest in case

--- Core/src/org/sleuthkit/autopsy/communications/FiltersPanel.java
+++ Core/src/org/sleuthkit/autopsy/communications/FiltersPanel.java
@@ -22,8 +22,13 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.eventbus.Subscribe;
 import java.awt.event.ItemListener;
 import java.beans.PropertyChangeListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.Collection;
 import java.util.EnumSet;
 import java.util.HashMap;
@@ -34,6 +39,7 @@ import java.util.logging.Level;
 import java.util.stream.Collectors;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
+import org.openide.util.Exceptions;
 import org.openide.util.NbBundle;
 import org.sleuthkit.autopsy.casemodule.Case;
 import static org.sleuthkit.autopsy.casemodule.Case.Events.CURRENT_CASE;
@@ -46,6 +52,7 @@ import static org.sleuthkit.autopsy.ingest.IngestManager.IngestModuleEvent.DATA_
 import org.sleuthkit.autopsy.ingest.ModuleDataEvent;
 import org.sleuthkit.datamodel.Account;
 import org.sleuthkit.datamodel.BlackboardArtifact;
+import org.sleuthkit.datamodel.CaseDbAccessManager.CaseDbAccessQueryCallback;
 import org.sleuthkit.datamodel.CommunicationsFilter;
 import org.sleuthkit.datamodel.CommunicationsFilter.AccountTypeFilter;
 import org.sleuthkit.datamodel.CommunicationsFilter.DateRangeFilter;
@@ -150,6 +157,29 @@ final public class FiltersPanel extends JPanel {
 
         applyFiltersButton.addActionListener(e -> applyFilters());
         refreshButton.addActionListener(e -> applyFilters());
+        
+        try {
+            String queryString = "max(date_time) as max,  min(date_time) as min from account_relationships"; // NON-NLS
+            Case.getCurrentCaseThrows().getSleuthkitCase().getCaseDbAccessManager().select(queryString, new FilterPanelQueryCallback() {
+                @Override
+                public void process(ResultSet rs) {
+                    try {
+                        if (rs.next()) {
+                            int startDate = rs.getInt("min"); // NON-NLS
+                            int endData = rs.getInt("max"); // NON-NLS
+
+                            startDatePicker.setDate(LocalDateTime.ofInstant(Instant.ofEpochSecond(startDate), Utils.getUserPreferredZoneId()).toLocalDate());
+                            endDatePicker.setDate(LocalDateTime.ofInstant(Instant.ofEpochSecond(endData), Utils.getUserPreferredZoneId()).toLocalDate());
+                        }
+                    } catch (SQLException ex) {
+                        logger.log(Level.WARNING, "Unable to set filter date pickers due to SQL exception", ex); //NON-NLS
+                    }
+                }
+
+            });
+        } catch (NoCurrentCaseException | TskCoreException ex) {
+            logger.log(Level.SEVERE, "Unable to set filter date pickers due to exception", ex); //NON-NLS
+        }
     }
 
     /**
@@ -788,4 +818,14 @@ final public class FiltersPanel extends JPanel {
     private final javax.swing.JButton unCheckAllAccountTypesButton = new javax.swing.JButton();
     private final javax.swing.JButton unCheckAllDevicesButton = new javax.swing.JButton();
     // End of variables declaration//GEN-END:variables
+
+    class FilterPanelQueryCallback implements CaseDbAccessQueryCallback {
+
+        @Override
+        public void process(ResultSet rs) {
+            
+        }
+        
+    }
+    
 }
