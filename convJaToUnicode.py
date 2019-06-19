245f491afaaccee01236278b64b3bfcb71766bfa
Ethan Roseman <eroseman@basistech.com>
2019-06-19 10:01:12 +0900

    changes to conv

--- convJaToUnicode.py
+++ convJaToUnicode.py
@@ -14,10 +14,18 @@ def main():
     for root, dirs, files in os.walk("."):
         for file in files:
             if file == "Bundle_ja.properties":
-                with open(root + "/" + file, "r", encoding=from_encoding) as f:
-                    file_text = f.read()
-                with open(root + "/" + file, "w", encoding=to_encoding) as f:
-                    f.write(file_text)
+                with open(root + "/" + file, "r", encoding=from_encoding) as fin:
+                    file_text = fin.read()
+                    if len(file_text) > 0 and file_text != "\\n" and file_text != "\n":
+                        if reverse:
+                            with open(root + "/" + file, "w") as fout:
+                                file_lines = file_text.splitlines()
+                                for line in file_lines:
+                                    encoded_line = line.encode(to_encoding).decode("ascii") + "\n"
+                                    fout.write(encoded_line)
+                        else: 
+                            with open(root + "/" + file, "w", encoding=to_encoding) as fout:
+                                fout.write(file_text)
 
 
 if __name__ == '__main__':
