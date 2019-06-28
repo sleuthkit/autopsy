3d11b7e6825a67592834fcfd200780ace2d55eac
Joe Ho <jkho@basistech.com>
2019-06-27 21:28:28 -0400

    Initial code changes.

--- Core/src/org/sleuthkit/autopsy/logicalimager/dsp/AddMultipleImageTask.java
+++ Core/src/org/sleuthkit/autopsy/logicalimager/dsp/AddMultipleImageTask.java
@@ -18,26 +18,23 @@
  */
 package org.sleuthkit.autopsy.logicalimager.dsp;
 
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import org.openide.util.NbBundle.Messages;
 import org.sleuthkit.autopsy.casemodule.Case;
 import org.sleuthkit.autopsy.casemodule.NoCurrentCaseException;
-import org.sleuthkit.autopsy.casemodule.services.FileManager;
 import org.sleuthkit.autopsy.corecomponentinterfaces.DataSourceProcessorCallback;
 import org.sleuthkit.autopsy.corecomponentinterfaces.DataSourceProcessorCallback.DataSourceProcessorResult;
 import org.sleuthkit.autopsy.corecomponentinterfaces.DataSourceProcessorProgressMonitor;
 import org.sleuthkit.autopsy.coreutils.Logger;
-import org.sleuthkit.datamodel.AbstractFile;
 import org.sleuthkit.datamodel.Content;
 import org.sleuthkit.datamodel.Image;
-import org.sleuthkit.datamodel.LocalFilesDataSource;
 import org.sleuthkit.datamodel.SleuthkitCase;
 import org.sleuthkit.datamodel.SleuthkitJNI;
 import org.sleuthkit.datamodel.TskCoreException;
 import org.sleuthkit.datamodel.TskDataException;
+import org.sleuthkit.datamodel.TskFileRange;
 
 /**
  *
@@ -51,12 +48,15 @@ class AddMultipleImageTask implements Runnable {
 
     private static final Logger LOGGER = Logger.getLogger(AddMultipleImageTask.class.getName());
     public static final String TSK_FS_TYPE_UNKNOWN_ERR_MSG = Bundle.AddMultipleImageTask_fsTypeUnknownErr();
+    private static final long TWO_GB = 2000000000L;
     private final String deviceId;
     private final List<String> imageFilePaths;
     private final String timeZone;
+    private final long chunkSize = TWO_GB;
     private final DataSourceProcessorProgressMonitor progressMonitor;
     private final DataSourceProcessorCallback callback;
     private final Case currentCase;
+
     private boolean criticalErrorOccurred;
     private volatile boolean cancelled;
 
@@ -79,7 +79,7 @@ class AddMultipleImageTask implements Runnable {
      * @throws NoCurrentCaseException The exception if there is no open case.
      */
     @Messages({
-        "# {0} - file", "AddMultipleImageTask.addingFileAsLogicalFile=Adding: {0} as logical file",
+        "# {0} - file", "AddMultipleImageTask.addingFileAsLogicalFile=Adding: {0} as an unallocated space file.",
         "# {0} - deviceId", "# {1} - exceptionMessage",
         "AddMultipleImageTask.errorAddingImgWithoutFileSystem=Error adding images without file systems for device %s: %s",})
     AddMultipleImageTask(String deviceId, List<String> imageFilePaths, String timeZone,
@@ -98,14 +98,14 @@ class AddMultipleImageTask implements Runnable {
          * Try to add the input image files as images.
          */
         List<Content> newDataSources = new ArrayList<>();
-        List<String> localFileDataSourcePaths = new ArrayList<>();
+        List<String> corruptedImageFilePaths = new ArrayList<>();
         List<String> errorMessages = new ArrayList<>();
         currentCase.getSleuthkitCase().acquireSingleUserCaseWriteLock();
         try {
             progressMonitor.setIndeterminate(true);
             for (String imageFilePath : imageFilePaths) {
                 if (!cancelled) {
-                    addImageToCase(imageFilePath, newDataSources, localFileDataSourcePaths, errorMessages);
+                    addImageToCase(imageFilePath, newDataSources, corruptedImageFilePaths, errorMessages);
                 }
             }
         } finally {
@@ -114,20 +114,41 @@ class AddMultipleImageTask implements Runnable {
 
         /*
          * Try to add any input image files that did not have file systems as a
-         * single local/logical files set with the device id as the root virtual
+         * single an unallocated space file with the device id as the root virtual
          * directory name.
          */
-        if (!cancelled && !localFileDataSourcePaths.isEmpty()) {
-            FileManager fileManager = currentCase.getServices().getFileManager();
-            FileManager.FileAddProgressUpdater progressUpdater = (final AbstractFile newFile) -> {
-                progressMonitor.setProgressText(Bundle.AddMultipleImageTask_addingFileAsLogicalFile(Paths.get(newFile.getParentPath(), newFile.getName())));
-            };
+        if (!cancelled && !corruptedImageFilePaths.isEmpty()) {
+            SleuthkitCase caseDatabase;
+            caseDatabase = currentCase.getSleuthkitCase();
             try {
-                LocalFilesDataSource localFilesDataSource = fileManager.addLocalFilesDataSource(deviceId, "", timeZone, localFileDataSourcePaths, progressUpdater);
-                newDataSources.add(localFilesDataSource);
-            } catch (TskCoreException | TskDataException ex) {
+                progressMonitor.setProgressText(Bundle.AddMultipleImageTask_addingFileAsLogicalFile(corruptedImageFilePaths.toString()));
+
+                caseDatabase.acquireSingleUserCaseWriteLock();
+
+                Image dataSource = caseDatabase.addImageInfo(0, corruptedImageFilePaths, timeZone);
+                List<TskFileRange> fileRanges = new ArrayList<>();
+
+                long imageSize = dataSource.getSize();
+                int sequence = 0;
+                //start byte and end byte
+                long start = 0;
+                if (chunkSize > 0 && imageSize >= TWO_GB) {
+                    for (double size = TWO_GB; size < dataSource.getSize(); size += TWO_GB) {
+                        fileRanges.add(new TskFileRange(start, TWO_GB, sequence));
+                        start += TWO_GB;
+                        sequence++;
+                    }
+
+                } 
+                double leftoverSize = imageSize - sequence * TWO_GB;
+                fileRanges.add(new TskFileRange(start, (long)leftoverSize, sequence));
+
+                caseDatabase.addLayoutFiles(dataSource, fileRanges);
+            } catch (TskCoreException ex) {
                 errorMessages.add(Bundle.AddMultipleImageTask_errorAddingImgWithoutFileSystem(deviceId, ex.getLocalizedMessage()));
                 criticalErrorOccurred = true;
+            } finally {
+                caseDatabase.releaseSingleUserCaseWriteLock();
             }
         }
 
@@ -169,11 +190,10 @@ class AddMultipleImageTask implements Runnable {
      * @param newDataSources           If the image is added, a data source is
      *                                 added to this list for eventual return to
      *                                 the caller via the callback.
-     * @param localFileDataSourcePaths If the image cannot be added because
+     * @param corruptedImageFilePaths  If the image cannot be added because
      *                                 Sleuth Kit cannot detect a filesystem,
      *                                 the image file path is added to this list
-     *                                 for later addition as a part of a
-     *                                 local/logical files data source.
+     *                                 for later addition as an unallocated space file.
      * @param errorMessages            If there are any error messages, the
      *                                 error messages are added to this list for
      *                                 eventual return to the caller via the
@@ -184,7 +204,7 @@ class AddMultipleImageTask implements Runnable {
         "# {0} - imageFilePath", "# {1} - deviceId", "# {2} - exceptionMessage", "AddMultipleImageTask.criticalErrorAdding=Critical error adding {0} for device {1}: {2}",
         "# {0} - imageFilePath", "# {1} - deviceId", "# {2} - exceptionMessage", "AddMultipleImageTask.criticalErrorReverting=Critical error reverting add image process for {0} for device {1}: {2}",
         "# {0} - imageFilePath", "# {1} - deviceId", "# {2} - exceptionMessage", "AddMultipleImageTask.nonCriticalErrorAdding=Non-critical error adding {0} for device {1}: {2}",})
-    private void addImageToCase(String imageFilePath, List<Content> newDataSources, List<String> localFileDataSourcePaths, List<String> errorMessages) {
+    private void addImageToCase(String imageFilePath, List<Content> newDataSources, List<String> corruptedImageFilePaths, List<String> errorMessages) {
         /*
          * Try to add the image to the case database as a data source.
          */
@@ -198,10 +218,10 @@ class AddMultipleImageTask implements Runnable {
                 /*
                  * If Sleuth Kit failed to add the image because it did not find
                  * a file system, save the image path so it can be added to the
-                 * case as part of a local/logical files data source. All other
+                 * case as an unallocated space file. All other
                  * errors are critical.
                  */
-                localFileDataSourcePaths.add(imageFilePath);
+                corruptedImageFilePaths.add(imageFilePath);
             } else {
                 errorMessages.add(Bundle.AddMultipleImageTask_criticalErrorAdding(imageFilePath, deviceId, ex.getLocalizedMessage()));
                 criticalErrorOccurred = true;
