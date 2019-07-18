a564d40918341ad376575346c3f1a5a7c24a97df
William Schaefer <wschaefer@basistech.net>
2019-07-18 10:21:42 -0400

    5114 addition clean up after review changes

--- Core/src/org/sleuthkit/autopsy/filequery/SearchWorker.java
+++ Core/src/org/sleuthkit/autopsy/filequery/SearchWorker.java
@@ -20,7 +20,6 @@ package org.sleuthkit.autopsy.filequery;
 
 import javax.swing.JButton;
 import javax.swing.SwingWorker;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import org.sleuthkit.autopsy.casemodule.Case;
