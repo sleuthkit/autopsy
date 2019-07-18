dd9ee9aa9a4bd36de4d42773b4bab6c7698fceaa
U-BASIS\dsmyda <dsmyda@win-dsmyd-4990.basistech.net>
2019-07-18 14:48:06 -0400

    Added files affected by refactor

--- Core/src/org/sleuthkit/autopsy/coreutils/FileTypeUtils.java
+++ Core/src/org/sleuthkit/autopsy/coreutils/FileTypeUtils.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.sleuthkit.autopsy.datamodel.utils;
+package org.sleuthkit.autopsy.coreutils;
 
 import com.google.common.collect.ImmutableSet;
 import static java.util.Arrays.asList;
