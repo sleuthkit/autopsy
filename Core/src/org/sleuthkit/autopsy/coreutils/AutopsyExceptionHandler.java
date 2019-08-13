944a961da596d82a3a2bc81ea6dd52dd39da5d58
William Schaefer <wschaefer@basistech.net>
2019-08-13 14:18:54 -0400

    5373 move exception handler back to coreutils

--- Core/src/org/sleuthkit/autopsy/coreutils/AutopsyExceptionHandler.java
+++ Core/src/org/sleuthkit/autopsy/coreutils/AutopsyExceptionHandler.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.sleuthkit.autopsy.exceptions;
+package org.sleuthkit.autopsy.coreutils;
 
 import java.util.logging.Filter;
 import java.util.logging.Handler;
