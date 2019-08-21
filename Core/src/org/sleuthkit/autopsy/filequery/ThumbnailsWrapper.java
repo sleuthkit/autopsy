3e13c3e4d2509f76adc698281f09feccd3ef41bf
William Schaefer <wschaefer@basistech.net>
2019-08-21 13:01:34 -0400

    5372 adjust text under video thumbnails

--- Core/src/org/sleuthkit/autopsy/filequery/ThumbnailsWrapper.java
+++ Core/src/org/sleuthkit/autopsy/filequery/ThumbnailsWrapper.java
@@ -1,7 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Autopsy
+ *
+ * Copyright 2019 Basis Technology Corp.
+ * Contact: carrier <at> sleuthkit <dot> org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.sleuthkit.autopsy.filequery;
 
@@ -9,6 +22,7 @@ import java.awt.Image;
 import java.util.Collections;
 import java.util.List;
 import org.sleuthkit.datamodel.AbstractFile;
+import org.sleuthkit.datamodel.TskCoreException;
 
 /**
  *
@@ -26,16 +40,20 @@ public class ThumbnailsWrapper {
         this.abstractFile = file;
     }
 
-    AbstractFile getAbstractFile(){
+    AbstractFile getAbstractFile() {
         return abstractFile;
     }
-    
-    int[] getTimeStamps(){
+
+    int[] getTimeStamps() {
         return timeStamps.clone();
     }
-    
+
     String getFileInfo() {
-        return abstractFile.getLocalAbsPath();
+        try {
+            return abstractFile.getUniquePath();
+        } catch (TskCoreException ingored) {
+            return abstractFile.getParentPath() + "/" + abstractFile.getName();
+        }
     }
 
     List<Image> getThumbnails() {
