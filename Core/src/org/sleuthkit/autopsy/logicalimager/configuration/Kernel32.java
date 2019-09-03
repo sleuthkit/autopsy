f680dddd345297d9b117336744a39a04e4f1fcf4
Joe Ho <jkho@basistech.com>
2019-09-03 10:17:05 -0400

    Fix codacy errors

--- Core/src/org/sleuthkit/autopsy/logicalimager/configuration/Kernel32.java
+++ Core/src/org/sleuthkit/autopsy/logicalimager/configuration/Kernel32.java
@@ -1,7 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Autopsy
+ *
+ * Copyright 2019 Basis Technology Corp.
+ * Contact: carrier <at> sleuthkit <dot> org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.sleuthkit.autopsy.logicalimager.configuration;
 
@@ -15,19 +28,20 @@ import com.sun.jna.win32.W32APITypeMapper;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+ * Windows Kernel32 interface
+ */
 public interface Kernel32 extends StdCallLibrary {
 
-    final static Map<String, Object> WIN32API_OPTIONS = new HashMap<String, Object>() {
-
+    Map<String, Object> WIN32API_OPTIONS = new HashMap<String, Object>() {
         private static final long serialVersionUID = 1L;
-
         {
             put(Library.OPTION_FUNCTION_MAPPER, W32APIFunctionMapper.UNICODE);
             put(Library.OPTION_TYPE_MAPPER, W32APITypeMapper.UNICODE);
         }
     };
 
-    public Kernel32 INSTANCE = (Kernel32) Native.loadLibrary("Kernel32", Kernel32.class, WIN32API_OPTIONS);
+    Kernel32 INSTANCE = (Kernel32) Native.loadLibrary("Kernel32", Kernel32.class, WIN32API_OPTIONS);
 
     /*
     BOOL WINAPI GetVolumeInformation(
@@ -41,7 +55,7 @@ public interface Kernel32 extends StdCallLibrary {
             __in       DWORD nFileSystemNameSize
             );
      */
-    public boolean GetVolumeInformation(
+    boolean GetVolumeInformation(
             String lpRootPathName,
             char[] lpVolumeNameBuffer,
             DWORD nVolumeNameSize,
@@ -52,5 +66,5 @@ public interface Kernel32 extends StdCallLibrary {
             DWORD nFileSystemNameSize
             );
 
-    public int GetLastError();
+    int GetLastError();
 }
