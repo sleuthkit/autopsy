f7977a1d78cb885826c236140e6adae496f79e07
Brian Kjersten <bhkjersten@gmail.com>
2019-09-05 15:20:52 -0400

    5426: Ignore paragraphs with few tokens. This improves text extraction on the news websites I looked at.

--- Experimental/src/org/sleuthkit/autopsy/experimental/textclassifier/TextClassifierUtils.java
+++ Experimental/src/org/sleuthkit/autopsy/experimental/textclassifier/TextClassifierUtils.java
@@ -21,10 +21,13 @@ package org.sleuthkit.autopsy.experimental.textclassifier;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.logging.Level;
 import opennlp.tools.doccat.DoccatFactory;
@@ -34,7 +37,12 @@ import opennlp.tools.ml.naivebayes.NaiveBayesModel;
 import opennlp.tools.ml.naivebayes.NaiveBayesModelReader;
 import opennlp.tools.ml.naivebayes.PlainTextNaiveBayesModelReader;
 import opennlp.tools.ml.naivebayes.PlainTextNaiveBayesModelWriter;
+import opennlp.tools.sentdetect.NewlineSentenceDetector;
+import opennlp.tools.sentdetect.SentenceDetector;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
 import org.apache.commons.io.IOUtils;
+import org.openide.modules.InstalledFileLocator;
 import org.sleuthkit.autopsy.coreutils.Logger;
 import org.sleuthkit.autopsy.textextractors.TextExtractorFactory;
 import org.sleuthkit.datamodel.AbstractFile;
@@ -64,7 +72,9 @@ class TextClassifierUtils {
     static final String MODEL_PATH = MODEL_DIR + File.separator + "model.txt";
     static final String LANGUAGE_CODE = "en";
     static final String ALGORITHM = "org.sleuthkit.autopsy.experimental.textclassifier.IncrementalNaiveBayesTrainer";
-
+    
+    private static SentenceDetector sentenceDetector;
+        
     TextClassifierUtils() throws IngestModuleException {
         try {
             this.fileTypeDetector = new FileTypeDetector();
@@ -132,18 +142,52 @@ class TextClassifierUtils {
             return new String[0];
         }
 
-        //Read the file, and tokenize it.
+        //Read the file and tokenize it.
         try {
             //Read the text
             String text = IOUtils.toString(reader);
-            //Tokenize the file.
-            return TOKENIZER.tokenize(text);
+            return cleanAndTokenize(text);
         } catch (IOException ex) {
             LOGGER.log(Level.WARNING, "Cannot extract tokens from file " + file.getName(), ex);
             return new String[0];
         }
     }
 
+    private static String[] cleanAndTokenize(String text) throws IOException {
+        //Initialize the sentenceDetector if that hasn't been done yet.
+        if (sentenceDetector == null) {
+            sentenceDetector = new NewlineSentenceDetector();
+            /*
+            //Define the sentence detector, trained on the train section of the EWT corpus, from Universal Dependencies
+            final File sentenceModelFile = InstalledFileLocator.getDefault().locate("doc_summary_models/en-sent-ewt.bin", TextClassifierUtils.class.getPackage().getName(), false); //NON-NLS
+            if (sentenceModelFile == null) {
+                throw new IOException("Error finding sentence detector module");
+            }
+            sentenceDetector = new SentenceDetectorME(new SentenceModel(new FileInputStream(sentenceModelFile)));
+            */
+        }
+        
+        //TODO: Delete this
+        System.out.println("***** START OF FILE *****");
+        
+        ArrayList<String> tokens = new ArrayList<>();
+        String[] sentences = sentenceDetector.sentDetect(text);
+        for (String sentence : sentences) {
+            //Tokenize the file.
+            String[] sentenceTokens = TOKENIZER.tokenize(sentence);
+            if (sentenceTokens.length > 20) {
+                tokens.addAll(Arrays.asList(sentenceTokens));
+                
+                //TODO: Delete this
+                System.out.println(sentence);
+            }
+        }
+        //TODO: Delete this        
+        System.out.println("****** END OF FILE ******");
+        return tokens.toArray(new String[0]);
+    }
+    
+    
     /**
      * Loads a Naive Bayes categorizer from disk.
      *
