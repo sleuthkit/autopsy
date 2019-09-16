08ce5a4555852968b777b02210144250174cc980
U-BASIS\dsmyda <dsmyda@win-dsmyd-4990.basistech.net>
2019-09-16 10:08:55 -0400

    Implemented contacts and some of the call logs parsing

--- InternalPythonModules/android/skype.py
+++ InternalPythonModules/android/skype.py
@@ -53,9 +53,9 @@ class SkypeAnalyzer(general.AndroidComponentAnalyzer):
    
     def __init__(self):
         self._logger = Logger.getLogger(self.__class__.__name__)
-        self._SKYPE_PACKAGE_NAME = ""
+        self._SKYPE_PACKAGE_NAME = "com.skype.raider"
         self._PARSER_NAME = "Skype Parser"
-        self._VERSION = ""
+        self._VERSION = "8.15.0.428"
 
     def analyze(self, dataSource, fileManager, context):
         """
@@ -65,12 +65,12 @@ class SkypeAnalyzer(general.AndroidComponentAnalyzer):
 
         try:
             skype_dbs = AppSQLiteDB.findAppDatabases(dataSource, 
-                    "", True, self._SKYPE_PACKAGE_NAME)
+                    "live", False, "") #self._SKYPE_PACKAGE_NAME)
 
             #Extract TSK_CONTACT and TSK_CALLLOG information
             for skype_db in skype_dbs:
                 helper = AppDBParserHelper(self._PARSER_NAME, 
-                        contact_and_calllog_db.getDBFile(), Account.Type.SKYPE) 
+                        skype_db.getDBFile(), Account.Type.SKYPE) 
 
                 contacts_parser = SkypeContactsParser(skype_db)
                 while contacts_parser.next():
@@ -83,7 +83,7 @@ class SkypeAnalyzer(general.AndroidComponentAnalyzer):
                         contacts_parser.get_email()
                     )
                 contacts_parser.close()
-
+                """
                 calllog_parser = SkypeCallLogsParser(skype_db)
                 while calllog_parser.next():
                     helper.addCalllog(
@@ -110,7 +110,7 @@ class SkypeAnalyzer(general.AndroidComponentAnalyzer):
                         messages_parser.get_thread_id()
                     )
                 messages_parser.close()
-
+                """
                 skype_db.close()
         except (SQLException, TskCoreException) as ex:
             #Error parsing Viber db
@@ -127,21 +127,75 @@ class SkypeCallLogsParser(TskCallLogsParser):
     def __init__(self, calllog_db):
         super(SkypeCallLogsParser, self).__init__(calllog_db.runQuery(
                  """
+                    SELECT full_contacts_list.id, 
+                           full_contacts_list.members, 
+                           full_contacts_list.names, 
+                           time, 
+                           duration, 
+                           is_sender_me, 
+                           person_id
+                    FROM   (SELECT conversation_id AS id , 
+                                   Group_concat(person_id) AS members, 
+                                   Group_concat(CASE 
+                                                  WHEN Ifnull(first_name, "") == "" 
+                                                       AND Ifnull(last_name,"") == "" THEN entry_id 
+                                                  WHEN Ifnull(first_name, "") == "" THEN last_name 
+                                                  WHEN Ifnull(last_name, "") == "" THEN first_name 
+                                                  ELSE first_name || " " || last_name 
+                                                END) AS names 
+                            FROM   particiapnt AS PART 
+                                   JOIN person AS P 
+                                     ON PART.person_id = P.entry_id 
+                            GROUP  BY conversation_id 
+                            UNION 
+                            SELECT entry_id AS id, 
+                                   NULL, 
+                                   CASE 
+                                     WHEN Ifnull(first_name, "") == "" 
+                                          AND Ifnull(last_name, "") == "" THEN entry_id 
+                                     WHEN Ifnull(first_name, "") == "" THEN last_name 
+                                     WHEN Ifnull(last_name, "") == "" THEN first_name 
+                                     ELSE first_name 
+                                          || " " 
+                                          || last_name 
+                                   end      AS name 
+                            FROM   person) AS full_contacts_list 
+                           JOIN chatitem AS C 
+                             ON C.conversation_link = full_contacts_list.id
+                    WHERE message_type == 3
                  """
              )    
         )
+        self._INCOMING_CALL_TYPE = 0
+        self._OUTGOING_CALL_TYPE = 1
+        
 
-   # def get_phone_number_from(self):
+    def get_phone_number_from(self):
+        if self.get_call_direction() == self._INCOMING_CALL_TYPE:
+            return Account.Address(self.result_set.getString("id"),
+                        self.result_set.getString("names"))
+        return super(SkypeCallLogsParser, self).get_phone_number_from()
 
-   # def get_phone_number_to(self):
+    def get_phone_number_to(self):
+        if self.get_call_direction() == self._OUTGOING_CALL_TYPE:
+            return Account.Address(self.result_set.getString("id"), 
+                        self.result_set.getString("names"))
+        return super(SkypeCallLogsParser, self).get_phone_number_to()
+        
 
-  #  def get_call_direction(self):
+    def get_call_direction(self):
+        direction = self.result_set.getInt("is_sender_me")
+        if direction == self._INCOMING_TYPE:
+            return self.INCOMING_CALL
+        return self.OUTGOING_CALL
 
-  #  def get_call_start_date_time(self):
+    def get_call_start_date_time(self):
+        return self.result_set.getLong("time") / 1000
 
-#    def get_call_end_date_time(self):
-
-#    def get_call_type(self):
+    def get_call_end_date_time(self):
+        start = self.get_call_start_date_time()
+        duration = self.result_set.getInt("duration") / 1000
+        return start + duration
 
 class SkypeContactsParser(TskContactsParser):
     """
@@ -153,15 +207,37 @@ class SkypeContactsParser(TskContactsParser):
     def __init__(self, contact_db):
         super(SkypeContactsParser, self).__init__(contact_db.runQuery(
                  """
+                    SELECT entry_id, 
+                           CASE 
+                             WHEN Ifnull(first_name, "") == "" 
+                                  AND Ifnull(last_name, "") == "" THEN entry_id 
+                             WHEN Ifnull(first_name, "") == "" THEN last_name 
+                             WHEN Ifnull(last_name, "") == "" THEN first_name 
+                             ELSE first_name 
+                                  || " " 
+                                  || last_name 
+                           end AS name 
+                    FROM   person 
+                    union
+                    SELECT entry_id, CASE 
+                             WHEN Ifnull(first_name, "") == "" 
+                                  AND Ifnull(last_name, "") == "" THEN entry_id 
+                             WHEN Ifnull(first_name, "") == "" THEN last_name 
+                             WHEN Ifnull(last_name, "") == "" THEN first_name 
+                             ELSE first_name 
+                                  || " " 
+                                  || last_name 
+                           end AS name
+                    FROM user
                  """                                                         
-             )
+              )
         )
     
-#    def get_account_name(self):
+    def get_account_name(self):
+        return self.result_set.getString("entry_id")
         
-#    def get_contact_name(self):
-
-#    def get_phone(self):
+    def get_contact_name(self):
+        return self.result_set.getString("name")
 
 class SkypeMessagesParser(TskMessagesParser):
     """
