2b10527b33d4e8b35e6a484deb5121171240b8b9
U-BASIS\dsmyda <dsmyda@win-dsmyd-4990.basistech.net>
2019-09-17 13:35:18 -0400

    fully implemented skype parser

--- InternalPythonModules/android/skype.py
+++ InternalPythonModules/android/skype.py
@@ -47,7 +47,7 @@ import general
 
 class SkypeAnalyzer(general.AndroidComponentAnalyzer):
     """
-        Parses the Viber App databases for TSK contacts, message 
+        Parses the Skype App databases for TSK contacts, message 
         and calllog artifacts.
     """
    
@@ -56,6 +56,25 @@ class SkypeAnalyzer(general.AndroidComponentAnalyzer):
         self._SKYPE_PACKAGE_NAME = "com.skype.raider"
         self._PARSER_NAME = "Skype Parser"
         self._VERSION = "8.15.0.428"
+    
+    def get_account_instance(self, skype_db):
+        account_query_result = skype_db.runQuery(
+            """
+               SELECT entry_id, 
+                   CASE
+                         WHEN first_name is NULL AND last_name is NULL THEN entry_id
+                         WHEN first_name is NULL THEN last_name
+                         WHEN last_name is NULL THEN first_name
+                         ELSE first_name || " " || last_name
+                   END as name
+               FROM user
+            """
+        )
+
+        if account_query_result is not None and account_query_result.next():
+            return Account.Address(account_query_result.getString("entry_id"),
+                        account_query_result.getString("name")) 
+        return None
 
     def analyze(self, dataSource, fileManager, context):
         """
@@ -64,13 +83,21 @@ class SkypeAnalyzer(general.AndroidComponentAnalyzer):
         """
 
         try:
+            #Skype databases are of the form: live:XYZ.db, where
+            #XYZ is the skype id of the user.
             skype_dbs = AppSQLiteDB.findAppDatabases(dataSource, 
                     "live", False, "") #self._SKYPE_PACKAGE_NAME)
 
             #Extract TSK_CONTACT and TSK_CALLLOG information
             for skype_db in skype_dbs:
-                helper = AppDBParserHelper(self._PARSER_NAME, 
-                        skype_db.getDBFile(), Account.Type.SKYPE) 
+                account_instance = self.get_account_instance(skype_db) 
+                if account_instance is None:
+                    helper = AppDBParserHelper(self._PARSER_NAME, 
+                            skype_db.getDBFile(), Account.Type.SKYPE) 
+                else:
+                    helper = AppDBParserHelper(self._PARSER_NAME,
+                            skype_db.getDBFile(), Account.Type.SKYPE,
+                            Account.Type.SKYPE, account_instance)
 
                 contacts_parser = SkypeContactsParser(skype_db)
                 while contacts_parser.next():
@@ -83,7 +110,7 @@ class SkypeAnalyzer(general.AndroidComponentAnalyzer):
                         contacts_parser.get_email()
                     )
                 contacts_parser.close()
-                """
+
                 calllog_parser = SkypeCallLogsParser(skype_db)
                 while calllog_parser.next():
                     helper.addCalllog(
@@ -110,7 +137,7 @@ class SkypeAnalyzer(general.AndroidComponentAnalyzer):
                         messages_parser.get_thread_id()
                     )
                 messages_parser.close()
-                """
+    
                 skype_db.close()
         except (SQLException, TskCoreException) as ex:
             #Error parsing Viber db
@@ -128,12 +155,10 @@ class SkypeCallLogsParser(TskCallLogsParser):
         super(SkypeCallLogsParser, self).__init__(calllog_db.runQuery(
                  """
                     SELECT full_contacts_list.id, 
-                           full_contacts_list.members, 
                            full_contacts_list.names, 
                            time, 
                            duration, 
-                           is_sender_me, 
-                           person_id
+                           is_sender_me 
                     FROM   (SELECT conversation_id AS id , 
                                    Group_concat(person_id) AS members, 
                                    Group_concat(CASE 
@@ -141,7 +166,9 @@ class SkypeCallLogsParser(TskCallLogsParser):
                                                        AND Ifnull(last_name,"") == "" THEN entry_id 
                                                   WHEN Ifnull(first_name, "") == "" THEN last_name 
                                                   WHEN Ifnull(last_name, "") == "" THEN first_name 
-                                                  ELSE first_name || " " || last_name 
+                                                  ELSE first_name
+                                                       || " "
+                                                       || last_name 
                                                 END) AS names 
                             FROM   particiapnt AS PART 
                                    JOIN person AS P 
@@ -158,7 +185,7 @@ class SkypeCallLogsParser(TskCallLogsParser):
                                      ELSE first_name 
                                           || " " 
                                           || last_name 
-                                   end      AS name 
+                                   END      AS name 
                             FROM   person) AS full_contacts_list 
                            JOIN chatitem AS C 
                              ON C.conversation_link = full_contacts_list.id
@@ -171,23 +198,24 @@ class SkypeCallLogsParser(TskCallLogsParser):
         
 
     def get_phone_number_from(self):
-        if self.get_call_direction() == self._INCOMING_CALL_TYPE:
+        if self.get_call_direction() == self.INCOMING_CALL:
             return Account.Address(self.result_set.getString("id"),
                         self.result_set.getString("names"))
         return super(SkypeCallLogsParser, self).get_phone_number_from()
 
     def get_phone_number_to(self):
-        if self.get_call_direction() == self._OUTGOING_CALL_TYPE:
+        if self.get_call_direction() == self.OUTGOING_CALL:
             return Account.Address(self.result_set.getString("id"), 
                         self.result_set.getString("names"))
         return super(SkypeCallLogsParser, self).get_phone_number_to()
         
-
     def get_call_direction(self):
         direction = self.result_set.getInt("is_sender_me")
-        if direction == self._INCOMING_TYPE:
+        if direction == self._INCOMING_CALL_TYPE:
             return self.INCOMING_CALL
-        return self.OUTGOING_CALL
+        if direction == self._OUTGOING_CALL_TYPE:
+            return self.OUTGOING_CALL
+        return super(SkypeCallLogsParser, self).get_call_direction()
 
     def get_call_start_date_time(self):
         return self.result_set.getLong("time") / 1000
@@ -218,17 +246,6 @@ class SkypeContactsParser(TskContactsParser):
                                   || last_name 
                            end AS name 
                     FROM   person 
-                    union
-                    SELECT entry_id, CASE 
-                             WHEN Ifnull(first_name, "") == "" 
-                                  AND Ifnull(last_name, "") == "" THEN entry_id 
-                             WHEN Ifnull(first_name, "") == "" THEN last_name 
-                             WHEN Ifnull(last_name, "") == "" THEN first_name 
-                             ELSE first_name 
-                                  || " " 
-                                  || last_name 
-                           end AS name
-                    FROM user
                  """                                                         
               )
         )
@@ -249,22 +266,97 @@ class SkypeMessagesParser(TskMessagesParser):
     def __init__(self, message_db):
         super(SkypeMessagesParser, self).__init__(message_db.runQuery(
                  """
-                 """
+                    SELECT full_contacts_list.id, 
+                           full_contacts_list.members,
+                           full_contacts_list.names, 
+                           time, 
+                           content, 
+                           file_name, 
+                           device_gallery_path, 
+                           is_sender_me 
+                    FROM   (SELECT conversation_id AS id , 
+                                   Group_concat(person_id) AS members, 
+                                   Group_concat(CASE 
+                                                  WHEN Ifnull(first_name, "") == "" 
+                                                       AND Ifnull(last_name,"") == "" THEN entry_id 
+                                                  WHEN Ifnull(first_name, "") == "" THEN last_name 
+                                                  WHEN Ifnull(last_name, "") == "" THEN first_name 
+                                                  ELSE first_name
+                                                       || " "
+                                                       || last_name 
+                                                END) AS names 
+                            FROM   particiapnt AS PART 
+                                   JOIN person AS P 
+                                     ON PART.person_id = P.entry_id 
+                            GROUP  BY conversation_id 
+                            UNION 
+                            SELECT entry_id AS id, 
+                                   NULL, 
+                                   CASE 
+                                     WHEN Ifnull(first_name, "") == "" 
+                                          AND Ifnull(last_name, "") == "" THEN entry_id 
+                                     WHEN Ifnull(first_name, "") == "" THEN last_name 
+                                     WHEN Ifnull(last_name, "") == "" THEN first_name 
+                                     ELSE first_name 
+                                          || " " 
+                                          || last_name 
+                                   END      AS name 
+                            FROM   person) AS full_contacts_list 
+                           JOIN chatitem AS C 
+                             ON C.conversation_link = full_contacts_list.id
+                    WHERE message_type != 3
+             """
              )
         )
+        self._SKYPE_MESSAGE_TYPE = "Skype Message"
+        self._OUTGOING_MESSAGE_TYPE = 1
+        self._INCOMING_MESSAGE_TYPE = 0
 
-    #def get_message_type(self):
+    def get_message_type(self):
+        return self._SKYPE_MESSAGE_TYPE
 
-    #def get_phone_number_from(self):
+    def get_phone_number_from(self):
+        if self.get_message_direction() == self.INCOMING:
+            return Account.Address(self.result_set.getString("id"),
+                        self.result_set.getString("names"))
+        return super(SkypeMessagesParser, self).get_phone_number_from()
 
-    #def get_message_direction(self):  
+    def get_message_direction(self):  
+        direction = self.result_set.getInt("is_sender_me")
+        if direction == self._OUTGOING_MESSAGE_TYPE:
+            return self.OUTGOING
+        if direction == self._INCOMING_MESSAGE_TYPE:
+            return self.INCOMING
+        return super(SkypeMessagesParser, self).get_message_direction()
     
-    #def get_phone_number_to(self):
+    def get_phone_number_to(self):
+        if self.get_message_direction() == self.OUTGOING:
+            return Account.Address(self.result_set.getString("id"),
+                        self.result_set.getString("names"))
+        return super(SkypeMessagesParser, self).get_phone_number_to()
 
-    #def get_message_date_time(self):
+    def get_message_date_time(self):
+        date = self.result_set.getLong("time")
+        return date / 1000
 
-    #def get_message_read_status(self):
+    def get_message_text(self):
+        content = self.result_set.getString("content")
 
-    #def get_message_text(self):
+        if content is not None:
+            file_name = self.result_set.getString("file_name")
+            file_path = self.result_set.getString("device_gallery_path")
 
-    #def get_thread_id(self):
+            #if a file name and file path are associated with a message, append it
+            if file_name is not None and file_path is not None:
+                attachment = "File Name: "+file_name +"\n"+ "File Path: "+file_path
+                return general.appendAttachmentList(content, [attachment]) 
+
+            return content
+
+        return super(SkypeMessagesParser, self).get_message_text()
+
+    def get_thread_id(self):
+        members = self.result_set.getString("members")
+        if members is not None:
+            return self.result_set.getString("id")
+        return super(SkypeMessagesParser, self).get_thread_id()
