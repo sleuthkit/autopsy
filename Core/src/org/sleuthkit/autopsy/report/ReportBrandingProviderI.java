d3eb0689fe945d3ddb1e88c694fd30af82819263
Eugene Livis <elivis@basistech.com>
2019-09-18 17:54:36 -0400

    Fixed build errors in tools that use Autopsy as platform

--- Core/src/org/sleuthkit/autopsy/report/ReportBrandingProviderI.java
+++ Core/src/org/sleuthkit/autopsy/report/ReportBrandingProviderI.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.sleuthkit.autopsy.report.infrastructure;
+package org.sleuthkit.autopsy.report;
 
 /**
  * Interface to implement by reports to add on custom branding, logos, etc
