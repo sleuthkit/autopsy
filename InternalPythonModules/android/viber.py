2bb38065a69395adb7a3f626d27a1acea92610b3
U-BASIS\dsmyda <dsmyda@win-dsmyd-4990.basistech.net>
2019-09-19 18:16:06 -0400

    Updated viber to new infra changes and changed how exception handling is managed

--- InternalPythonModules/android/viber.py
+++ InternalPythonModules/android/viber.py
@@ -16,7 +16,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
 from java.io import File
 from java.lang import Class
 from java.lang import ClassNotFoundException
@@ -26,18 +25,26 @@ from java.sql import ResultSet
 from java.sql import SQLException
 from java.sql import Statement
 from java.util.logging import Level
+from java.util import ArrayList
 from org.apache.commons.codec.binary import Base64
 from org.sleuthkit.autopsy.casemodule import Case
 from org.sleuthkit.autopsy.coreutils import Logger
+from org.sleuthkit.autopsy.coreutils import MessageNotifyUtil
 from org.sleuthkit.autopsy.coreutils import AppSQLiteDB
-from org.sleuthkit.autopsy.coreutils import AppDBParserHelper
+
+from org.sleuthkit.autopsy.datamodel import ContentUtils
 from org.sleuthkit.autopsy.ingest import IngestJobContext
 from org.sleuthkit.datamodel import AbstractFile
 from org.sleuthkit.datamodel import BlackboardArtifact
 from org.sleuthkit.datamodel import BlackboardAttribute
 from org.sleuthkit.datamodel import Content
 from org.sleuthkit.datamodel import TskCoreException
+from org.sleuthkit.datamodel.Blackboard import BlackboardException
+from org.sleuthkit.autopsy.casemodule import NoCurrentCaseException
 from org.sleuthkit.datamodel import Account
+from org.sleuthkit.datamodel.blackboardutils import CommunicationArtifactsHelper
+from org.sleuthkit.datamodel.blackboardutils.CommunicationArtifactsHelper import MessageReadStatus
+from org.sleuthkit.datamodel.blackboardutils.CommunicationArtifactsHelper import CommunicationDirection
 from TskMessagesParser import TskMessagesParser
 from TskContactsParser import TskContactsParser
 from TskCallLogsParser import TskCallLogsParser
@@ -49,6 +56,14 @@ class ViberAnalyzer(general.AndroidComponentAnalyzer):
     """
         Parses the Viber App databases for TSK contacts, message 
         and calllog artifacts.
+
+        The Viber v11.5.0 database structure is as follows:
+            - People can take part in N conversation(s). A conversation can have M 
+              members and messages are exchanged in a conversation. 
+            - Viber has a conversation table, a participant table (the people/members in the above
+              analogy) and a messages table.
+            - Each row of the participants table maps a person to a conversation_id
+            - Each row in the messages table has a from participant id and a conversation id.
     """
    
     def __init__(self):
@@ -71,61 +86,110 @@ class ViberAnalyzer(general.AndroidComponentAnalyzer):
 
             #Extract TSK_CONTACT and TSK_CALLLOG information
             for contact_and_calllog_db in contact_and_calllog_dbs:
-                helper = AppDBParserHelper(self._PARSER_NAME, 
+                current_case = Case.getCurrentCaseThrows()
+                helper = CommunicationArtifactsHelper(
+                        current_case.getSleuthkitCase(), self._PARSER_NAME, 
                         contact_and_calllog_db.getDBFile(), Account.Type.VIBER) 
-
-                contacts_parser = ViberContactsParser(contact_and_calllog_db)
-                while contacts_parser.next():
-                    helper.addContact( 
-                        contacts_parser.get_account_name(), 
-                        contacts_parser.get_contact_name(), 
-                        contacts_parser.get_phone(),
-                        contacts_parser.get_home_phone(),
-                        contacts_parser.get_mobile_phone(),
-                        contacts_parser.get_email()
-                    )
-                contacts_parser.close()
-
-                calllog_parser = ViberCallLogsParser(contact_and_calllog_db)
-                while calllog_parser.next():
-                    helper.addCalllog(
-                        calllog_parser.get_call_direction(),
-                        calllog_parser.get_phone_number_from(),
-                        calllog_parser.get_phone_number_to(),
-                        calllog_parser.get_call_start_date_time(),
-                        calllog_parser.get_call_end_date_time(),
-                        calllog_parser.get_call_type()
-                    )
-                calllog_parser.close()
-
-                contact_and_calllog_db.close()
+                self.parse_contacts(contact_and_calllog_db, helper)
+                self.parse_calllogs(contact_and_calllog_db, helper)
 
             #Extract TSK_MESSAGE information
             for message_db in message_dbs:
-                helper = AppDBParserHelper(self._PARSER_NAME, 
+                current_case = Case.getCurrentCaseThrows()
+                helper = CommunicationArtifactsHelper(
+                        current_case.getSleuthkitCase(), self._PARSER_NAME, 
                         message_db.getDBFile(), Account.Type.VIBER)
+                self.parse_messages(message_db, helper)
 
-                messages_parser = ViberMessagesParser(message_db)
-                while messages_parser.next():
-                    helper.addMessage(
-                        messages_parser.get_message_type(),
-                        messages_parser.get_message_direction(),
-                        messages_parser.get_phone_number_from(),
-                        messages_parser.get_phone_number_to(),
-                        messages_parser.get_message_date_time(),
-                        messages_parser.get_message_read_status(),
-                        messages_parser.get_message_subject(),
-                        messages_parser.get_message_text(),
-                        messages_parser.get_thread_id()
-                    )
-                messages_parser.close()
+        except NoCurrentCaseException as ex:
+            self._logger.log(Level.WARNING, "No case currently open.", ex)
+            self._logger.log(Level.WARNING, traceback.format_exec())
+        
+        for message_db in messages_db:
+            message_db.close()
+        
+        for contact_and_calllog_db in contact_and_calllog_dbs:
+            contact_and_calllog_db.close()
 
-                message_db.close()
-        except (SQLException, TskCoreException) as ex:
-            #Error parsing Viber db
-            self._logger.log(Level.WARNING, "Error parsing Viber Databases", ex)
+    def parse_contacts(self, contacts_db, helper):
+        try:
+            contacts_parser = ViberContactsParser(contacts_db)
+            while contacts_parser.next():
+                helper.addContact( 
+                    contacts_parser.get_account_name(), 
+                    contacts_parser.get_contact_name(), 
+                    contacts_parser.get_phone(),
+                    contacts_parser.get_home_phone(),
+                    contacts_parser.get_mobile_phone(),
+                    contacts_parser.get_email()
+                )
+            contacts_parser.close()
+        except SQLException as ex:
+            self._logger.log(Level.WARNING, "Error querying the viber database for contacts.", ex)
+            self._logger.log(Level.WARNING, traceback.format_exec())
+        except TskCoreException as ex:
+            self._logger.log(Level.SEVERE, 
+                    "Error adding viber contacts artifact to case database.", ex)
+            self._logger.log(Level.SEVERE, traceback.format_exec())
+        except BlackboardException as ex:
+            self._logger.log(Level.WARNING, 
+                    "Error posting viber contacts artifact to the blackboard.", ex)
             self._logger.log(Level.WARNING, traceback.format_exec())
 
+    def parse_calllogs(self, calllogs_db, helper):
+        try:
+            calllog_parser = ViberCallLogsParser(calllogs_db)
+            while calllog_parser.next():
+                helper.addCalllog(
+                    calllog_parser.get_call_direction(),
+                    calllog_parser.get_phone_number_from(),
+                    calllog_parser.get_phone_number_to(),
+                    calllog_parser.get_call_start_date_time(),
+                    calllog_parser.get_call_end_date_time(),
+                    calllog_parser.get_call_type()
+                )
+            calllog_parser.close()
+        except SQLException as ex:
+            self._logger.log(Level.WARNING, "Error querying the viber database for calllogs.", ex)
+            self._logger.log(Level.WARNING, traceback.format_exec())
+        except TskCoreException as ex:
+            self._logger.log(Level.SEVERE, 
+                    "Error adding viber calllogs artifact to case database.", ex)
+            self._logger.log(Level.SEVERE, traceback.format_exec())
+        except BlackboardException as ex:
+            self._logger.log(Level.WARNING, 
+                    "Error posting viber calllogs artifact to the blackboard.", ex)
+            self._logger.log(Level.WARNING, traceback.format_exec())
+
+
+    def parse_messages(self, messages_db, helper):
+        try:
+            messages_parser = ViberMessagesParser(messages_db)
+            while messages_parser.next():
+                helper.addMessage(
+                    messages_parser.get_message_type(),
+                    messages_parser.get_message_direction(),
+                    messages_parser.get_phone_number_from(),
+                    messages_parser.get_phone_number_to(),
+                    messages_parser.get_message_date_time(),
+                    messages_parser.get_message_read_status(),
+                    messages_parser.get_message_subject(),
+                    messages_parser.get_message_text(),
+                    messages_parser.get_thread_id()
+                )
+            messages_parser.close()
+        except SQLException as ex:
+            self._logger.log(Level.WARNING, "Error querying the viber database for messages.", ex)
+            self._logger.log(Level.WARNING, traceback.format_exec())
+        except TskCoreException as ex:
+            self._logger.log(Level.SEVERE, 
+                    "Error adding viber messages artifact to case database.", ex)
+            self._logger.log(Level.SEVERE, traceback.format_exec())
+        except BlackboardException as ex:
+            self._logger.log(Level.WARNING, 
+                    "Error posting viber messages artifact to the blackboard.", ex)
+            self._logger.log(Level.WARNING, traceback.format_exec())
+        
 class ViberCallLogsParser(TskCallLogsParser):
     """
         Extracts TSK_CALLLOG information from the Viber database.
@@ -227,14 +291,6 @@ class ViberMessagesParser(TskMessagesParser):
 
     def __init__(self, message_db):
         """
-            For our purposes, the Viber datamodel is as follows:
-                - People can take part in N conversation(s). A conversation can have M 
-                  members and messages are exchanged in a conversation. 
-                - Viber has a conversation table, a participant table (the people/members in the above
-                  analogy) and a messages table.
-                - Each row of the participants table maps a person to a conversation_id
-                - Each row in the messages table has a from participant id and a conversation id.
-            
             The query below does the following:
                 - The first two inner joins on participants and participants_info build
                   the 1 to many (M) mappings between the sender and the recipients for each 
