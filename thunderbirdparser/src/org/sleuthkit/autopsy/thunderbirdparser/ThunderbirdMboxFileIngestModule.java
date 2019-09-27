ade820b4bad27e420290becc4f34f0f4f107a243
Eugene Livis <elivis@basistech.com>
2019-09-27 09:31:54 -0400

    Fix for NPE

--- thunderbirdparser/src/org/sleuthkit/autopsy/thunderbirdparser/ThunderbirdMboxFileIngestModule.java
+++ thunderbirdparser/src/org/sleuthkit/autopsy/thunderbirdparser/ThunderbirdMboxFileIngestModule.java
@@ -418,6 +418,19 @@ public final class ThunderbirdMboxFileIngestModule implements FileIngestModule {
      * @param abstractFile
      */
     private void processEmails(List<EmailMessage> partialEmailsForThreading, Iterator<EmailMessage> fullMessageIterator, AbstractFile abstractFile) {
+        
+        // sometimes parser returns ParseResult=OK but there are no messages
+        if (fullMessageIterator == null) {
+            // parsing error: log message
+            postErrorMessage(
+                    NbBundle.getMessage(this.getClass(), "ThunderbirdMboxFileIngestModule.processPst.errProcFile.msg",
+                            abstractFile.getName()),
+                    NbBundle.getMessage(this.getClass(),
+                            "ThunderbirdMboxFileIngestModule.processPst.errProcFile.details"));
+            logger.log(Level.INFO, "PSTParser failed to parse {0}", abstractFile.getName()); //NON-NLS
+            return;
+        }
+        
         // Putting try/catch around this to catch any exception and still allow
         // the creation of the artifacts to continue.
         try{
