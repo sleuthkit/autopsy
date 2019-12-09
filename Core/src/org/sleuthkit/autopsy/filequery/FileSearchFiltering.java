17fb01f30c0e649e4d933047f1b325937aefaa09
William Schaefer <wschaefer@basistech.net>
2019-12-09 13:52:40 -0500

    5716 rename to device original artifact

--- Core/src/org/sleuthkit/autopsy/filequery/FileSearchFiltering.java
+++ Core/src/org/sleuthkit/autopsy/filequery/FileSearchFiltering.java
@@ -859,7 +859,7 @@ class FileSearchFiltering {
         String getWhereClause() {
             String queryStr = "(obj_id IN (SELECT obj_id from blackboard_artifacts WHERE artifact_id IN "
                     + "(SELECT artifact_id FROM blackboard_attributes WHERE artifact_type_id = "
-                    + BlackboardArtifact.ARTIFACT_TYPE.TSK_USER_CREATED_SUSPECTED.getTypeID() + ")))";
+                    + BlackboardArtifact.ARTIFACT_TYPE.TSK_DEVICE_ORIGINAL_SUSPECTED.getTypeID() + ")))";
 
             return queryStr;
         }
