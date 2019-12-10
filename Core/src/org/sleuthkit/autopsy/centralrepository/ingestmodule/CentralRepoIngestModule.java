492ce36c8cee4e669e9b79208ef9b17d82b5ddd1
Kelly Kelly <kelly@basistech.com>
2019-12-10 12:13:41 -0500

    Added missing message

--- Core/src/org/sleuthkit/autopsy/centralrepository/ingestmodule/CentralRepoIngestModule.java
+++ Core/src/org/sleuthkit/autopsy/centralrepository/ingestmodule/CentralRepoIngestModule.java
@@ -366,6 +366,7 @@ final class CentralRepoIngestModule implements FileIngestModule {
         "CentralRepoIngestModule_notable_message_header=<html>A file in this data source was previously seen and tagged as Notable.<br>",
         "CentralRepoIngestModel_name_header=Name:<br>",
         "CentralRepoIngestModel_previous_case_header=<br>Previous Cases:<br>",
+        "# {0} - Name of file that is Notable",
         "CentralRepoIngestModule_postToBB_knownBadMsg=Notable: {0}"
     })
 
