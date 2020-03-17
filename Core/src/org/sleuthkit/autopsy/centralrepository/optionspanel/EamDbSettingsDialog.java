04638faa85268212992ad7917ce346f76df40dca
Greg DiCristofaro <gregd@basistech.com>
2020-03-17 14:57:11 -0400

    updated to use TypedBasicComboBoxRenderer

--- Core/src/org/sleuthkit/autopsy/centralrepository/optionspanel/EamDbSettingsDialog.java
+++ Core/src/org/sleuthkit/autopsy/centralrepository/optionspanel/EamDbSettingsDialog.java
@@ -38,7 +38,6 @@ import javax.swing.SwingUtilities;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.filechooser.FileFilter;
-import javax.swing.plaf.basic.BasicComboBoxRenderer;
 import org.netbeans.spi.options.OptionsPanelController;
 import org.openide.util.NbBundle;
 import org.openide.util.NbBundle.Messages;
@@ -65,19 +64,17 @@ public class EamDbSettingsDialog extends JDialog {
     /**
      * This class handles displaying and rendering drop down menu for database choices in central repo.
      */
-    private class DbChoiceRenderer extends BasicComboBoxRenderer {
+    private class DbChoiceRenderer extends TypedBasicComboBoxRenderer<CentralRepoDbChoice> {
         private static final long serialVersionUID = 1L;
         
-        @SuppressWarnings("rawtypes")
-        public Component getListCellRendererComponent(JList list, Object value,
+        public Component getListCellRendererComponent(
+                JList<? extends CentralRepoDbChoice> list, CentralRepoDbChoice value,
                 int index, boolean isSelected, boolean cellHasFocus) {
-
-            CentralRepoDbChoice item = (CentralRepoDbChoice) value;
 
             // disable cell if it is the db connection from multi user settings 
             // and that option is not enabled in multi user settings
-            setText(item.getTitle());
-            setEnabled(isDbChoiceSelectable(item));
+            setText(value.getTitle());
+            setEnabled(isDbChoiceSelectable(value));
             return this;
         }
     }
@@ -136,15 +133,7 @@ public class EamDbSettingsDialog extends JDialog {
         valid();
         display();
     }
-    
-    
-    /**
-     * Sets the drowndown renderer so that an item is unselectable if inappropriate.
-     */
-    @SuppressWarnings("unchecked")
-    private void setDropdownRenderer() {
-        cbDatabaseType.setRenderer(DB_CHOICE_RENDERER);
-    }
+
     
     private void setupDbChoice(CentralRepoDbChoice initialMenuItem) {
         // setup initially selected item
@@ -154,7 +143,7 @@ public class EamDbSettingsDialog extends JDialog {
             CentralRepoDbChoice.DB_CHOICES[0] :
             initialMenuItem;
                 
-        setDropdownRenderer();
+        cbDatabaseType.setRenderer(DB_CHOICE_RENDERER);
         changeDbSelection(toSelect);
     }
     
