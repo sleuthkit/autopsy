b61a6114fcbb0d1c8941d86f1880a9ddd6fc4f26
Richard Cordovano <rcordovano@basistech.com>
2020-03-19 14:15:47 -0400

    6108 central repo schema updates in app service

---
+++ Core/src/org/sleuthkit/autopsy/centralrepository/datamodel/CentralRepositoryService.java
@@ -0,0 +1,119 @@
+/*
+ * Central Repository
+ *
+ * Copyright 2018-2020 Basis Technology Corp.
+ * Contact: carrier <at> sleuthkit <dot> org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sleuthkit.autopsy.centralrepository.datamodel;
+
+import org.openide.util.NbBundle;
+import org.openide.util.lookup.ServiceProvider;
+import org.sleuthkit.autopsy.appservices.AutopsyService;
+import org.sleuthkit.autopsy.progress.ProgressIndicator;
+import org.sleuthkit.autopsy.casemodule.Case;
+import org.sleuthkit.datamodel.Content;
+import org.sleuthkit.datamodel.DataSource;
+import org.sleuthkit.datamodel.TskCoreException;
+
+/**
+ * The Autopsy application service for the central repository.
+ */
+@ServiceProvider(service = AutopsyService.class)
+public class CentralRepositoryService implements AutopsyService {
+
+    @Override
+    @NbBundle.Messages({
+        "CentralRepositoryService.serviceName=Central Repository Service"
+    })
+    public String getServiceName() {
+        return Bundle.CentralRepositoryService_serviceName();
+    }
+
+    @NbBundle.Messages({
+        "CentralRepositoryService.progressMsg.updatingSchema=Updating schema...",
+        "CentralRepositoryService.progressMsg.updatingDataSourcesTable=Checking for v1.2 data updates...",})
+    @Override
+    public void openCaseResources(CaseContext context) throws AutopsyServiceException {
+        if (!CentralRepository.isEnabled()) {
+            return;
+        }
+
+        /*
+         * TODO (Jira-6108): We should consider adding coordination service
+         * locking to the central repository in collaborative environments.
+         */
+        ProgressIndicator progress = context.getProgressIndicator();
+        progress.progress(Bundle.CentralRepositoryService_progressMsg_updatingSchema());
+        updateSchema();
+
+        if (context.cancelRequested()) {
+            return;
+        }
+
+        progress.progress(Bundle.CentralRepositoryService_progressMsg_updatingDataSourcesTable());
+        dataUpgradeForVersion1dot2(context.getCase());
+    }
+
+    /**
+     * Updates the central repository schema to the latest version.
+     *
+     * @throws AutopsyServiceException
+     */
+    private void updateSchema() throws AutopsyServiceException {
+        try {
+            CentralRepoDbManager.upgradeDatabase();
+        } catch (CentralRepoException ex) {
+            throw new AutopsyServiceException("Failed to update the Central Repository schema", ex);
+        }
+    }
+
+    /**
+     * Adds missing data source object IDs from data sources in this case to the
+     * corresponding records in the central repository. This is a data update to
+     * go with the v1.2 schema update.
+     *
+     * @throws AutopsyServiceException
+     */
+    private void dataUpgradeForVersion1dot2(Case currentCase) throws AutopsyServiceException {
+        try {
+            /*
+             * If the case is in the central repository, there may be missing
+             * data source object IDs in the data_sources.datasource_obj_id
+             * column that was added in the version 1.2 schema update.
+             */
+            CentralRepository centralRepository = CentralRepository.getInstance();
+            CorrelationCase correlationCase = centralRepository.getCase(currentCase);
+            if (correlationCase != null) {
+                for (CorrelationDataSource correlationDataSource : centralRepository.getDataSources()) {
+                    /*
+                     * ResultSet.getLong returns zero when the value in the
+                     * result set is NULL.
+                     */
+                    if (correlationDataSource.getCaseID() == correlationCase.getID() && correlationDataSource.getDataSourceObjectID() == 0) {
+                        for (Content dataSource : currentCase.getDataSources()) {
+                            if (((DataSource) dataSource).getDeviceId().equals(correlationDataSource.getDeviceID()) && dataSource.getName().equals(correlationDataSource.getName())) {
+                                centralRepository.addDataSourceObjectId(correlationDataSource.getID(), dataSource.getId());
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (CentralRepoException | TskCoreException ex) {
+            throw new AutopsyServiceException("Failed to update data sources in the Central Repository for schema v1.2", ex);
+        }
+    }
+
+}
