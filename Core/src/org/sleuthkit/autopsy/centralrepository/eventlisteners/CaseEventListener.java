c04cc8463348f0e5926a360ce57e9aa079d5efd0
Greg DiCristofaro <gregd@basistech.com>
2020-04-30 09:26:47 -0400

    refactoring and allowing for deletion when multiple tags and comments

--- Core/src/org/sleuthkit/autopsy/centralrepository/eventlisteners/CaseEventListener.java
+++ Core/src/org/sleuthkit/autopsy/centralrepository/eventlisteners/CaseEventListener.java
@@ -23,6 +23,7 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -55,6 +56,7 @@ import org.sleuthkit.datamodel.TagName;
 import org.sleuthkit.datamodel.TskCoreException;
 import org.sleuthkit.datamodel.TskData;
 import org.sleuthkit.autopsy.centralrepository.datamodel.CentralRepository;
+import org.sleuthkit.datamodel.Tag;
 
 /**
  * Listen for case events and update entries in the Central Repository database
@@ -141,6 +143,56 @@ final class CaseEventListener implements PropertyChangeListener {
     void uninstallListeners() {
         Case.removeEventTypeSubscriber(CASE_EVENTS_OF_INTEREST, this);
     }
+    
+    /**
+     * Retrieves the most recent (based on tag id) notable tag that has a comment
+     * @param tags      The list of tags to search.
+     * @return          The most recent notable tag that has a comment.
+     */
+    private static String getMostRecentNotableComment(List<? extends Tag> tags) {
+        if (tags == null)
+            return null;
+        
+        Optional<? extends Tag> result = tags.stream()
+            .filter(CaseEventListener::isNotableTag)
+            .filter(t -> StringUtils.isNotEmpty(t.getComment()))
+            .max((t1,t2) -> Long.compare(t1.getId(), t2.getId()));
+        
+        return result.isPresent() ? result.get().getComment() : null;
+    }
+    
+    /**
+     * Returns true if the tag has a notable status.
+     * @param t     The tag to use in determination.
+     * @return      Whether or not it is a notable tag.
+     */
+    private static boolean isNotableTag(Tag t) {
+        return (t == null) ? false : isNotableTagName(t.getName());
+    }
+    
+    /**
+     * Returns true if the tag name has a notable status.
+     * @param t     The tag name to use in determination.
+     * @return      Whether or not it is a notable tag name.
+     */
+    private static boolean isNotableTagName(TagName t) {
+        return (t == null) ? false : TagsManager.getNotableTagDisplayNames().contains(t.getDisplayName());
+    }
+    
+    /**
+     * Searches a list of tags for a tag with a notable status.
+     * @param tags      The tags to search.
+     * @return          Whether or not the list contains a notable tag.
+     */
+    private static boolean hasNotableTag(List<? extends Tag> tags) {
+        if (tags == null)
+            return false;
+        
+        return tags.stream()        
+            .filter(CaseEventListener::isNotableTag)
+            .findFirst()
+            .isPresent();
+    }
 
     private final class ContentTagTask implements Runnable {
 
@@ -154,79 +206,83 @@ final class CaseEventListener implements PropertyChangeListener {
 
         @Override
         public void run() {
-            if (!CentralRepository.isEnabled()) {
+            if (!CentralRepository.isEnabled())
+                return;
+
+            if (Case.Events.valueOf(event.getPropertyName()) == Case.Events.CONTENT_TAG_ADDED) {
+                handleTagAdded((ContentTagAddedEvent) event);
+            } 
+            else { 
+                handleTagDeleted((ContentTagDeletedEvent) event);
+            }
+        }
+        
+        private void handleTagDeleted(ContentTagDeletedEvent tagDeletedEvent) {
+            // CONTENT_TAG_DELETED
+            // For deleted tags, we want to set the file status to UNKNOWN if:
+            //   - The tag that was just removed is notable in central repo
+            //   - There are no remaining tags that are notable 
+            //   - If there are other notable tags, the comment should be updated to most recent tag
+
+            if (!isNotableTagName(tagDeletedEvent.getDeletedTagInfo().getName())) {
+                // If the tag that got removed isn't on the list of central repo tags, do nothing
                 return;
             }
 
-            AbstractFile af;
-            TskData.FileKnown knownStatus;
-            String comment = null;
-            boolean isTagDelete;
-            if (Case.Events.valueOf(event.getPropertyName()) == Case.Events.CONTENT_TAG_ADDED) {
-                // For added tags, we want to change the known status to BAD if the 
-                // tag that was just added is in the list of central repo tags.
-                final ContentTagAddedEvent tagAddedEvent = (ContentTagAddedEvent) event;
-                final ContentTag tagAdded = tagAddedEvent.getAddedTag();
-                comment = tagAdded.getComment();
-                isTagDelete = false;
-                        
-                if (TagsManager.getNotableTagDisplayNames().contains(tagAdded.getName().getDisplayName())) {
-                    if (tagAdded.getContent() instanceof AbstractFile) {
-                        af = (AbstractFile) tagAdded.getContent();
-                        knownStatus = TskData.FileKnown.BAD;
-                    } else {
-                        LOGGER.log(Level.WARNING, "Error updating non-file object");
-                        return;
-                    }
-                } else {
-                    // The added tag isn't flagged as bad in central repo, so do nothing
-                    return;
-                }
-            } else { // CONTENT_TAG_DELETED
-                // For deleted tags, we want to set the file status to UNKNOWN if:
-                //   - The tag that was just removed is notable in central repo
-                //   - There are no remaining tags that are notable 
-                final ContentTagDeletedEvent tagDeletedEvent = (ContentTagDeletedEvent) event;
+            try {
+                // Get the remaining tags on the content object
                 long contentID = tagDeletedEvent.getDeletedTagInfo().getContentID();
-                comment = tagDeletedEvent.getDeletedTagInfo().getComment();
-                isTagDelete = true;
-                
-                String tagName = tagDeletedEvent.getDeletedTagInfo().getName().getDisplayName();
-                if (!TagsManager.getNotableTagDisplayNames().contains(tagName)) {
-                    // If the tag that got removed isn't on the list of central repo tags, do nothing
+                Content content = Case.getCurrentCaseThrows().getSleuthkitCase().getContentById(contentID);
+                TagsManager tagsManager = Case.getCurrentCaseThrows().getServices().getTagsManager();
+                List<ContentTag> tags = tagsManager.getContentTagsByContent(content);
+
+                AbstractFile af = getAbstractFile(content);
+                if (af == null)
                     return;
+
+                if (hasNotableTag(tags)) {
+                    // There's still at least one bad tag, so leave the known status but update the comment to most recent
+                    String comment = getMostRecentNotableComment(tags);
+                    setContentKnownStatus(af, comment, TskData.FileKnown.BAD, false);
                 }
-
-                try {
-                    // Get the remaining tags on the content object
-                    Content content = Case.getCurrentCaseThrows().getSleuthkitCase().getContentById(contentID);
-                    TagsManager tagsManager = Case.getCurrentCaseThrows().getServices().getTagsManager();
-                    List<ContentTag> tags = tagsManager.getContentTagsByContent(content);
-
-                    if (tags.stream()
-                            .map(tag -> tag.getName().getDisplayName())
-                            .filter(TagsManager.getNotableTagDisplayNames()::contains)
-                            .collect(Collectors.toList())
-                            .isEmpty()) {
-
-                        // There are no more bad tags on the object
-                        if (content instanceof AbstractFile) {
-                            af = (AbstractFile) content;
-                            knownStatus = TskData.FileKnown.UNKNOWN;
-                        } else {
-                            LOGGER.log(Level.WARNING, "Error updating non-file object");
-                            return;
-                        }
-                    } else {
-                        // There's still at least one bad tag, so leave the known status as is
-                        return;
-                    }
-                } catch (TskCoreException | NoCurrentCaseException ex) {
-                    LOGGER.log(Level.SEVERE, "Failed to find content", ex);
-                    return;
+                else {
+                    // There are no more bad tags on the object.  So, remove the comment if comment is same as CR tag
+                    String comment = tagDeletedEvent.getDeletedTagInfo().getComment();
+                    setContentKnownStatus(af, comment, TskData.FileKnown.UNKNOWN, true);
                 }
+            } catch (TskCoreException | NoCurrentCaseException ex) {
+                LOGGER.log(Level.SEVERE, "Failed to find content", ex);
+                return;
             }
+        }
+        
+        private void handleTagAdded(ContentTagAddedEvent tagAddedEvent) {
+            // For added tags, we want to change the known status to BAD if the 
+            // tag that was just added is in the list of central repo tags.
+            final ContentTag tagAdded = tagAddedEvent.getAddedTag();
+            
+            // The added tag isn't flagged as bad in central repo, so do nothing
+            if (!isNotableTag(tagAdded))
+                return;
+            
+            AbstractFile af = getAbstractFile(tagAdded.getContent());
+            if (af == null)
+                return;
+            
+            setContentKnownStatus(af, tagAdded.getComment(), TskData.FileKnown.BAD, false);
+        }
+        
+        private AbstractFile getAbstractFile(Content content) {
+            if (content instanceof AbstractFile) {
+                return (AbstractFile) content;
+            } 
+            else {
+                LOGGER.log(Level.WARNING, "Error updating non-file object");
+                return null;
+            }
+        }
 
+        private void setContentKnownStatus(AbstractFile af, String comment, TskData.FileKnown knownStatus, boolean isTagDelete) {
             final CorrelationAttributeInstance eamArtifact = CorrelationAttributeUtil.makeCorrAttrFromFile(af);
             eamArtifact.setComment(comment);
 
@@ -253,86 +309,77 @@ final class CaseEventListener implements PropertyChangeListener {
 
         @Override
         public void run() {
-            if (!CentralRepository.isEnabled()) {
+            if (!CentralRepository.isEnabled())
+                return;
+
+            if (Case.Events.valueOf(event.getPropertyName()) == Case.Events.BLACKBOARD_ARTIFACT_TAG_ADDED) {
+                handleTagAdded((BlackBoardArtifactTagAddedEvent) event);
+            } 
+            else { 
+                handleTagDeleted((BlackBoardArtifactTagDeletedEvent) event);
+            }
+        } // BLACKBOARD_ARTIFACT_TAG_ADDED, BLACKBOARD_ARTIFACT_TAG_DELETED
+        
+        private void handleTagDeleted(BlackBoardArtifactTagDeletedEvent tagDeletedEvent) {
+            //BLACKBOARD_ARTIFACT_TAG_DELETED
+            Case openCase;
+            try {
+                openCase = Case.getCurrentCaseThrows();
+            } catch (NoCurrentCaseException ex) {
+                LOGGER.log(Level.SEVERE, "Exception while getting open case.", ex);
+                return;
+            }
+            // For deleted tags, we want to set the file status to UNKNOWN if:
+            //   - The tag that was just removed is notable in central repo
+            //   - There are no remaining tags that are notable 
+
+            long contentID = tagDeletedEvent.getDeletedTagInfo().getContentID();
+            long artifactID = tagDeletedEvent.getDeletedTagInfo().getArtifactID();
+
+            if (!isNotableTagName(tagDeletedEvent.getDeletedTagInfo().getName())) {
+                // If the tag that got removed isn't on the list of central repo tags, do nothing
                 return;
             }
 
-            Content content;
-            BlackboardArtifact bbArtifact;
-            TskData.FileKnown knownStatus;
-            String comment;
-            boolean isTagDeleted;
-            if (Case.Events.valueOf(event.getPropertyName()) == Case.Events.BLACKBOARD_ARTIFACT_TAG_ADDED) {
-                // For added tags, we want to change the known status to BAD if the 
-                // tag that was just added is in the list of central repo tags.
-                final BlackBoardArtifactTagAddedEvent tagAddedEvent = (BlackBoardArtifactTagAddedEvent) event;
-                final BlackboardArtifactTag tagAdded = tagAddedEvent.getAddedTag();
-                
-                comment = tagAdded.getComment();
-                isTagDeleted = false;
-                
-                if (TagsManager.getNotableTagDisplayNames().contains(tagAdded.getName().getDisplayName())) {
-                    content = tagAdded.getContent();
-                    bbArtifact = tagAdded.getArtifact();
-                    knownStatus = TskData.FileKnown.BAD;    
-                } else {
-                    // The added tag isn't flagged as bad in central repo, so do nothing
-                    return;
-                }
-            } else { //BLACKBOARD_ARTIFACT_TAG_DELETED
-                Case openCase;
-                try {
-                    openCase = Case.getCurrentCaseThrows();
-                } catch (NoCurrentCaseException ex) {
-                    LOGGER.log(Level.SEVERE, "Exception while getting open case.", ex);
-                    return;
-                }
-                // For deleted tags, we want to set the file status to UNKNOWN if:
-                //   - The tag that was just removed is notable in central repo
-                //   - There are no remaining tags that are notable 
-                final BlackBoardArtifactTagDeletedEvent tagDeletedEvent = (BlackBoardArtifactTagDeletedEvent) event;
-                long contentID = tagDeletedEvent.getDeletedTagInfo().getContentID();
-                long artifactID = tagDeletedEvent.getDeletedTagInfo().getArtifactID();
-                
-                comment = tagDeletedEvent.getDeletedTagInfo().getComment();
-                isTagDeleted = true;
-                
-                String tagName = tagDeletedEvent.getDeletedTagInfo().getName().getDisplayName();
-                if (!TagsManager.getNotableTagDisplayNames().contains(tagName)) {
-                    // If the tag that got removed isn't on the list of central repo tags, do nothing
-                    return;
-                }
+            try {
+                // Get the remaining tags on the artifact
+                Content content = openCase.getSleuthkitCase().getContentById(contentID);
+                BlackboardArtifact bbArtifact = openCase.getSleuthkitCase().getBlackboardArtifact(artifactID);
+                TagsManager tagsManager = openCase.getServices().getTagsManager();
+                List<BlackboardArtifactTag> tags = tagsManager.getBlackboardArtifactTagsByArtifact(bbArtifact);
 
-                try {
-                    // Get the remaining tags on the artifact
-                    content = openCase.getSleuthkitCase().getContentById(contentID);
-                    bbArtifact = openCase.getSleuthkitCase().getBlackboardArtifact(artifactID);
-                    TagsManager tagsManager = openCase.getServices().getTagsManager();
-                    List<BlackboardArtifactTag> tags = tagsManager.getBlackboardArtifactTagsByArtifact(bbArtifact);
-
-                    if (tags.stream()
-                            .map(tag -> tag.getName().getDisplayName())
-                            .filter(TagsManager.getNotableTagDisplayNames()::contains)
-                            .collect(Collectors.toList())
-                            .isEmpty()) {
-
-                        // There are no more bad tags on the object
-                        knownStatus = TskData.FileKnown.UNKNOWN;
-
-                    } else {
-                        // There's still at least one bad tag, so leave the known status as is
-                        return;
-                    }
-                } catch (TskCoreException ex) {
-                    LOGGER.log(Level.SEVERE, "Failed to find content", ex);
-                    return;
+                if (hasNotableTag(tags)) {
+                    // There's still at least one bad tag, so leave the known status but update the comment to most recent
+                    String comment = getMostRecentNotableComment(tags);
+                    setArtifactKnownStatus(content, bbArtifact, comment, TskData.FileKnown.BAD, false);
                 }
+                else {
+                    // There are no more bad tags on the object.  So, remove the comment if comment is same as CR tag
+                    String comment = tagDeletedEvent.getDeletedTagInfo().getComment();
+                    setArtifactKnownStatus(content, bbArtifact, comment, TskData.FileKnown.UNKNOWN, true);
+                }
+            } catch (TskCoreException ex) {
+                LOGGER.log(Level.SEVERE, "Failed to find content", ex);
+                return;
             }
+        }
+        
+        private void handleTagAdded(BlackBoardArtifactTagAddedEvent tagAddedEvent) {
+            // For added tags, we want to change the known status to BAD if the 
+            // tag that was just added is in the list of central repo tags.
+            final BlackboardArtifactTag tagAdded = tagAddedEvent.getAddedTag();
+            
+            // The added tag isn't flagged as bad in central repo, so do nothing
+            if (!isNotableTag(tagAdded)) 
+                return;
+                
+            setArtifactKnownStatus(tagAdded.getContent(), tagAdded.getArtifact(), tagAdded.getComment(), TskData.FileKnown.BAD, false);
+        }
 
+        private void setArtifactKnownStatus(Content content, BlackboardArtifact bbArtifact, String comment, TskData.FileKnown knownStatus, boolean isTagDeleted) {
             if ((content instanceof AbstractFile) && (((AbstractFile) content).getKnown() == TskData.FileKnown.KNOWN)) {
                 return;
             }
-
             List<CorrelationAttributeInstance> convertedArtifacts = CorrelationAttributeUtil.makeCorrAttrsForCorrelation(bbArtifact);
             for (CorrelationAttributeInstance eamArtifact : convertedArtifacts) {
                 eamArtifact.setComment(comment);
@@ -342,7 +389,7 @@ final class CaseEventListener implements PropertyChangeListener {
                     LOGGER.log(Level.SEVERE, "Error connecting to Central Repository database while setting artifact known status.", ex); //NON-NLS
                 }
             }
-        } // BLACKBOARD_ARTIFACT_TAG_ADDED, BLACKBOARD_ARTIFACT_TAG_DELETED
+        }
 
     }
 
