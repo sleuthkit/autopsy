a102b6ff906c8e11ee451b43f988d8d0a0fe8782
Mark McKinnon <mark.mckinnon@davenport.edu>
2020-05-19 15:06:59 -0400

    Update regripper plugins
    Update the plugins with Wills PR change on the regripper repo that has not been merged as of yet. PR on Regripper repo is https://github.com/keydet89/RegRipper2.8/pull/31

--- thirdparty/rr-full/plugins/appcompatcache.pl
+++ thirdparty/rr-full/plugins/appcompatcache.pl
@@ -304,6 +304,7 @@ sub appWin8 {
 	
 	while($ofs < $len) {
 		my $tag = unpack("V",substr($data,$ofs,4));
+        last unless (defined $tag);
 # 32-bit		
 		if ($tag == 0x73746f72) {
 			$jmp = unpack("V",substr($data,$ofs + 8,4));
@@ -349,6 +350,7 @@ sub appWin81 {
 	
 	while ($ofs < $len) {
 		$tag = substr($data,$ofs,4);
+        last unless (defined $tag);
 		if ($tag eq "10ts") {
 			
 			$sz = unpack("V",substr($data,$ofs + 0x08,4));
