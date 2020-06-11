07319d764c4170b4e820dd44f4a221ffa6dd0e70
Ethan Roseman <eroseman@basistech.com>
2020-06-11 13:33:17 -0400

    6390 PR suggestion

--- Core/src/org/sleuthkit/autopsy/centralrepository/datamodel/Persona.java
+++ Core/src/org/sleuthkit/autopsy/centralrepository/datamodel/Persona.java
@@ -417,7 +417,10 @@ public class Persona {
                 + " GROUP BY p.id";
 
         PersonaQueryCallback queryCallback = new PersonaQueryCallback();
-        CentralRepository.getInstance().executeSelectSQL(queryClause, queryCallback);
+        CentralRepository cr = CentralRepository.getInstance();
+        if (cr != null) {
+            cr.executeSelectSQL(queryClause, queryCallback);
+        }
 
         return queryCallback.getPersonas();
     }
