16ef4b3b3e3136f8fa634f9ea37910df073b9053
William Schaefer <wschaefer@basistech.net>
2020-06-17 13:56:10 -0400

    6305 don't setpage when group is empty after page size change

--- Core/src/org/sleuthkit/autopsy/discovery/ResultsPanel.java
+++ Core/src/org/sleuthkit/autopsy/discovery/ResultsPanel.java
@@ -322,8 +322,10 @@ final class ResultsPanel extends javax.swing.JPanel {
                     logger.log(Level.SEVERE, "Error loading central repository database, no central repository options will be available for Discovery", ex);
                 }
             }
-            pageWorker = new PageWorker(searchFilters, groupingAttribute, groupSort, fileSortMethod, selectedGroupKey, startingEntry, pageSize, resultType, centralRepo);
-            pageWorker.execute();
+            if (groupSize != 0) {
+                pageWorker = new PageWorker(searchFilters, groupingAttribute, groupSort, fileSortMethod, selectedGroupKey, startingEntry, pageSize, resultType, centralRepo);
+                pageWorker.execute();
+            }
         }
     }
 
