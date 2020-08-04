3fb3e68c6b9349a7788151bff7f15b11cbd9f7cd
U-BASIS\dsmyda <dsmyda@win-dsmyd-2925.basistech.net>
2020-08-04 17:19:20 -0400

    Added support for HEIC files and merged EXIF into the Picture Analysis module

---
+++ Core/src/org/sleuthkit/autopsy/modules/pictureanalysis/PictureAnalysisIngestModuleFactory.java
@@ -0,0 +1,69 @@
+/*
+ * Autopsy Forensic Browser
+ *
+ * Copyright 2020 Basis Technology Corp.
+ * Contact: carrier <at> sleuthkit <dot> org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sleuthkit.autopsy.modules.pictureanalysis;
+
+import org.openide.util.NbBundle;
+import org.openide.util.lookup.ServiceProvider;
+import org.sleuthkit.autopsy.coreutils.Version;
+import org.sleuthkit.autopsy.ingest.FileIngestModule;
+import org.sleuthkit.autopsy.ingest.IngestModuleFactory;
+import org.sleuthkit.autopsy.ingest.IngestModuleFactoryAdapter;
+import org.sleuthkit.autopsy.ingest.IngestModuleIngestJobSettings;
+
+@ServiceProvider(service = IngestModuleFactory.class)
+public class PictureAnalysisIngestModuleFactory extends IngestModuleFactoryAdapter {
+
+    @NbBundle.Messages({
+        "PictureAnalysisIngestModuleFactory.module_name=Picture Analysis"
+    })
+    public static String getModuleName() {
+        return Bundle.PictureAnalysisIngestModuleFactory_module_name();
+    }
+    
+    @Override
+    public String getModuleDisplayName() {
+        return getModuleName();
+    }
+
+    @Override
+    @NbBundle.Messages({
+        "PictureAnalysisIngestModuleFactory.module_description=Performs general"
+                + " analysis on picture files, including extracting EXIF metadata"
+                + " and converting between formats."
+    })
+    public String getModuleDescription() {
+        return Bundle.PictureAnalysisIngestModuleFactory_module_description();
+    }
+
+    @Override
+    public String getModuleVersionNumber() {
+        return Version.getVersion();
+    }
+
+    @Override
+    public FileIngestModule createFileIngestModule(IngestModuleIngestJobSettings ingestOptions) {
+        return new PictureAnalysisIngestModule();
+    }
+    
+    @Override
+    public boolean isFileIngestModuleFactory() {
+        return true;
+    }
+
+}
