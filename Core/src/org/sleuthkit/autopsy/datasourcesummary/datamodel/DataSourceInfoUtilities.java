6e164dcfda9c5f8aa2912f9592f3ba39086639a5
Kelly Kelly <kelly@basistech.com>
2020-08-21 16:42:46 -0400

    Added getArtifact method to DataSourceInfoUtilities

--- Core/src/org/sleuthkit/autopsy/datasourcesummary/datamodel/DataSourceInfoUtilities.java
+++ Core/src/org/sleuthkit/autopsy/datasourcesummary/datamodel/DataSourceInfoUtilities.java
@@ -20,13 +20,20 @@ package org.sleuthkit.autopsy.datasourcesummary.datamodel;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.TreeMap;
 import java.util.logging.Level;
 import org.sleuthkit.autopsy.coreutils.Logger;
 import org.sleuthkit.datamodel.SleuthkitCase;
 import org.sleuthkit.datamodel.TskCoreException;
 import org.apache.commons.lang.StringUtils;
+import org.bouncycastle.util.Arrays;
 import org.sleuthkit.autopsy.casemodule.Case;
 import org.sleuthkit.autopsy.casemodule.NoCurrentCaseException;
+import org.sleuthkit.datamodel.BlackboardArtifact;
+import org.sleuthkit.datamodel.BlackboardAttribute;
 import org.sleuthkit.datamodel.TskData;
 import org.sleuthkit.datamodel.DataSource;
 import org.sleuthkit.datamodel.TskData.TSK_FS_META_FLAG_ENUM;
@@ -132,10 +139,112 @@ final class DataSourceInfoUtilities {
     static String getMetaFlagsContainsStatement(TSK_FS_META_FLAG_ENUM flag) {
         return "meta_flags & " + flag.getValue() + " > 0";
     }
+    
+    enum SortOrder {
+        DECENDING,
+        ASCENDING
+    }
+    
+    /**
+     * Return a list of artifacts that have been sorted by their attribute
+     * of attributeType.  
+     * 
+     * Sorting on attributes of type byte[] and JSON is not currently
+     * supported.
+     *
+     * @param skCase        SleuthkitCase instance.
+     * @param artifactType  Type of artifacts to sort.
+     * @param dataSource    Data Source that the artifact belongs to.
+     * @param attributeType Attribute type to sort by.
+     * @param sortOrder     Sort order of the attributes, either ascending or
+     *                      descending.
+     * @param maxCount      Maximum number of results to return. To return all
+     *                      values maxCount should be -1.
+     *
+     * @return A list of artifacts of type artifactType sorted by the attribute
+     *         of attributeType in the given sortOrder. If no artifacts are
+     *         found an empty list will be returned.
+     *
+     * @throws TskCoreException
+     */
+    static List<BlackboardArtifact> getArtifacts(SleuthkitCase skCase, BlackboardArtifact.Type artifactType, DataSource dataSource, BlackboardAttribute.Type attributeType, SortOrder sortOrder, int maxCount) throws TskCoreException {
+        if (maxCount < 1 && maxCount != -1) {
+            throw new IllegalArgumentException("Invalid maxCount passed to getArtifacts, value must be at greater 0");
+        }
+
+        TreeMap<BlackboardAttribute, BlackboardArtifact> sortedMap = new TreeMap<>(new AttributeComparator(sortOrder));
+        List<BlackboardArtifact> artifactList = skCase.getBlackboard().getArtifacts(artifactType.getTypeID(), dataSource.getId());
+
+        for (BlackboardArtifact artifact : artifactList) {
+            BlackboardAttribute attribute = artifact.getAttribute(attributeType);
+            if (attribute != null) {
+                sortedMap.put(attribute, artifact);
+            }
+        }
+
+        artifactList = new ArrayList<>();
+
+        for (BlackboardArtifact artifact : sortedMap.values()) {
+            artifactList.add(artifact);
+            if (maxCount != -1 && artifactList.size() == maxCount - 1) {
+                break;
+            }
+        } 
+        return artifactList;
+    }
 
     /**
      * Empty private constructor
      */
     private DataSourceInfoUtilities() {
+    }
+    
+    /**
+     * Compares the value of two BlackboardAttributes that are of the same type.
+     * This comparator is specialized for data source summary and only supports
+     * the basic attribute types of string, integer, long, datetime (long), and
+     * double.
+     */
+    private static class AttributeComparator implements Comparator<BlackboardAttribute> {
+
+        private final SortOrder direction;
+
+        AttributeComparator(SortOrder direction) {
+            this.direction = direction;
+        }
+
+        @Override
+        public int compare(BlackboardAttribute attribute1, BlackboardAttribute attribute2) {
+            if (attribute1.getAttributeType() != attribute2.getAttributeType()) {
+                throw new IllegalArgumentException("Unable to compare attributes of different types");
+            }
+
+            int result;
+            switch (attribute1.getValueType()) {
+                case STRING:
+                    result = attribute1.getValueString().compareTo(attribute2.getValueString());
+                    break;
+                case INTEGER:
+                    result = Integer.compare(attribute1.getValueInt(), attribute2.getValueInt());
+                    break;
+                case LONG:
+                case DATETIME:
+                    result = Long.compare(attribute1.getValueLong(), attribute2.getValueLong());
+                    break;
+                case DOUBLE:
+                    result = Double.compare(attribute1.getValueDouble(), attribute2.getValueDouble());
+                    break;
+                case BYTE:
+                case JSON:
+                default:
+                    throw new IllegalArgumentException("Unable to compare attributes of type " + attribute1.getAttributeType().getTypeName());
+            }
+
+            if (direction == SortOrder.DECENDING) {
+                result *= -1;
+            }
+
+            return result;
+        }
     }
 }
