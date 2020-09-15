89faf2c089b53322452fec89174955ebc02fd2d5
Greg DiCristofaro <gregd@basistech.com>
2020-09-14 20:27:55 -0400

    add in localhost

--- Core/src/org/sleuthkit/autopsy/datasourcesummary/datamodel/DataSourceUserActivitySummary.java
+++ Core/src/org/sleuthkit/autopsy/datasourcesummary/datamodel/DataSourceUserActivitySummary.java
@@ -71,9 +71,9 @@ public class DataSourceUserActivitySummary {
 
     private static final Comparator<TopAccountResult> TOP_ACCOUNT_RESULT_DATE_COMPARE = (a, b) -> a.getLastAccess().compareTo(b.getLastAccess());
     private static final Comparator<TopWebSearchResult> TOP_WEBSEARCH_RESULT_DATE_COMPARE = (a, b) -> a.getDateAccessed().compareTo(b.getDateAccessed());
+    
     private static final Set<String> DEVICE_EXCLUDE_LIST = new HashSet<>(Arrays.asList("ROOT_HUB", "ROOT_HUB20"));
-
-    private static final String LOOPBACK_ADDRESS = "127.0.0.1";
+    private static final Set<String> DOMAIN_EXCLUDE_LIST = new HashSet<>(Arrays.asList("127.0.0.1", "LOCALHOST"));
 
     private static final long MS_PER_DAY = 1000 * 60 * 60 * 24;
     private static final long DOMAIN_WINDOW_DAYS = 30;
@@ -215,7 +215,7 @@ public class DataSourceUserActivitySummary {
 
             // if there isn't a last access date or domain for this artifact, it can be ignored.
             // Also, ignore the loopback address.
-            if (artifactDateSecs == null || StringUtils.isBlank(domain) || LOOPBACK_ADDRESS.equals(domain.toUpperCase().trim())) {
+            if (artifactDateSecs == null || StringUtils.isBlank(domain) || DOMAIN_EXCLUDE_LIST.contains(domain.toUpperCase().trim())) {
                 continue;
             }
 
