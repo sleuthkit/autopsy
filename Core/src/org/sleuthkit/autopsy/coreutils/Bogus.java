35cde033b7c5dbab041edb377d71bb5382e89ef7
Richard Cordovano <rcordovano@basistech.com>
2020-09-22 17:08:56 -0400

    Test PR for Codacy rule change

---
+++ Core/src/org/sleuthkit/autopsy/coreutils/Bogus.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sleuthkit.autopsy.coreutils;
+
+
+public class Bogus {
+
+    private Bogus() {}
+    
+    static void foo() {
+        
+    }
+    
+    static void bar() {
+        
+    }
+    
+}
