2acbfc2712c36be3b98162f672794edab1798e76
Richard Cordovano <rcordovano@basistech.com>
2020-10-01 18:42:06 -0400

    Revert quoting of ProcessBuilder arguments

--- Core/src/org/sleuthkit/autopsy/report/modules/portablecase/PortableCaseReportModule.java
+++ Core/src/org/sleuthkit/autopsy/report/modules/portablecase/PortableCaseReportModule.java
@@ -1329,10 +1329,10 @@ public class PortableCaseReportModule implements ReportModule {
         File zipFile = Paths.get(tempZipFolder.getAbsolutePath(), caseName + ".zip").toFile(); // NON-NLS
         ProcessBuilder procBuilder = new ProcessBuilder();
         procBuilder.command(
-                String.format("\"%s\"",sevenZipExe.getAbsolutePath()),
+                sevenZipExe.getAbsolutePath(),
                 "a", // Add to archive
-                String.format("\"%s\"",zipFile.getAbsolutePath()),
-                String.format("\"%s\"",dirToCompress.toAbsolutePath().toString()),
+                zipFile.getAbsolutePath(),
+                dirToCompress.toAbsolutePath().toString(),
                 chunkOption
         );
 
