98f30bfc72e4cdd226f993c60992df6425d2f015
William Schaefer <wschaefer@basistech.net>
2020-10-23 15:42:46 -0400

    6774 convert row index to model

--- Core/src/org/sleuthkit/autopsy/discovery/ui/ArtifactsListPanel.java
+++ Core/src/org/sleuthkit/autopsy/discovery/ui/ArtifactsListPanel.java
@@ -77,7 +77,7 @@ class ArtifactsListPanel extends JPanel {
      *         selected.
      */
     BlackboardArtifact getSelectedArtifact() {
-        int selectedIndex = jTable1.getSelectionModel().getLeadSelectionIndex();
+        int selectedIndex = jTable1.convertRowIndexToModel(jTable1.getSelectionModel().getLeadSelectionIndex());
         if (selectedIndex < jTable1.getSelectionModel().getMinSelectionIndex() || jTable1.getSelectionModel().getMaxSelectionIndex() < 0 || selectedIndex > jTable1.getSelectionModel().getMaxSelectionIndex()) {
             return null;
         }
