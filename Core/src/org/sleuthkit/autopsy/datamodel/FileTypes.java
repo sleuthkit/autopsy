3fd6ec25ae3fa98e874c620b5c311ef68f6b716b
Raman Arora <raman@basistech.com>
2020-11-03 14:57:16 -0500

    3979: Make basic AnalysisResult infrastructure.

--- Core/src/org/sleuthkit/autopsy/datamodel/FileTypes.java
+++ Core/src/org/sleuthkit/autopsy/datamodel/FileTypes.java
@@ -20,6 +20,7 @@ package org.sleuthkit.autopsy.datamodel;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
@@ -34,6 +35,7 @@ import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 import org.openide.util.lookup.Lookups;
 import org.sleuthkit.autopsy.coreutils.Logger;
+import org.sleuthkit.datamodel.AnalysisResult;
 import org.sleuthkit.datamodel.BlackboardArtifact;
 import org.sleuthkit.datamodel.BlackboardAttribute;
 import org.sleuthkit.datamodel.Content;
@@ -43,6 +45,7 @@ import org.sleuthkit.datamodel.Directory;
 import org.sleuthkit.datamodel.File;
 import org.sleuthkit.datamodel.LayoutFile;
 import org.sleuthkit.datamodel.LocalFile;
+import org.sleuthkit.datamodel.Score;
 import org.sleuthkit.datamodel.SlackFile;
 import org.sleuthkit.datamodel.SleuthkitCase;
 import org.sleuthkit.datamodel.SleuthkitItemVisitor;
@@ -383,6 +386,11 @@ public final class FileTypes implements AutopsyVisitableItem {
         @Override
         public BlackboardArtifact newArtifact(int artifactTypeID) throws TskCoreException {
             return content.newArtifact(artifactTypeID);
+        }
+
+        @Override
+        public AnalysisResult newAnalysisResult(BlackboardArtifact.Type artifactType, Score score, String conclusion, String configuration, String justification, Collection<BlackboardAttribute> attributesList) throws TskCoreException {
+            return content.newAnalysisResult(artifactType, score, conclusion, configuration, justification, attributesList);
         }
 
         @Override
