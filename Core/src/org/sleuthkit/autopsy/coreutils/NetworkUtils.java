a843d34b2c883fc27e9c5b0b6fc87710b2c5d313
Greg DiCristofaro <gregd@basistech.com>
2020-11-24 12:37:10 -0500

    domain categorization

--- Core/src/org/sleuthkit/autopsy/coreutils/NetworkUtils.java
+++ Core/src/org/sleuthkit/autopsy/coreutils/NetworkUtils.java
@@ -18,13 +18,16 @@
  */
 package org.sleuthkit.autopsy.coreutils;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.UnknownHostException;
-import java.util.StringTokenizer;
+import java.util.logging.Level;
 
 public class NetworkUtils {
-    
+
+    private static final Logger logger = Logger.getLogger(NetworkUtils.class.getName());
+
     private NetworkUtils() {
     }
 
@@ -32,7 +35,7 @@ public class NetworkUtils {
      * Set the host name variable. Sometimes the network can be finicky, so the
      * answer returned by getHostName() could throw an exception or be null.
      * Have it read the environment variable if getHostName() is unsuccessful.
-     * 
+     *
      * @return the local host name
      */
     public static String getLocalHostName() {
@@ -49,63 +52,11 @@ public class NetworkUtils {
         }
         return hostName;
     }
-    
-    /**
-     * Attempt to manually extract the domain from a URL.
-     * 
-     * @param url
-     * @return empty string if no domain could be found
-     */
-    private static String getBaseDomain(String url) {
-        String host = null;
-        
-        //strip protocol
-        String cleanUrl = url.replaceFirst(".*:\\/\\/", "");
-
-        //strip after slashes
-        String dirToks[] = cleanUrl.split("\\/");
-        if (dirToks.length > 0) {
-            host = dirToks[0];
-        } else {
-            host = cleanUrl;
-        }
-
-        //get the domain part from host (last 2)
-        StringTokenizer tok = new StringTokenizer(host, ".");
-        StringBuilder hostB = new StringBuilder();
-        int toks = tok.countTokens();
-
-        for (int count = 0; count < toks; ++count) {
-            String part = tok.nextToken();
-            int diff = toks - count;
-            if (diff < 3) {
-                hostB.append(part);
-            }
-            if (diff == 2) {
-                hostB.append(".");
-            }
-        }
-        
-        
-        String base = hostB.toString();
-        // verify there are no special characters in there
-        if (base.matches(".*[~`!@#$%^&\\*\\(\\)\\+={}\\[\\];:\\?<>,/ ].*")) {
-            return "";
-        }
-        
-        //verify that the base domain actually has a '.', details JIRA-4609
-        if(!base.contains(".")) {
-            return "";
-        }
-        
-        return base;
-    }
 
     /**
-     * Attempt to extract the domain from a URL.
-     * Will start by using the built-in URL class, and if that fails will
-     * try to extract it manually.
-     * 
+     * Attempt to extract the domain from a URL. Will start by using the
+     * built-in URL class, and if that fails will try to extract it manually.
+     *
      * @param urlString The URL to extract the domain from
      * @return empty string if no domain name was found
      */
@@ -124,9 +75,13 @@ public class NetworkUtils {
 
         //was not a valid URL, try a less picky method
         if (result == null || result.trim().isEmpty()) {
-            return getBaseDomain(urlString);
+            try {
+                return DomainCategorizer.getInstance().getDomain(urlString);
+            } catch (IOException ex) {
+                logger.log(Level.SEVERE, "Unable to load resources for domain categorization.", ex);
+            }
         }
         return result;
     }
-    
+
 }
