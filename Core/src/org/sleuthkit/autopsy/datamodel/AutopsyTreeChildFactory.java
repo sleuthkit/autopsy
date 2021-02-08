a2e0ade69167e044496622fdd2012f8973379fde
Greg DiCristofaro <gregd@basistech.com>
2021-02-08 09:12:36 -0500

    updates to getHost

--- Core/src/org/sleuthkit/autopsy/datamodel/AutopsyTreeChildFactory.java
+++ Core/src/org/sleuthkit/autopsy/datamodel/AutopsyTreeChildFactory.java
@@ -100,8 +100,7 @@ public final class AutopsyTreeChildFactory extends ChildFactory.Detachable<Objec
                     return true;
                 } else {
                     // otherwise, just show host level
-                    Set<Host> hosts = tskCase.getHostManager().getHostsByStatus(HostStatus.ACTIVE);
-                    hosts.stream()
+                    tskCase.getHostManager().getHosts().stream()
                             .map(HostGrouping::new)
                             .sorted()
                             .forEach(list::add);
