166fda3ff3aa9dc8008eed3c8ce51e44e2a2e136
Kelly Kelly <kelly@basistech.com>
2021-03-08 14:45:26 -0500

    Removed the admin property from the content viewer

--- Core/src/org/sleuthkit/autopsy/contentviewers/osaccount/OsAccountDataPanel.java
+++ Core/src/org/sleuthkit/autopsy/contentviewers/osaccount/OsAccountDataPanel.java
@@ -146,8 +146,6 @@ public class OsAccountDataPanel extends JPanel {
         data.addData(Bundle.OsAccountDataPanel_basic_address(),
                 account.getName() == null || account.getName().isEmpty() ? "" : account.getName());
 
-        data.addData(Bundle.OsAccountDataPanel_basic_admin(), getIsAdminValue(account.isAdmin()));
-
         data.addData(Bundle.OsAccountDataPanel_basic_type(), account.getOsAccountType().getName());
         
         Optional<Long> crTime = account.getCreationTime();
@@ -289,28 +287,4 @@ public class OsAccountDataPanel extends JPanel {
 
         return constraints;
     }
-
-    @Messages({
-        "OsAccountDataPanel_basic_yes=Yes",
-        "OsAccountDataPanel_basic_no=No",
-        "OsAccountDataPanel_basic_unknown=Unknown",})
-    /**
-     * Return a string representation of the isAdmin status for OsAccounts.
-     *
-     * @param isAdmin Admin state for the OsAccount
-     *
-     * @return String representation of the admin state.
-     */
-    private String getIsAdminValue(Optional<Boolean> isAdmin) {
-        if (!isAdmin.isPresent()) {
-            return Bundle.OsAccountDataPanel_basic_unknown();
-        } else {
-            if (isAdmin.get()) {
-                return Bundle.OsAccountDataPanel_basic_yes();
-            } else {
-                return Bundle.OsAccountDataPanel_basic_no();
-            }
-        }
-    }
-
 }
