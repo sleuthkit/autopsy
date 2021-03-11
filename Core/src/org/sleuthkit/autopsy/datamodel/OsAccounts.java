b09ccea25de55d293f0a24ca18ba2b38d6f18d4f
Kelly Kelly <kelly@basistech.com>
2021-03-11 15:31:33 -0500

    merged in latest develop

--- Core/src/org/sleuthkit/autopsy/datamodel/OsAccounts.java
+++ Core/src/org/sleuthkit/autopsy/datamodel/OsAccounts.java
@@ -24,6 +24,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
