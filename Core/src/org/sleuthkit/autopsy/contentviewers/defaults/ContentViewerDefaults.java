31ee21d2ca265e9742d7766579134303ed0daf75
Greg DiCristofaro <gregd@basistech.com>
2021-06-02 14:56:05 -0400

    supplier fix

--- Core/src/org/sleuthkit/autopsy/contentviewers/defaults/ContentViewerDefaults.java
+++ Core/src/org/sleuthkit/autopsy/contentviewers/defaults/ContentViewerDefaults.java
@@ -5,10 +5,11 @@
  */
 package org.sleuthkit.autopsy.contentviewers.defaults;
 
+import com.google.common.base.Suppliers;
 import java.awt.Font;
 import java.awt.Insets;
 import java.awt.Toolkit;
-import java.util.function.Supplier;
+import com.google.common.base.Supplier;
 import javax.swing.UIManager;
 
 /**
@@ -16,41 +17,22 @@ import javax.swing.UIManager;
  * @author gregd
  */
 public class ContentViewerDefaults {
-
-    private static class Cacheable<T> {
-
-        private final Supplier<T> itemProvider;
-        private T item = null;
-
-        Cacheable(Supplier<T> itemProvider) {
-            this.itemProvider = itemProvider;
-        }
-
-        T get() {
-            if (item == null) {
-                item = itemProvider.get();
-            }
-
-            return item;
-        }
-    }
-
-    private static final Cacheable<Font> DEFAULT_FONT = new Cacheable<>(() -> UIManager.getDefaults().getFont("Label.font"));
+    private static final Supplier<Font> DEFAULT_FONT = Suppliers.memoize(() -> UIManager.getDefaults().getFont("Label.font"));
     
-    private static final Cacheable<Integer> DEFAULT_FONT_PX = new Cacheable<>(() -> {
+    private static final Supplier<Integer> DEFAULT_FONT_PX = Suppliers.memoize(() -> {
         // based on https://stackoverflow.com/questions/5829703/java-getting-a-font-with-a-specific-height-in-pixels/26564924#26564924
         return (int) Math.round(DEFAULT_FONT.get().getSize() * Toolkit.getDefaultToolkit().getScreenResolution() / 72.0);
     });
     
-    private static final Cacheable<Font> HEADER_FONT = new Cacheable<>(() -> {
+    private static final Supplier<Font> HEADER_FONT = Suppliers.memoize(() -> {
         Font defaultFont = DEFAULT_FONT.get();
         return defaultFont.deriveFont(Font.BOLD, defaultFont.getSize() + 2);
     });
         
-    private static final Cacheable<Insets> DEFAULT_PANEL_INSETS = new Cacheable<>(() -> UIManager.getDefaults().getInsets("TextPane.margin"));
+    private static final Supplier<Insets> DEFAULT_PANEL_INSETS = Suppliers.memoize(() -> UIManager.getDefaults().getInsets("TextPane.margin"));
     
-    private static final Cacheable<Integer> DEFAULT_INDENT = new Cacheable<>(() -> DEFAULT_FONT_PX.get());
-    private static final Cacheable<Integer> DEFAULT_SECTION_SPACING = new Cacheable<>(() -> DEFAULT_FONT_PX.get());
+    private static final Supplier<Integer> DEFAULT_INDENT = Suppliers.memoize(() -> DEFAULT_FONT_PX.get());
+    private static final Supplier<Integer> DEFAULT_SECTION_SPACING = Suppliers.memoize(() -> DEFAULT_FONT_PX.get());
     
 
     
