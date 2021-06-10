21048b038c2aefb902e0284c42448c996be23e5e
Kelly Kelly <kelly@basistech.com>
2021-06-10 15:14:51 -0400

    Modified other locations to make sure nodes are created async

--- Core/src/org/sleuthkit/autopsy/datamodel/AbstractContentNode.java
+++ Core/src/org/sleuthkit/autopsy/datamodel/AbstractContentNode.java
@@ -112,7 +112,7 @@ public abstract class AbstractContentNode<T extends Content> extends ContentNode
      * @param lookup  The Lookup object for the node.
      */
     AbstractContentNode(T content, Lookup lookup) {
-        super(Children.create(new ContentChildren(content), false), lookup);
+        super(Children.create(new ContentChildren(content), true), lookup);
         this.content = content;
         //super.setName(ContentUtils.getSystemName(content));
         super.setName("content_" + Long.toString(content.getId())); //NON-NLS
