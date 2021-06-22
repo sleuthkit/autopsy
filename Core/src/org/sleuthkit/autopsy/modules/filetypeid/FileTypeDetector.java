ef26e072f48ae39c0894cbf78973514026cec055
apriestman <apriestman@basistech.com>
2021-06-22 10:57:12 -0400

    Removing timing

--- Core/src/org/sleuthkit/autopsy/modules/filetypeid/FileTypeDetector.java
+++ Core/src/org/sleuthkit/autopsy/modules/filetypeid/FileTypeDetector.java
@@ -194,7 +194,6 @@ public class FileTypeDetector {
             // optional parameter attached.
             return removeOptionalParameter(mimeType);
         }
-        long startTime = java.lang.System.currentTimeMillis();
         /*
          * Mark non-regular files (refer to TskData.TSK_FS_META_TYPE_ENUM),
          * zero-sized files, unallocated space, and unused blocks (refer to
@@ -313,18 +312,6 @@ public class FileTypeDetector {
          * Documented side effect: write the result to the AbstractFile object.
          */
         file.setMIMEType(mimeType);
-
-        long endTime = java.lang.System.currentTimeMillis();
-        long elapsed = endTime - startTime;
-        synchronized(timingLock) {
-            totalFiles++;
-            totalMs+= elapsed;
-
-            if (totalFiles % 1000 == 0) {
-                double ave = totalMs / totalFiles;
-                System.out.println("### " + totalFiles + " processed - average time: " + ave + " ms (" + totalFiles + " files, " + totalMs + " total ms)");
-            }
-        }
         
         return mimeType;
     }
