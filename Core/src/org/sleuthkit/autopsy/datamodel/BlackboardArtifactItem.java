722cb2d898799b066dcaaac24f4c9dbef11c3c6c
Richard Cordovano <rcordovano@basistech.com>
2021-09-08 15:07:38 -0400

    7797 fix analysis result tagging

---
+++ Core/src/org/sleuthkit/autopsy/datamodel/BlackboardArtifactItem.java
@@ -0,0 +1,43 @@
+/*
+ * Autopsy Forensic Browser
+ *
+ * Copyright 2021-2021 Basis Technology Corp.
+ * Contact: carrier <at> sleuthkit <dot> org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sleuthkit.autopsy.datamodel;
+
+import com.google.common.annotations.Beta;
+import org.sleuthkit.datamodel.BlackboardArtifact;
+
+/**
+ * An abstract super class for an Autopsy Data Model item class with an
+ * underlying BlackboardArtifact Sleuth Kit Data Model object.
+ *
+ * @param <T> The concrete BlackboardArtifact sub class type.
+ */
+public abstract class BlackboardArtifactItem<T extends BlackboardArtifact> extends TskContentItem<T> {
+
+    /**
+     * Constructs an Autopsy Data Model item with an underlying
+     * BlackboardArtifact Sleuth Kit Data Model object.
+     *
+     * @param blackboardArtifact The BlackboardArtifact object.
+     */
+    @Beta
+    BlackboardArtifactItem(T blackboardArtifact) {
+        super(blackboardArtifact);
+    }
+
+}
