e7eb5788ae6ee6c10a12233e3bd4d2f1a5aebfcd
William Schaefer <wschaefer@basistech.net>
2021-09-15 18:13:33 -0400

    7917 refine comment on new count case method

--- Core/src/org/sleuthkit/autopsy/centralrepository/datamodel/CentralRepository.java
+++ Core/src/org/sleuthkit/autopsy/centralrepository/datamodel/CentralRepository.java
@@ -376,15 +376,20 @@ public interface CentralRepository {
     Long getCountUniqueCaseDataSourceTuplesHavingTypeValue(CorrelationAttributeInstance.Type aType, String value) throws CentralRepoException, CorrelationAttributeNormalizationException;
 
     /**
-     * Retrieves number of unique cases which have correlation attributes other
+     * Retrieves number of unique cases which have correlation attributes *
      * which are not associated with the specified file with the same type and
-     * value.
+     * value. Gets the count of cases that have an instance of a given
+     * correlation attribute.
+     *
+     * This count will ignore the specified instance of the correlation
+     * attribute and any other instances of this correlation attribute existing
+     * on the same object.
      *
      * @param instance The instance having its cases with other occurrences
      *                 counted.
      *
-     * @return Number of cases with additional instances of this attribute type
-     *         and value.
+     * @return Number of cases with additional instances of this
+     *         CorrelationAttributeInstance.
      */
     Long getCountCasesWithOtherInstances(CorrelationAttributeInstance instance) throws CentralRepoException, CorrelationAttributeNormalizationException;
 
