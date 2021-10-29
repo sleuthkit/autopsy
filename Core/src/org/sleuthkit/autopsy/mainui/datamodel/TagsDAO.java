4f018b328130555015cccb703d1175582585e537
Eugene Livis <elivis@basistech.com>
2021-10-29 10:39:34 -0400

    Cleaning things up

--- Core/src/org/sleuthkit/autopsy/mainui/datamodel/TagsDAO.java
+++ Core/src/org/sleuthkit/autopsy/mainui/datamodel/TagsDAO.java
@@ -32,18 +32,15 @@ import org.sleuthkit.autopsy.casemodule.Case;
 import org.sleuthkit.autopsy.casemodule.NoCurrentCaseException;
 import org.sleuthkit.autopsy.core.UserPreferences;
 import org.sleuthkit.autopsy.coreutils.TimeZoneUtils;
-import org.sleuthkit.autopsy.datamodel.FileTypeExtensions;
-import org.sleuthkit.autopsy.mainui.datamodel.FileRowDTO.ExtensionMediaType;
 import org.sleuthkit.datamodel.AbstractFile;
 import org.sleuthkit.datamodel.BlackboardArtifactTag;
 import org.sleuthkit.datamodel.Content;
 import org.sleuthkit.datamodel.ContentTag;
-import org.sleuthkit.datamodel.SleuthkitCase;
 import org.sleuthkit.datamodel.TagName;
 import org.sleuthkit.datamodel.TskCoreException;
 
 /**
- * Provides information to populate the results viewer for data in the views
+ * Provides information to populate the results viewer for data in the tags
  * section.
  */
 @Messages({"TagsDAO.fileColumns.nameColLbl=Name",
@@ -66,7 +63,7 @@ import org.sleuthkit.datamodel.TskCoreException;
     "TagsDAO.tagColumns.userNameColLbl=User Name"})
 public class TagsDAO {
 
-    private static final int CACHE_SIZE = 15; // rule of thumb: 5 entries times number of cached SearchParams sub-types
+    private static final int CACHE_SIZE = 5; // rule of thumb: 5 entries times number of cached SearchParams sub-types
     private static final long CACHE_DURATION = 2;
     private static final TimeUnit CACHE_DURATION_UNITS = TimeUnit.MINUTES;    
     private final Cache<SearchParams<?>, SearchResultsDTO> searchParamsCache = CacheBuilder.newBuilder().maximumSize(CACHE_SIZE).expireAfterAccess(CACHE_DURATION, CACHE_DURATION_UNITS).build();
@@ -75,12 +72,10 @@ public class TagsDAO {
     
     private static final String FILE_TAG_TYPE_ID = "FILE_TAG";
     private static final String RESULT_TAG_TYPE_ID = "RESULT_TAG";
-    private static final String FILE_TAG_DISPLAY_NAME = "File Tag";
-    private static final String RESULT_TAG_DISPLAY_NAME = "Result Tag";
     
     private static final List<ColumnKey> FILE_TAG_COLUMNS = Arrays.asList(
             getFileColumnKey(Bundle.TagsDAO_fileColumns_nameColLbl()),
-            getFileColumnKey(Bundle.TagsDAO_fileColumns_originalName()), // ELODO handle translation
+            getFileColumnKey(Bundle.TagsDAO_fileColumns_originalName()), // GVDTODO handle translation
             getFileColumnKey(Bundle.TagsDAO_fileColumns_filePathColLbl()),
             getFileColumnKey(Bundle.TagsDAO_fileColumns_commentColLbl()),
             getFileColumnKey(Bundle.TagsDAO_fileColumns_modifiedTimeColLbl()),
@@ -112,45 +107,14 @@ public class TagsDAO {
     private static ColumnKey getFileColumnKey(String name) {
         return new ColumnKey(name, name, Bundle.TagsDAO_fileColumns_noDescription());
     }
-
-    private SleuthkitCase getCase() throws NoCurrentCaseException {
-        return Case.getCurrentCaseThrows().getSleuthkitCase();
-    }
-    
-    static ExtensionMediaType getExtensionMediaType(String ext) {
-        if (StringUtils.isBlank(ext)) {
-            return ExtensionMediaType.UNCATEGORIZED;
-        } else {
-            ext = "." + ext;
-        }
-        if (FileTypeExtensions.getImageExtensions().contains(ext)) {
-            return ExtensionMediaType.IMAGE;
-        } else if (FileTypeExtensions.getVideoExtensions().contains(ext)) {
-            return ExtensionMediaType.VIDEO;
-        } else if (FileTypeExtensions.getAudioExtensions().contains(ext)) {
-            return ExtensionMediaType.AUDIO;
-        } else if (FileTypeExtensions.getDocumentExtensions().contains(ext)) {
-            return ExtensionMediaType.DOC;
-        } else if (FileTypeExtensions.getExecutableExtensions().contains(ext)) {
-            return ExtensionMediaType.EXECUTABLE;
-        } else if (FileTypeExtensions.getTextExtensions().contains(ext)) {
-            return ExtensionMediaType.TEXT;
-        } else if (FileTypeExtensions.getWebExtensions().contains(ext)) {
-            return ExtensionMediaType.WEB;
-        } else if (FileTypeExtensions.getPDFExtensions().contains(ext)) {
-            return ExtensionMediaType.PDF;
-        } else if (FileTypeExtensions.getArchiveExtensions().contains(ext)) {
-            return ExtensionMediaType.ARCHIVE;
-        } else {
-            return ExtensionMediaType.UNCATEGORIZED;
-        }
-    }
     
     public SearchResultsDTO getTags(TagsSearchParams key, long startItem, Long maxCount, boolean hardRefresh) throws ExecutionException, IllegalArgumentException {
         if (key.getTagName() == null) {
-            throw new IllegalArgumentException("Must have non-null filter");
+            throw new IllegalArgumentException("Must have non-null tag name");
         } else if (key.getDataSourceId() != null && key.getDataSourceId() <= 0) {
             throw new IllegalArgumentException("Data source id must be greater than 0 or null");
+        } else if (key.getTagType() == null) {
+            throw new IllegalArgumentException("Must have non-null tag type");
         }
         
         SearchParams<TagsSearchParams> searchParams = new SearchParams<>(key, startItem, maxCount);
@@ -161,33 +125,18 @@ public class TagsDAO {
         return searchParamsCache.get(searchParams, () -> fetchTagsDTOs(key.getTagName(), key.getTagType(), key.getDataSourceId(), startItem, maxCount));
     }
 
-    @NbBundle.Messages({"FileTag.name.text=File Tag"})
+    @NbBundle.Messages({"FileTag.name.text=File Tag",
+            "ResultTag.name.text=Result Tag"})
     private SearchResultsDTO fetchTagsDTOs(TagName tagName, TagsSearchParams.TagType type, Long dataSourceId, long startItem, Long maxResultCount) throws NoCurrentCaseException, TskCoreException {
-        if (null == type) {
-            // ELTODO throw?
-            return null;
-        }
-
         switch (type) {
             case FILE:
                 return fetchFileTags(tagName, dataSourceId, startItem, maxResultCount);
             case RESULT:
                 return fetchResultTags(tagName, dataSourceId, startItem, maxResultCount);
             default:
-                // ELTODO throw?
-            return null;
+                throw new IllegalArgumentException("Unsupported tag type");
         }
     }
-    
-    /* GET RESULT TAGS
-     * BlackboardArtifactTagNodeFactory.createKeys(List<BlackboardArtifactTag> tags)
-     * BlackboardArtifactTagNode.createSheet()
-     */
-    
-    /* GET FILE TAGS
-     * ContentTagNodeFactory.createKeys(List<ContentTag> tags)
-     * ContentTagNode.createSheet()
-     */
     
     private SearchResultsDTO fetchResultTags(TagName tagName, Long dataSourceId, long startItem, Long maxResultCount) throws NoCurrentCaseException, TskCoreException {
 
@@ -227,7 +176,7 @@ public class TagsDAO {
 
             List<Object> cellValues = Arrays.asList(
                     name,
-                    null, // ELTODO translation column
+                    null, // GVDTODO translation column
                     contentPath,
                     tag.getArtifact().getDisplayName(),
                     tag.getComment(),
@@ -239,7 +188,7 @@ public class TagsDAO {
                     tag.getId()));
         }
 
-        return new BaseSearchResultsDTO(RESULT_TAG_TYPE_ID, RESULT_TAG_DISPLAY_NAME, RESULT_TAG_COLUMNS, fileRows, startItem, fileRows.size());
+        return new BaseSearchResultsDTO(RESULT_TAG_TYPE_ID, Bundle.ResultTag_name_text(), RESULT_TAG_COLUMNS, fileRows, 0, fileRows.size());
     }
     
     private SearchResultsDTO fetchFileTags(TagName tagName, Long dataSourceId, long startItem, Long maxResultCount) throws NoCurrentCaseException, TskCoreException {
@@ -269,7 +218,7 @@ public class TagsDAO {
 
             List<Object> cellValues = Arrays.asList(
                     content.getName(),
-                    null, // ELTODO translation column
+                    null, // GVDTODO translation column
                     contentPath,
                     tag.getComment(),
                     file != null ? TimeZoneUtils.getFormattedTime(file.getMtime()) : "",
@@ -286,6 +235,6 @@ public class TagsDAO {
                     file.getId()));
         }
 
-        return new BaseSearchResultsDTO(FILE_TAG_TYPE_ID, FILE_TAG_DISPLAY_NAME, FILE_TAG_COLUMNS, fileRows, startItem, fileRows.size());
+        return new BaseSearchResultsDTO(FILE_TAG_TYPE_ID, Bundle.FileTag_name_text(), FILE_TAG_COLUMNS, fileRows, 0, fileRows.size());
     }
 }
