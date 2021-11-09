3417c864852d35ea6895f661ec0d7400dea38f1c
Greg DiCristofaro <gregd@basistech.com>
2021-11-09 15:33:50 -0500

    abstract dao

---
+++ Core/src/org/sleuthkit/autopsy/mainui/datamodel/AbstractDao.java
@@ -0,0 +1,34 @@
+/*
+ * Autopsy Forensic Browser
+ *
+ * Copyright 2021 Basis Technology Corp.
+ * Contact: carrier <at> sleuthkit <dot> org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sleuthkit.autopsy.mainui.datamodel;
+
+import java.beans.PropertyChangeEvent;
+
+/**
+ * Internal methods that DAOs implement.
+ */
+abstract class AbstractDao {
+    
+    /**
+     * Clear any cached data (Due to change in view
+     */
+    abstract void clearCaches();
+    
+    abstract void handleAutopsyEvent(PropertyChangeEvent evt);
+}
