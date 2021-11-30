df48c0d4538bc4b6ac714b94f47489c8d773b5f4
Kelly Kelly <kelly@basistech.com>
2021-11-30 14:01:22 -0500

    Added listener to update SCO column

--- Core/src/org/sleuthkit/autopsy/mainui/nodes/BaseNode.java
+++ Core/src/org/sleuthkit/autopsy/mainui/nodes/BaseNode.java
@@ -20,17 +20,29 @@ package org.sleuthkit.autopsy.mainui.nodes;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import java.lang.ref.WeakReference;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
-import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import javax.swing.Action;
 import javax.swing.SwingUtilities;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
 import org.openide.nodes.Sheet;
 import org.openide.util.Lookup;
+import org.openide.util.WeakListeners;
+import org.sleuthkit.autopsy.casemodule.Case;
+import org.sleuthkit.autopsy.casemodule.events.BlackBoardArtifactTagAddedEvent;
+import org.sleuthkit.autopsy.casemodule.events.BlackBoardArtifactTagDeletedEvent;
+import org.sleuthkit.autopsy.casemodule.events.CommentChangedEvent;
+import org.sleuthkit.autopsy.casemodule.events.ContentTagAddedEvent;
+import org.sleuthkit.autopsy.casemodule.events.ContentTagDeletedEvent;
 import org.sleuthkit.autopsy.datamodel.NodeProperty;
 import org.sleuthkit.autopsy.mainui.datamodel.BaseRowDTO;
 import org.sleuthkit.autopsy.mainui.datamodel.SearchResultsDTO;
@@ -38,24 +50,84 @@ import org.sleuthkit.autopsy.mainui.nodes.actions.ActionContext;
 import org.sleuthkit.autopsy.mainui.nodes.actions.ActionsFactory;
 import org.sleuthkit.autopsy.mainui.sco.SCOFetcher;
 import org.sleuthkit.autopsy.mainui.sco.SCOSupporter;
+import org.sleuthkit.autopsy.mainui.sco.SCOUtils;
+import org.sleuthkit.datamodel.BlackboardArtifact;
+import org.sleuthkit.datamodel.Content;
 
 /**
  * A a simple starting point for nodes.
  */
-public abstract class BaseNode<S extends SearchResultsDTO, R extends BaseRowDTO> extends AbstractNode implements ActionContext {
-    
+abstract class BaseNode<S extends SearchResultsDTO, R extends BaseRowDTO> extends AbstractNode implements ActionContext {
+
     private final S results;
     private final R rowData;
-    
+
+    private static final Set<Case.Events> CASE_EVENTS_OF_INTEREST = EnumSet.of(
+            Case.Events.BLACKBOARD_ARTIFACT_TAG_ADDED,
+            Case.Events.BLACKBOARD_ARTIFACT_TAG_DELETED,
+            Case.Events.CONTENT_TAG_ADDED,
+            Case.Events.CONTENT_TAG_DELETED,
+            Case.Events.CR_COMMENT_CHANGED,
+            Case.Events.CURRENT_CASE);
+
+    private final PropertyChangeListener listener = new PropertyChangeListener() {
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            String eventType = evt.getPropertyName();
+            if (eventType.equals(Case.Events.BLACKBOARD_ARTIFACT_TAG_ADDED.toString())) {
+                BlackBoardArtifactTagAddedEvent event = (BlackBoardArtifactTagAddedEvent) evt;
+                Optional<BlackboardArtifact> optional = getArtifact();
+                if (optional.isPresent()
+                        && event.getAddedTag().getArtifact().equals(optional.get())) {
+                    updateSCOColumns();
+                }
+            } else if (eventType.equals(Case.Events.BLACKBOARD_ARTIFACT_TAG_DELETED.toString())) {
+                BlackBoardArtifactTagDeletedEvent event = (BlackBoardArtifactTagDeletedEvent) evt;
+                Optional<BlackboardArtifact> optional = getArtifact();
+                if (optional.isPresent() && event.getDeletedTagInfo().getArtifactID() == optional.get().getArtifactID()) {
+                    updateSCOColumns();
+                }
+            } else if (eventType.equals(Case.Events.CONTENT_TAG_ADDED.toString())) {
+                ContentTagAddedEvent event = (ContentTagAddedEvent) evt;
+                Optional<Content> optional = getSourceContent();
+                if (optional.isPresent() && event.getAddedTag().getContent().equals(optional.get())) {
+                    updateSCOColumns();
+                }
+
+            } else if (eventType.equals(Case.Events.CONTENT_TAG_DELETED.toString())) {
+                ContentTagDeletedEvent event = (ContentTagDeletedEvent) evt;
+                Optional<Content> optional = getSourceContent();
+                if (optional.isPresent() && event.getDeletedTagInfo().getContentID() == optional.get().getId()) {
+                    updateSCOColumns();
+                }
+            } else if (eventType.equals(Case.Events.CR_COMMENT_CHANGED.toString())) {
+                CommentChangedEvent event = (CommentChangedEvent) evt;
+                Optional<Content> optional = getSourceContent();
+                if (optional.isPresent() && event.getContentID() == optional.get().getId()) {
+                    updateSCOColumns();
+                }
+            } else if (eventType.equals(Case.Events.CURRENT_CASE.toString())) {
+                if (evt.getNewValue() == null) {
+                    /*
+                     * The case has been closed.
+                     */
+//                    unregisterListener();
+                }
+            }
+        }
+    };
+
+    private PropertyChangeListener weakListener = null;
+
     /**
      * A pool of background tasks to run any long computation needed to populate
      * this node.
      */
     static final ExecutorService backgroundTasksPool;
     private static final Integer MAX_POOL_SIZE = 10;
-    
+
     private FutureTask<String> scoFutureTask;
-    
+
     static {
         //Initialize this pool only once! This will be used by every instance BaseNode
         //to do their heavy duty SCO column and translation updates.
@@ -67,6 +139,12 @@ public abstract class BaseNode<S extends SearchResultsDTO, R extends BaseRowDTO>
         super(children, lookup);
         this.results = results;
         this.rowData = rowData;
+
+        // If the S column is there register the listeners.
+        if (results.getColumns().stream().map(p -> p.getDisplayName()).collect(Collectors.toList()).contains(SCOUtils.SCORE_COLUMN_NAME)) {
+            weakListener = WeakListeners.propertyChange(listener, null);
+            Case.addEventTypeSubscriber(CASE_EVENTS_OF_INTEREST, weakListener);
+        }
     }
 
     /**
@@ -89,13 +167,8 @@ public abstract class BaseNode<S extends SearchResultsDTO, R extends BaseRowDTO>
 
     @Override
     protected Sheet createSheet() {
-        Sheet sheet =  ContentNodeUtil.setSheet(super.createSheet(), results.getColumns(), rowData.getCellValues());
-        
-        if((scoFutureTask == null || scoFutureTask.isDone()) && this instanceof SCOSupporter) {
-            scoFutureTask = new FutureTask<>(new SCOFetcher<>(new WeakReference<>((SCOSupporter)this)), "");
-            backgroundTasksPool.submit(scoFutureTask);
-        }
-        
+        Sheet sheet = ContentNodeUtil.setSheet(super.createSheet(), results.getColumns(), rowData.getCellValues());
+        updateSCOColumns();
         return sheet;
     }
 
@@ -103,7 +176,19 @@ public abstract class BaseNode<S extends SearchResultsDTO, R extends BaseRowDTO>
     public Action[] getActions(boolean context) {
         return ActionsFactory.getActions(this);
     }
-    
+
+    private void updateSCOColumns() {
+        if (scoFutureTask != null && !scoFutureTask.isDone()) {
+            scoFutureTask.cancel(true);
+            scoFutureTask = null;
+        }
+
+        if ((scoFutureTask == null || scoFutureTask.isDone()) && this instanceof SCOSupporter) {
+            scoFutureTask = new FutureTask<>(new SCOFetcher<>(new WeakReference<>((SCOSupporter) this)), "");
+            backgroundTasksPool.submit(scoFutureTask);
+        }
+    }
+
     /**
      * Updates the values of the properties in the current property sheet with
      * the new properties being passed in. Only if that property exists in the
@@ -116,7 +201,7 @@ public abstract class BaseNode<S extends SearchResultsDTO, R extends BaseRowDTO>
      *
      * @param newProps New file property instances to be updated in the current
      *                 sheet.
-     */ 
+     */
     protected synchronized void updateSheet(List<NodeProperty<?>> newProps) {
         SwingUtilities.invokeLater(() -> {
             /*
