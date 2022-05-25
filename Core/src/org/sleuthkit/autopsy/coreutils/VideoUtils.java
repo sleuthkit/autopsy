1c3e37031cb18727ce3510d62c1664c18d9beae6
Kelly Kelly <kelly@basistech.com>
2022-05-25 18:07:28 -0400

    New methods are now tested and working

--- Core/src/org/sleuthkit/autopsy/coreutils/VideoUtils.java
+++ Core/src/org/sleuthkit/autopsy/coreutils/VideoUtils.java
@@ -43,6 +43,7 @@ import org.opencv.videoio.VideoCapture;
 import org.opencv.videoio.VideoWriter;
 import org.opencv.videoio.Videoio;
 import org.openide.modules.InstalledFileLocator;
+import org.openide.util.Exceptions;
 import org.openide.util.NbBundle;
 import org.sleuthkit.autopsy.casemodule.Case;
 import org.sleuthkit.autopsy.casemodule.NoCurrentCaseException;
@@ -259,44 +260,42 @@ public class VideoUtils {
 
     /**
      * Compress the given the files. This method takes advantage of the exiting
-     * getVideoFile method to create a temp copy of the inputFile. It does not 
+     * getVideoFile method to create a temp copy of the inputFile. It does not
      * delete the temp file, it leaves it in the video temp file for future use.
-     * 
-     * When using this method there is no way to cancel the process between the 
+     *
+     * When using this method there is no way to cancel the process between the
      * creation of the temp file and the launching of the process. For better
      * control use the other compressVideo method.
      *
      * @param inputFile    The AbstractFile representing the video.
      * @param outputFile   Output file.
      * @param terminator   A processTerminator for the ffmpeg executable.
-     * @param ffmpegStdout A file to send the output of ffmpeg stdout.
-     * @param ffmpegStderr A file to send the output of ffmpeg stderr.
-     * 
+     * @param logFileDirectory A file to send the output of ffmpeg stdout.
+     *
      * @return The ffmpeg process exit value.
      *
      * @throws IOException
      */
-    static public int compressVideo(AbstractFile inputFile, File outputFile, ExecUtil.ProcessTerminator terminator, File ffmpegStdout, File ffmpegStderr) throws IOException {
-        return compressVideo(getVideoFile(inputFile), outputFile, terminator, ffmpegStdout, ffmpegStderr);
+    static public int compressVideo(AbstractFile inputFile, File outputFile, ExecUtil.ProcessTerminator terminator, File logFileDirectory) throws IOException {
+        return compressVideo(getVideoFile(inputFile), outputFile, terminator, logFileDirectory);
     }
 
     /**
      * Compress the given the files.
-     * 
-     * The output from ffmpeg seem to go to the error file not the
-     * out file. Text in the err file does not mean an issue occurred.
      *
-     * @param inputFile  Absolute path to input video.
-     * @param outputFile Path for scaled file.
-     * @param terminator A processTerminator for the ffmpeg executable.
-     * @param ffmpegStdout A file to send the output of ffmpeg stdout.
-     * @param ffmpegStderr A file to send the output of ffmpeg stderr.
-     * 
+     * The output from ffmpeg seem to go to the error file not the out file.
+     * Text in the err file does not mean an issue occurred.
+     *
+     * @param inputFile    Absolute path to input video.
+     * @param outputFile   Path for scaled file.
+     * @param terminator   A processTerminator for the ffmpeg executable.
+     * @param logFileDirectory Location to put the output of ffmpeg
+     *
      * @return The ffmpeg process exit value.
      *
      * @throws IOException
      */
-    static public int compressVideo(File inputFile, File outputFile, ExecUtil.ProcessTerminator terminator, File ffmpegStdout, File ffmpegStderr) throws IOException {
+    static public int compressVideo(File inputFile, File outputFile, ExecUtil.ProcessTerminator terminator, File logFileDirectory) throws IOException {
         Path executablePath = Paths.get(FFMPEG, FFMPEG_EXE);
         File exeFile = InstalledFileLocator.getDefault().locate(executablePath.toString(), VideoUtils.class.getPackage().getName(), true);
         if (exeFile == null) {
@@ -311,15 +310,31 @@ public class VideoUtils {
             throw new IOException(String.format("Failed to compress %s, output file already exists %s", inputFile.toString(), outputFile.toString()));
         }
 
+        File ffmpegStderr = null;
+        File ffmpegStdout = null;
+        
+        // In case the folder doesn't exist.
+        if(logFileDirectory != null) {
+            logFileDirectory.mkdirs();
+            ffmpegStderr = File.createTempFile("ffmpegstderr", ".txt", logFileDirectory);
+            ffmpegStdout = File.createTempFile("ffmpegstdout", ".txt", logFileDirectory);
+        } else {
+            throw new IOException("Invalid output file location null");
+        }
+
         ProcessBuilder processBuilder = buildProcessWithRunAsInvoker(
                 "\"" + exeFile.getAbsolutePath() + "\"",
                 "-i", "\"" + inputFile.toString() + "\"",
                 "-vcodec", "libx264",
                 "-crf", "28",
                 "\"" + outputFile.toString() + "\"");
-
+ 
         processBuilder.redirectError(ffmpegStderr);
         processBuilder.redirectOutput(ffmpegStdout);
+
+        if (terminator == null) {
+            return ExecUtil.execute(processBuilder);
+        }
 
         return ExecUtil.execute(processBuilder, terminator);
     }
@@ -329,6 +344,7 @@ public class VideoUtils {
      * representing by the AbstractFile object.
      *
      * @param file
+     *
      * @return
      */
     static File getVideoFile(AbstractFile file) {
@@ -368,50 +384,58 @@ public class VideoUtils {
         processBuilder.environment().put("__COMPAT_LAYER", "RunAsInvoker"); //NON-NLS
         return processBuilder;
     }
-    
+
     // Defaults for the screen capture methods.
     private static final long MIN_FRAME_INTERVAL_MILLIS = 500;
     private static final int DEFAULT_FRAMES_PER_SECOND = 1;
     private static final int DEFAULT_TOTAL_FRAMES = 100;
     private static final int DEFAULT_SCALE = 100;
-    
+
     /**
-     * Creates an output video containing a series of screen captures from the 
-     * input video.
-     * 
-     * @param inputFile
-     * @param outputFile
-     * 
-     * @throws IOException 
+     * Creates an output video containing a series of screen captures from the
+     * input video. The output file will be an avi with a unique name based on
+     * the input file.
+     *
+     * @param inputFile    File representing the input video.
+     * @param outDirectory The directory for the output file.
+     *
+     * @return The newly created file.
+     *
+     * @throws IOException
      */
-    static public void createScreenCaptureVideo(File inputFile, File outputFile)  throws IOException{
-        createScreenCaptureVideo(inputFile, outputFile, DEFAULT_TOTAL_FRAMES, DEFAULT_SCALE, DEFAULT_FRAMES_PER_SECOND);
+    static public File createScreenCaptureVideo(File inputFile, File outDirectory) throws IOException {
+        return createScreenCaptureVideo(inputFile, outDirectory, DEFAULT_TOTAL_FRAMES, DEFAULT_SCALE, DEFAULT_FRAMES_PER_SECOND);
     }
-    
+
     /**
      * Creates a video containing a series of screen captures from the input
      * video.
-     * @param inputFile File representing the input video.
-     * @param outputFile File representing the output video. 
-     *                  An exception will be thrown if the output video exists.
-     * @param numFrames Total number of screen captures to included in the video.
-     * @param scale     Percentage to scale the screen captures. The value of
-     *                  0 or 100 will cause no change.
-     * @param framesPerSecond The number of frames to show in the video each second
-     *                        The lower this value the longer the image will appear on the screen.
-     * @throws IOException 
+     *
+     * @param inputFile       File representing the input video.
+     * @param outDirectory    The directory for the output file.
+     * @param numFrames       Total number of screen captures to included in the
+     *                        video.
+     * @param scale           Percentage to scale the screen captures. The value
+     *                        of 0 or 100 will cause no change.
+     * @param framesPerSecond The number of frames to show in the video each
+     *                        second The lower this value the longer the image
+     *                        will appear on the screen.
+     *
+     * @return The newly created file.
+     *
+     * @throws IOException
      */
-    static public void createScreenCaptureVideo(File inputFile, File outputFile, long numFrames, double scale, int framesPerSecond) throws IOException{
+    static public File createScreenCaptureVideo(File inputFile, File outDirectory, long numFrames, double scale, int framesPerSecond) throws IOException {
 
-        if(outputFile.exists()) {
-            throw new IOException(String.format("Failed to compress %s, output file already exists %s", inputFile.getName(), outputFile.toString()));
+        if (!outDirectory.exists()) {
+            outDirectory.mkdirs();
+        } else if (!outDirectory.isDirectory()) {
+            throw new IOException(String.format("The passed in outDir is not a directory", outDirectory.getName()));
         }
-        
+
         if (!inputFile.exists() || !inputFile.canRead()) {
             throw new IOException(String.format("Failed to compress %s, input file cannot be read.", inputFile.getName()));
         }
-        
-        outputFile.mkdirs();
 
         String file_name = inputFile.toString();//OpenCV API requires string for file name
         VideoCapture videoCapture = new VideoCapture(file_name); //VV will contain the videos
@@ -447,13 +471,15 @@ public class VideoUtils {
             Size s = new Size((int) videoCapture.get(Videoio.CAP_PROP_FRAME_WIDTH), (int) videoCapture.get(Videoio.CAP_PROP_FRAME_HEIGHT));
             Size newSize = (scale == 0 || scale == 100) ? s : new Size((int) (videoCapture.get(Videoio.CAP_PROP_FRAME_WIDTH) * scale), (int) (videoCapture.get(Videoio.CAP_PROP_FRAME_HEIGHT) * scale));
 
+            File outputFile = createEmptyOutputFile(inputFile, outDirectory, ".avi");
             writer = new VideoWriter(outputFile.toString(), VideoWriter.fourcc('M', 'J', 'P', 'G'), 1, newSize, true);
 
             if (!writer.isOpened()) {
+                outputFile.delete();
                 throw new IOException(String.format("Problem with video file; problem when attempting to open output file. %s", outputFile.toString()));
             }
 
-            for (int frame = 0; frame < numFramesToGet; ++frame) {                
+            for (int frame = 0; frame < numFramesToGet; ++frame) {
                 long timeStamp = frame * frameInterval;
                 videoCapture.set(0, timeStamp); //set video in timeStamp ms
 
@@ -466,12 +492,35 @@ public class VideoUtils {
                 writer.write(resized);
             }
 
+            return outputFile;
+
         } finally {
             videoCapture.release();
             if (writer != null) {
                 writer.release();
             }
         }
+    }
+
+    /**
+     * Generates an empty at the given outputDirectory location with a unique
+     * name based on the inputFile name with the given extension.
+     *
+     * @param inputFile       InputFile to base new file name on.
+     * @param outputDirectory The directory the new file should be created in.
+     * @param extension       The extension the new file should have.
+     *
+     * @return The file.
+     *
+     * @throws IOException
+     */
+    private static File createEmptyOutputFile(File inputFile, File outputDirectory, String extension) throws IOException {
+        Path path = Paths.get(inputFile.getAbsolutePath());
+        String fileName = path.getFileName().toString();
+        if (fileName.contains(".")) {
+            fileName = fileName.substring(0, fileName.lastIndexOf('.'));
+        }
+        return File.createTempFile(fileName, extension, outputDirectory);
     }
 
     /**
