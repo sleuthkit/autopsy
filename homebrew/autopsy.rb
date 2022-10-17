2ce70f0564710764697a3286f4d0fc3a880a5ab0
Greg DiCristofaro <gregd@basistech.com>
2022-10-17 16:26:24 -0400

    updates

--- homebrew/autopsy.rb
+++ homebrew/autopsy.rb
@@ -11,14 +11,34 @@ class Autopsy < Formula
   depends_on "postgresql@14"
   depends_on "testdisk"
   depends_on "sleuthkit"
+  # TODO is this right?
+  depen# ds_on "gstreamer"
+  depends_on "zip" => :build
+  depends_on "tar" => :build
   depends_on "ant" => :build
 
   def install
     ENV.deparallelize
     # ----- GET ADDITIONAL DEPENDENCIES -----
     # TODO may be a better way to handle this
-    system "brew", "tap", "bell-sw/liberica"
-    system "brew", "install", "--cask", "liberica-jdk8-full"
+    # system "brew", "tap", "bell-sw/liberica"
+    # system "brew", "install", "--cask", "liberica-jdk8-full"
+    # jvm_tar_name = "bellsoft-jdk8u345+1-macos-amd64.tar.gz"
+    # jvm_tmp_path = "#{prefix}/#{jvm_tar_name}"
+    # jvm_bin_path = "#{prefix}/liberica_jvm"
+    # system "curl", "-k", "-o", jvm_tmp_path, "https://download.bell-sw.com/java/8u345+1/#{jvm_tar_name}"
+    # system "mkdir", "-p", jvm_bin_path
+    # system "tar", "-xf", "#{prefix}/#{jvm_tar_name}", "-C", jvm_bin_path
+    # # TODO do we need any permissions?
+    # system "rm", jvm_tmp_path
+
+    jvm_pkg_name = "bellsoft-jdk8u345+1-macos-amd64.pkg"
+    jvm_tmp_path = "#{prefix}/#{jvm_pkg_name}"
+    jvm_bin_path = "#{prefix}/liberica_jvm"
+    system "curl", "-k", "-o", jvm_tmp_path, "https://download.bell-sw.com/java/8u345+1/bellsoft-jdk8u345+1-macos-amd64.pkg"
+    system "mkdir", "-p", jvm_bin_path
+    system "installer", "-pkg", jvm_tmp_path, "-target", jvm_bin_path
+    system "rm", jvm_tmp_path
 
     # TODO may be a better way to handle this
     gstreamer_tmp_path = "#{prefix}/gstreamer-1.0-1.20.3-universal.pkg"
@@ -30,7 +50,7 @@ class Autopsy < Formula
 
     # ----- BUILD ZIP -----
     autopsy_src_path = `pwd`
-    java_path = `/usr/libexec/java_home -v 1.8`
+    java_path = `#{jvm_bin_path}/bin/java`
 
     netbeans_plat_ver = `grep "netbeans-plat-version=" "$AUTOPSY_SRC_PATH/nbproject/platform.properties" | cut -d'=' -f2`
     autopsy_platform_path = File.join(autopsy_src_path, "netbeans-plat", netbeans_plat_ver)
