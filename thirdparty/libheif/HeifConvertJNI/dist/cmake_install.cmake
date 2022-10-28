77bd89bf080ae04b8a5dfb94c376959a18c8edb5
Greg DiCristofaro <gregd@basistech.com>
2022-10-27 20:58:36 -0400

    updates

--- thirdparty/libheif/HeifConvertJNI/dist/cmake_install.cmake
+++ thirdparty/libheif/HeifConvertJNI/dist/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/heifconvert")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/heifconvert")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
