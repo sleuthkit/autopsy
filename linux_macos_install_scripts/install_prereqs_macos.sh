5a5b678484044fbcf6fc7e7040e7d7c7cc445739
Greg DiCristofaro <gregd@basistech.com>
2023-05-19 09:46:56 -0400

    mac prereqs update

--- linux_macos_install_scripts/install_prereqs_macos.sh
+++ linux_macos_install_scripts/install_prereqs_macos.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-echo "Installing dependencies..."
-brew install ant automake libtool afflib libewf postgresql testdisk libheif \
-    gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly gstreamer
-    
-if [[ $? -ne 0 ]] 
-then 
-    echo "Unable to install necessary dependencies" >> /dev/stderr
-    exit 1
-fi
-
 echo "Installing liberica java 17..."
 brew tap bell-sw/liberica && \
 brew install --cask liberica-jdk17-full
 if [[ $? -ne 0 ]] 
 then 
     echo "Unable to install liberica java" >> /dev/stderr
+    exit 1
+fi
+
+echo "Installing remaining dependencies..."
+brew install ant automake libtool afflib libewf postgresql@15 testdisk libheif gstreamer
+    
+if [[ $? -ne 0 ]] 
+then 
+    echo "Unable to install necessary dependencies" >> /dev/stderr
     exit 1
 fi
 
