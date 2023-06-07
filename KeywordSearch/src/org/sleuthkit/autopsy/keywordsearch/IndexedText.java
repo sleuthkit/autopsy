3757ca606da1b786c475760eff5e155f44989140
eugene.livis <elivis@basistech.com>
2023-06-07 15:22:35 -0400

    Minor

--- KeywordSearch/src/org/sleuthkit/autopsy/keywordsearch/IndexedText.java
+++ KeywordSearch/src/org/sleuthkit/autopsy/keywordsearch/IndexedText.java
@@ -1,7 +1,7 @@
 /*
  * Autopsy Forensic Browser
  *
- * Copyright 2011-2018 Basis Technology Corp.
+ * Copyright 2011-2023 Basis Technology Corp.
  * Contact: carrier <at> sleuthkit <dot> org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,12 +24,13 @@ import org.openide.util.NbBundle;
  * Interface to provide HTML text to display in ExtractedContentViewer. There is
  * a SOLR implementation of this that interfaces with SOLR to highlight the
  * keyword hits and a version that does not do markup so that you can simply
- * view the stored text.
+ * view the stored text. There is also an implementation that extracts text from
+ * a file using one os TextExtractors.
  */
 @NbBundle.Messages({
-    "IndexedText.errorMessage.errorGettingText=<span style='font-style:italic'>Error retrieving indexed text.</span>",
+    "IndexedText.errorMessage.errorGettingText=<span style='font-style:italic'>Error retrieving text.</span>",
     "IndexedText.warningMessage.knownFile=<span style='font-style:italic'>This file is a known file (based on MD5 hash) and does not have indexed text.</span>",
-    "IndexedText.warningMessage.noTextAvailable=<span style='font-style:italic'>No indexed text for this file.</span>"
+    "IndexedText.warningMessage.noTextAvailable=<span style='font-style:italic'>No text available for this file.</span>"
 })
 interface IndexedText {
 
