09e1b3e6ee887a26d16c84a45700ecf7b073af66
eugene.livis <elivis@basistech.com>
2025-03-11 11:59:19 -0400

    Minor

--- Core/src/org/sleuthkit/autopsy/casemodule/services/TagSetDefinition.java
+++ Core/src/org/sleuthkit/autopsy/casemodule/services/TagSetDefinition.java
@@ -166,8 +166,8 @@ final public class TagSetDefinition {
     // In TSK release 4.13.0 and Autopsy release 4.22.0 we:
     // 1) renamed "TskData.KnownStatus" to "TskData.TagType"
     // 2) renamed "TagSetDefinition.knownStatus" to "TagSetDefinition.tagType"
-    // 3) "TskData.KnownStatus" of "unknown" used to carry a score if "suspicious". 
-    //      Now "TskData.TagType" of "unknown" carries a score if "unknown".
+    // 3) "TskData.KnownStatus" of "unknown" used to carry a score of "suspicious". 
+    //      Now "TskData.TagType" of "unknown" carries a score of "unknown".
     //      
     private static class TagSetDefinitionDeserializer implements JsonDeserializer<TagSetDefinition> {
 
