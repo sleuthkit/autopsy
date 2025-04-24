f5482929cd522738b91da4d069405680a279a08d
Mark McKinnon <Mark.McKinnon13@outlook.com>
2025-04-24 10:36:20 -0400

    Update RAImageIngestModule.java
    Fix csv export erroring out where it could not find bytes_sent/Recvd

--- RecentActivity/src/org/sleuthkit/autopsy/recentactivity/RAImageIngestModule.java
+++ RecentActivity/src/org/sleuthkit/autopsy/recentactivity/RAImageIngestModule.java
@@ -85,6 +85,8 @@ public final class RAImageIngestModule implements DataSourceIngestModule {
         extractors.add(recycleBin);
         extractors.add(jumpList);
         extractors.add(recentDocuments);
+        extractors.add(sru);
+        extractors.add(prefetch);
         extractors.add(registry); //  needs to run before the DataSourceUsageAnalyzer
         extractors.add(osExtract); // this needs to run before the DataSourceUsageAnalyzer
         extractors.add(dataSourceAnalyzer); //this needs to run after ExtractRegistry and ExtractOs
@@ -96,8 +98,6 @@ public final class RAImageIngestModule implements DataSourceIngestModule {
         extractors.add(SEUQA); // this needs to run after the web browser modules
         extractors.add(webAccountType); // this needs to run after the web browser modules
         extractors.add(zoneInfo); // this needs to run after the web browser modules
-        extractors.add(sru);
-        extractors.add(prefetch);
         extractors.add(messageDomainType);
 
         browserExtractors.add(chrome);
